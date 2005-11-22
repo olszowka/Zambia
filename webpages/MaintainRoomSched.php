bfc185d1f73166e2b6352b5cfc05bb177dbdc79e
olszowka <olszowka@users.noreply.github.com>
2005-11-22 04:15:29 +0000

    Room/session scheduler created -- not working yet

--- webpages/MaintainRoomSched.php
+++ webpages/MaintainRoomSched.php
@@ -44,10 +44,19 @@ if ($selroomid==0) {
     exit();
     }
 $query = <<<EOD
-SELECT roomid, roomname, opentime1, closetime1, opentime2, closetime2, opentime3, closetime3
-FROM Rooms WHERE roomid=$selroomid
+SELECT roomid, roomname, opentime1, closetime1, opentime2, closetime2, opentime3, closetime3,
+height, dimensions, area, notes FROM Rooms WHERE roomid=$selroomid
 EOD;
 if (!$result=mysql_query($query,$link)) {
+    $message=$query."<BR>Error querying database. Unable to continue.<BR>";
+    echo "<P class\"errmsg\">".$message."\n";
+    staff_footer();
+    exit();
+    }
+$query = <<<EOD
+SELECT capacity FROM RoomHasSet WHERE roomsetid=5 AND roomid=$selroomid
+EOD;
+if (!$Cresult=mysql_query($query,$link)) {
     $message=$query."<BR>Error querying database. Unable to continue.<BR>";
     echo "<P class\"errmsg\">".$message."\n";
     staff_footer();
@@ -56,7 +65,7 @@ if (!$result=mysql_query($query,$link)) {
 echo "<H2>$selroomid - ".htmlspecialchars(mysql_result($result,0,"roomname"))."</H2>";
 //echo "|".mysql_result($result,0,"opentime1")."|<BR>\n";
 echo "<P>Open Times\n";
-echo "<P class=\"border1111 lrmargin lrpad\">";
+echo "<DIV class=\"border1111 lrpad\"><P class=\"lrmargin\">";
 if (mysql_result($result,0,"opentime1")!="") {
     echo time_description(mysql_result($result,0,"opentime1"))." through ".time_description(mysql_result($result,0,"closetime1"))."<BR>\n";
     }
@@ -66,6 +75,26 @@ if (mysql_result($result,0,"opentime2")!="") {
 if (mysql_result($result,0,"opentime3")!="") {
     echo time_description(mysql_result($result,0,"opentime3"))." through ".time_description(mysql_result($result,0,"closetime3"))."<BR>\n";
     }
+echo "</DIV>\n";
+echo "<P>Characteristics\n";
+echo "   <TABLE class=\"border1111=\">\n";
+echo "      <TR>\n";
+echo "         <TH class=\"lrpad border1111\">Dimensions</TH>\n";
+echo "         <TH class=\"lrpad border1111\">Area</TH>\n";
+echo "         <TH class=\"lrpad border1111\">Height</TH>\n";
+echo "         <TH class=\"lrpad border1111\">Theater<BR>Seating</TH>\n";
+echo "         </TR>\n";
+echo "      <TR>\n";
+echo "         <TD class=\"lrpad border1111\">".htmlspecialchars(mysql_result($result,0,"dimensions"))."</TD>\n";
+echo "         <TD class=\"lrpad border1111\">".htmlspecialchars(mysql_result($result,0,"area"))."</TD>\n";
+echo "         <TD class=\"lrpad border1111\">".htmlspecialchars(mysql_result($result,0,"height"))."</TD>\n";
+echo "         <TD class=\"lrpad border1111\">".htmlspecialchars(mysql_result($Cresult,0,"capacity"))."</TD>\n";
+echo "         </TR>\n";
+echo "      <TR>\n";
+echo "         <TD colspan=4 class=\"lrpad border1111\">".htmlspecialchars(mysql_result($result,0,"notes"))."</TD>\n";
+echo "         </TR>\n";
+echo "      </TABLE>\n";
+
 echo "<HR>\n";
 $query = <<<EOD
 SELECT scheduleid, starttime, sessionid FROM Schedule WHERE roomid=$selroomid ORDER BY starttime
