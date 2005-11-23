cfc1d81c00032c71ac59155a630c8d29689c1e7d
olszowka <olszowka@users.noreply.github.com>
2005-11-23 06:43:42 +0000

    Room/session scheduler now working

--- webpages/MaintainRoomSched.php
+++ webpages/MaintainRoomSched.php
@@ -46,7 +46,7 @@ if ($selroomid==0) {
     }
 $query = <<<EOD
 SELECT roomid, roomname, opentime1, closetime1, opentime2, closetime2, opentime3, closetime3,
-height, dimensions, area, notes FROM Rooms WHERE roomid=$selroomid
+function, floor, height, dimensions, area, notes FROM Rooms WHERE roomid=$selroomid
 EOD;
 if (!$result=mysql_query($query,$link)) {
     $message=$query."<BR>Error querying database. Unable to continue.<BR>";
@@ -54,18 +54,9 @@ if (!$result=mysql_query($query,$link)) {
     staff_footer();
     exit();
     }
-$query = <<<EOD
-SELECT capacity FROM RoomHasSet WHERE roomsetid=5 AND roomid=$selroomid
-EOD;
-if (!$Cresult=mysql_query($query,$link)) {
-    $message=$query."<BR>Error querying database. Unable to continue.<BR>";
-    echo "<P class\"errmsg\">".$message."\n";
-    staff_footer();
-    exit();
-    }
 echo "<H2>$selroomid - ".htmlspecialchars(mysql_result($result,0,"roomname"))."</H2>";
 //echo "|".mysql_result($result,0,"opentime1")."|<BR>\n";
-echo "<P>Open Times\n";
+echo "<H4>Open Times</H4>\n";
 echo "<DIV class=\"border1111 lrpad lrmargin\"><P class=\"lrmargin\">";
 if (mysql_result($result,0,"opentime1")!="") {
     echo time_description(mysql_result($result,0,"opentime1"))." through ".time_description(mysql_result($result,0,"closetime1"))."<BR>\n";
@@ -77,30 +68,30 @@ if (mysql_result($result,0,"opentime3")!="") {
     echo time_description(mysql_result($result,0,"opentime3"))." through ".time_description(mysql_result($result,0,"closetime3"))."<BR>\n";
     }
 echo "</DIV>\n";
-echo "<P>Characteristics\n";
+echo "<H4>Characteristics</H4>\n";
 echo "   <TABLE class=\"border1111=\">\n";
 echo "      <TR>\n";
+echo "         <TH class=\"lrpad border1111\">Function</TH>\n";
+echo "         <TH class=\"lrpad border1111\">Floor</TH>\n";
 echo "         <TH class=\"lrpad border1111\">Dimensions</TH>\n";
 echo "         <TH class=\"lrpad border1111\">Area</TH>\n";
 echo "         <TH class=\"lrpad border1111\">Height</TH>\n";
-echo "         <TH class=\"lrpad border1111\">Theater<BR>Seating</TH>\n";
 echo "         </TR>\n";
 echo "      <TR>\n";
+echo "         <TD class=\"lrpad border1111\">".htmlspecialchars(mysql_result($result,0,"function"))."</TD>\n";
+echo "         <TD class=\"lrpad border1111\">".htmlspecialchars(mysql_result($result,0,"floor"))."</TD>\n";
 echo "         <TD class=\"lrpad border1111\">".htmlspecialchars(mysql_result($result,0,"dimensions"))."</TD>\n";
 echo "         <TD class=\"lrpad border1111\">".htmlspecialchars(mysql_result($result,0,"area"))."</TD>\n";
 echo "         <TD class=\"lrpad border1111\">".htmlspecialchars(mysql_result($result,0,"height"))."</TD>\n";
-echo "         <TD class=\"lrpad border1111\">".htmlspecialchars(mysql_result($Cresult,0,"capacity"))."</TD>\n";
 echo "         </TR>\n";
 echo "      <TR>\n";
-echo "         <TD colspan=4 class=\"lrpad border1111\">".htmlspecialchars(mysql_result($result,0,"notes"))."</TD>\n";
+echo "         <TD colspan=5 class=\"lrpad border1111\">".htmlspecialchars(mysql_result($result,0,"notes"))."</TD>\n";
 echo "         </TR>\n";
 echo "      </TABLE>\n";
-
-echo "<HR>\n";
+echo "<H4>Room Sets</H4>\n";
 $query = <<<EOD
-SELECT SC.scheduleid, SC.starttime, S.duration, SC.sessionid, T.trackname, S.title 
-FROM Schedule SC join Sessions S on SC.sessionid = S.sessionid 
-join Tracks T on S.trackid = T.trackid WHERE SC.roomid=$selroomid ORDER BY SC.starttime
+SELECT RS.roomsetname, RHS.capacity FROM RoomSets RS, RoomHasSet RHS WHERE
+RS.roomsetid=RHS.roomsetid AND RHS.roomid=$selroomid
 EOD;
 if (!$result=mysql_query($query,$link)) {
     $message=$query."<BR>Error querying database. Unable to continue.<BR>";
@@ -113,7 +104,39 @@ while ($bigarray[$i] = mysql_fetch_array($result, MYSQL_ASSOC)) {
     $i++;
     }
 $numrows=$i;
+echo "   <TABLE class=\"border1111=\">\n";
+echo "      <TR>\n";
+echo "         <TH class=\"lrpad border1111\">Room Set</TH>\n";
+echo "         <TH class=\"lrpad border1111\">Capacity</TH>\n";
+echo "         </TR>\n";
+for ($i=0;$i<$numrows;$i++) {
+    echo "   <TR>\n";
+    echo "      <TD class=\"vatop lrpad border1111\">".$bigarray[$i]["roomsetname"]."</TD>\n";
+    echo "      <TD class=\"vatop lrpad border1111\">".$bigarray[$i]["capacity"]."</TD>\n";
+    echo "      </TR>\n";
+    }
+echo "      </TABLE>\n";
+$query = <<<EOD
+SELECT SC.scheduleid, SC.starttime, S.duration, SC.sessionid, T.trackname, S.title, ST.roomsetname 
+FROM Schedule SC, Sessions S, Tracks T, RoomSets ST WHERE
+SC.sessionid = S.sessionid AND S.trackid = T.trackid AND S.roomsetid=ST.roomsetid
+AND SC.roomid=$selroomid ORDER BY SC.starttime
+EOD;
+if (!$result=mysql_query($query,$link)) {
+    $message=$query."<BR>Error querying database. Unable to continue.<BR>";
+    echo "<P class\"errmsg\">".$message."\n";
+    staff_footer();
+    exit();
+    }
+$i=0;
+while ($bigarray[$i] = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    $i++;
+    }
+$numrows=$i;
+
+echo "<HR>\n";
 echo "<FORM name=\"rmschdform\" method=POST action=\"MaintainRoomSched.php\">\n";
+echo "<H4>Current Room Schedule</H4>\n";
 echo "<TABLE>\n";
 echo "   <TR>\n";
 echo "      <TH>Delete</TH>\n";
@@ -122,19 +145,22 @@ echo "      <TH>Duration</TH>\n";
 echo "      <TH>Track</TH>\n";
 echo "      <TH>Session ID</TH>\n";
 echo "      <TH>Title</TH>\n";
+echo "      <TH>Room Set</TH>\n";
 echo "      </TR>\n";
 for ($i=0;$i<$numrows;$i++) {
     echo "   <TR>\n";
-    echo "      <TD class=\"vatop\"><INPUT type=\"checkbox\" name=\"del$i\" value=\"1\"></TD>\n";
+    echo "      <TD class=\"border0010\"><INPUT type=\"checkbox\" name=\"del$i\" value=\"1\"></TD>\n";
     echo "<INPUT type=\"hidden\" name=\"row$i\" value=\"".$bigarray[$i]["scheduleid"]."\"></TD>\n";
-    echo "      <TD class=\"vatop lrpad\">".time_description($bigarray[$i]["starttime"])."</TD>\n";
-    echo "      <TD class=\"vatop lrpad\">".$bigarray[$i]["duration"]."</TD>\n";
-    echo "      <TD class=\"vatop lrpad\">".$bigarray[$i]["trackname"]."</TD>\n";
-    echo "      <TD class=\"vatop lrpad\">".$bigarray[$i]["sessionid"]."</TD>\n";
-    echo "      <TD class=\"vatop lrpad\">".$bigarray[$i]["title"]."</TD>\n";
+    echo "      <TD class=\"vatop lrpad border0010\">".time_description($bigarray[$i]["starttime"])."</TD>\n";
+    echo "      <TD class=\"vatop lrpad border0010\">".$bigarray[$i]["duration"]."</TD>\n";
+    echo "      <TD class=\"vatop lrpad border0010\">".$bigarray[$i]["trackname"]."</TD>\n";
+    echo "      <TD class=\"vatop lrpad border0010\">".$bigarray[$i]["sessionid"]."</TD>\n";
+    echo "      <TD class=\"vatop lrpad border0010\">".$bigarray[$i]["title"]."</TD>\n";
+    echo "      <TD class=\"vatop lrpad border0010\">".$bigarray[$i]["roomsetname"]."</TD>\n";
     echo "      </TR>\n";
     }
 echo "   </TABLE>\n";
+echo "<H4>Add To Room Schedule</H4>\n";
 echo "<TABLE>\n";
 $query = <<<EOD
 SELECT S.sessionid, T.trackname, S.title, SC.roomid FROM Tracks AS T join Sessions AS S ON T.trackid = S.trackid
@@ -154,20 +180,20 @@ while ($bigarray[$i] = mysql_fetch_array($result, MYSQL_ASSOC)) {
 $numsessions=$i;
 for ($i=1;$i<=newroomslots;$i++) {
     echo "   <TR>\n";
-    echo "      <TD><Select name=\"day$i\"><Option value=0 selected>Day</Option><Option value=1>Fri</Option>";
+    echo "      <TD><Select name=\"day$i\"><Option value=0 selected>Day&nbsp;</Option><Option value=1>Fri</Option>";
     echo "<Option value=2>Sat</Option><Option value=3>Sun</Option></Select>&nbsp;\n";
-    echo "          <Select name=\"hour$i\"><Option value=\"unset\" selected>Hour</Option><Option value=0>12</Option>";
+    echo "          <Select name=\"hour$i\"><Option value=\"unset\" selected>Hour&nbsp;</Option><Option value=0>12</Option>";
     for ($j=1;$j<=11;$j++) {
         echo "<Option value=$j>$j</Option>";
         }
     echo "</select>\n";
-    echo "          <Select name=\"min$i\"><Option value=\"unset\" selected>Min</Option><Option value=0>00</Option>";
+    echo "          <Select name=\"min$i\"><Option value=\"unset\" selected>Min&nbsp;</Option><Option value=0>00</Option>";
     echo "<Option value=5>05</Option>";
     for ($j=10;$j<=55;$j+=5) {
         echo "<Option value=$j>$j</Option>";
         }
     echo "</select>\n";
-    echo "          <Select name=\"ampm$i\"><Option value=0 selected>AM</Option><Option value=1>PM</Option>";
+    echo "          <Select name=\"ampm$i\"><Option value=0 selected>AM&nbsp;</Option><Option value=1>PM</Option>";
     echo "</select>\n";
     echo "          </TD>";
     echo "      <TD><Select name=\"sess$i\"><Option value=\"unset\" selected>Select Session</Option>\n";
