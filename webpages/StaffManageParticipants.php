b65edd136fb436290ac09c8c3ce1f81cb1ae7f29
trg <trgangwer@gmail.com>
2005-11-26 22:01:44 +0000

    Added room scheduling to menu.

--- webpages/StaffManageParticipants.php
+++ webpages/StaffManageParticipants.php
@@ -15,6 +15,8 @@ On this page you will find the online tools for managing Participants.</p>
    <DD>Use this tool to put sessions marked "invited guests only" on a participant's interest list.</DD>
    <DT><A HREF="AssignParticipants.php">Assign participants to a session</A></DT>
    <DD>Use this tool to assign participants to a session and select moderator.</DD>
+   <DT><A HREF="MaintainRoomSched.php">Maintain room schedule</A></DT>
+   <DD>Assign sessions at particular times in a room.</DD>
 </DL>
 
 <?php staff_footer(); ?>
