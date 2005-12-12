f50e843426e939ed0a175bc4b62ca4afd469f72f
trg <trg@676fd721-d978-42df-8417-bacc17f9f818>
2005-12-12 23:18:17 +0000

    changes in prod

--- trunk/webpages/db_functions.php
+++ trunk/webpages/db_functions.php
@@ -11,7 +11,7 @@ function prepare_db() {
     }
 
 function isStaff($badgeid) {
-    return (array_search($badgeid,array("qwerty","00001","40099","38322","92972","53159","27051","4694","9563","4400","40439","5833","4398","4398","4516","27066","26912","20027","4720")));
+    return (array_search($badgeid,array("qwerty","00001","40099","38322","92972","53159","27051","4694","9563","4440","40439","5833","4398","4398","4516","27066","26912","20027","4720","9951","6622","93178")));
 
 //  Don't add folk to the above until thier password has changed  aka
 //    select badgeid from Participants 
@@ -33,6 +33,9 @@ function isStaff($badgeid) {
 //20027 - Merv
 //26912 - Jesse
 //4720 - Mike
+//9951 - Janet
+//6622 - Sarah (reg) wolfgrrl
+//93178 - Lisa (reg) Jasra
 // need changing:
 //4321 - Joel Herda
 //4782 - Patricia Vandenberg (dda's assistant div head)
