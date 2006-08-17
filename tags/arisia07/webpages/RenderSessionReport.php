f7d842e6aec36186cc4454469a3f21f77c75f227
nobody <nobody@676fd721-d978-42df-8417-bacc17f9f818>
2006-08-17 01:26:21 +0000

    This commit was manufactured by cvs2svn to create tag 'arisia07'.

---
+++ tags/arisia07/webpages/RenderSessionReport.php
@@ -0,0 +1,32 @@
+<?php
+  function RenderSessionReport() {
+    global $result; 
+    $title="Session Report";
+    require_once('db_functions.php');
+    require_once('StaffHeader.php');
+    require_once('StaffFooter.php');
+    require_once('StaffCommonCode.php');
+    staff_header($title);
+?>
+<p> Here are the results of your search.  The report includes Session id, track, title, duration, estimated attendance, pocket program text, notes for perspective participants.
+<table>
+<?php
+    while (list($sessionid,$trackname,$title,$duration,$estatten,$pocketprogtext, $persppartinfo)= mysql_fetch_array($result, MYSQL_NUM)) {
+        echo "        <TR>\n";
+        echo "            <TD rowspan=3 class=\"border0000\" id=\"sessidtcell\">
+<A HREF=\"EditSession.php?id=".$sessionid."\"><b>".$sessionid."</a>&nbsp;&nbsp;</TD>\n";
+
+        echo "            <TD class=\"border0000\"><b>".$trackname."</TD>\n";
+        echo "            <TD class=\"border0000\"><b>".htmlspecialchars($title,ENT_NOQUOTES)."</TD>\n";
+        echo "            <TD class=\"border0000\"><b>".$duration." hr</TD>\n";
+        echo "            <TD rowspan=3 class=\"border0000\">".$estatten."&nbsp;&nbsp;</TD>\n";
+        echo "            </TR>\n";
+        echo "        <TR><TD colspan=3 class=\"border0010\">".htmlspecialchars($pocketprogtext,ENT_NOQUOTES)."</TD></TR>\n";
+        echo "        <TR><TD colspan=3 class=\"border0000\">".htmlspecialchars($persppartinfo,ENT_NOQUOTES)."</TD></TR>\n";
+        echo "        <TR><TD colspan=5 class=\"border0020\">&nbsp;</TD></TR>\n";
+        }
+?>
+        </TABLE>
+<?php
+  staff_footer();    }
+?>
