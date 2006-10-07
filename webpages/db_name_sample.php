c891d79b963960e1baa1b2c95879797e97886563
trg <trgangwer@gmail.com>
2006-10-07 14:31:26 +0000

    Added Sample file.

---
+++ webpages/db_name_sample.php
@@ -0,0 +1,13 @@
+<?php
+// This is an example file.  Please copy to db_name.php and edit as needed.
+define("DBHOSTNAME","localhost");
+define("DBUSERID","zambiademo");
+define("DBPASSWORD","4fandom");
+define("DBDB","zambiademo");
+define("CON_NAME","Zambia Demo");
+define("BRAINSTORM_EMAIL","brain@somewhere.net");
+define("PROGRAM_EMAIL","nobody@somewhere.net");
+define("REG_EMAIL","noone@somewhere.net");
+define("stripfancy_from","ÀÁÂÃÄÅÆÇÈÉÊË®");
+define("stripfancy_to","AAAAAAECEEEE ");
+?>
