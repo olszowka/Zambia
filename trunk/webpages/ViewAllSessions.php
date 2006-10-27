ae4396da1988a63dd5cd8d5a6c60f4f2852f51d0
trg <trg@676fd721-d978-42df-8417-bacc17f9f818>
2006-10-27 03:38:31 +0000

    Excluded last years sessions

--- trunk/webpages/ViewAllSessions.php
+++ trunk/webpages/ViewAllSessions.php
@@ -13,7 +13,7 @@
         exit ();
         }
    $query = <<<EOD
-SELECT sessionid, trackname, title, concat( if(left(duration,2)=00, '', if(left(duration,1)=0, concat(right(left(duration,2),1),'hr '), concat(left(duration,2),'hr '))), if(date_format(duration,'%i')=00, '', if(left(date_format(duration,'%i'),1)=0, concat(right(date_format(duration,'%i'),1),'min'), concat(date_format(duration,'%i'),'min')))), estatten, statusname from Sessions, Tracks, SessionStatuses where Sessions.trackid=Tracks.trackid and Sessions.statusid=SessionStatuses.statusid
+SELECT sessionid, trackname, title, concat( if(left(duration,2)=00, '', if(left(duration,1)=0, concat(right(left(duration,2),1),'hr '), concat(left(duration,2),'hr '))), if(date_format(duration,'%i')=00, '', if(left(date_format(duration,'%i'),1)=0, concat(right(date_format(duration,'%i'),1),'min'), concat(date_format(duration,'%i'),'min')))), estatten, statusname from Sessions, Tracks, SessionStatuses where Sessions.trackid=Tracks.trackid and Sessions.statusid=SessionStatuses.statusid and SessionStatuses.statusname not in ('A06scheduled')
 EOD;
     if (($result=mysql_query($query,$link))===false) {
         $message="Error retrieving data from database.";
