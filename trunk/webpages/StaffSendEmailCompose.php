1c974316958113d8916052511c42e3c605e2094c
olszowka <olszowka@676fd721-d978-42df-8417-bacc17f9f818>
2006-10-28 23:09:37 +0000

    More progress on send email functionality, but nothing functional yet.

--- trunk/webpages/StaffSendEmailCompose.php
+++ trunk/webpages/StaffSendEmailCompose.php
@@ -1,14 +1,13 @@
 <?php
-$title="Send Email to Participants";
+require_once('email_functions.php');
 require_once('db_functions.php');
-require_once('StaffHeader.php');
-require_once('StaffFooter.php');
-require_once('StaffCommonCode.php');
-staff_header($title);
-
-echo "<P>Sorry.  This is a non-functional stubb.</P>";
-echo "<FORM name=\"emailform\" method=POST action=\"POSTStaffSendEmail2.php\">";
-echo "<DIV><LABEL for=\"selpart\">Select Participant</LABEL>\n";
-echo "<SELECT name=\"selpart\">\n";
-echo "<P>&nbsp;";
-staff_footer(); ?>
+require_once('StaffCommonCode.php'); //reset connection to db and check if logged in
+if (isset($_POST['sendto'])) { // page has been visited before
+// restore previous values to form
+        $email=get_email_from_post();
+        }
+    else { // page hasn't just been visited
+        $email=set_email_defaults();
+        }
+render_send_email($email);
+?>
