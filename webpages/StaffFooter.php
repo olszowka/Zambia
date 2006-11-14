44b41156bb414f0b94d98b06953224b5476c823c
trg <trgangwer@gmail.com>
2006-11-14 04:58:57 +0000

    using variable for email address

--- webpages/StaffFooter.php
+++ webpages/StaffFooter.php
@@ -4,7 +4,7 @@
 
 <hr>
 <p> 
-If you would like assistance using this tool or you would like to communicate an idea that you cannot fit into this form, please contact <a href="mailto: nobody@nowhere.org"> nobody@nowhere.org </a>.
+If you would like assistance using this tool or you would like to communicate an idea that you cannot fit into this form, please contact <a href="mailto: <?php echo PROGRAM_EMAIL."\">".PROGRAM_EMAIL ?></a>.
 </body>
 </html>
 
