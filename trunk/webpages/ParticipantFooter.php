05f00b25c6c4797be8e82fb2c4bb0167c9c748eb
trg <trg@676fd721-d978-42df-8417-bacc17f9f818>
2006-11-14 04:57:27 +0000

    changed to using email variable

--- trunk/webpages/ParticipantFooter.php
+++ trunk/webpages/ParticipantFooter.php
@@ -4,7 +4,7 @@
 
 <hr>
 <p> 
-If you need help or to tell us something that doesn't fit here, please email <a href="mailto: nobody@nowhere.org">nobody@nowhere.org</a>.
+If you need help or to tell us something that doesn't fit here, please email <a href="mailto: <?php echo PROGRAM_EMAIL."\">".PROGRAM_EMAIL ?></a>.
 <?php
 // This line is for debugging only
 // echo "<PRE>".print_r($_SESSION['permission_set_specific'])."</PRE><BR>";
