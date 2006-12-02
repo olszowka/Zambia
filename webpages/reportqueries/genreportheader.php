8c671490c76e3aea9c5e4c3c6ea006243b59f20d
trg <trgangwer@gmail.com>
2006-12-02 03:27:45 +0000

    Updated report for Rachel added links!  and added support for returning from edit to the calling page.

--- webpages/reportqueries/genreportheader.php
+++ webpages/reportqueries/genreportheader.php
@@ -5,6 +5,7 @@
   require_once('StaffFooter.php');
   require_once('StaffCommonCode.php');
   staff_header($title);
+  $_SESSION['return_to_page']="REPORT_LINK";
 ?>
 <p align=center> Generated: REPORT_DATE
 <P>REPORT_DESCRIPTION
