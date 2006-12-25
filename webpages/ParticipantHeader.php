9c833ac740239e4f0f91ee89177ae873e621442c
trg <trgangwer@gmail.com>
2006-12-25 04:46:36 +0000

    Added encoding note for browsers.

--- webpages/ParticipantHeader.php
+++ webpages/ParticipantHeader.php
@@ -7,6 +7,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/strict.dtd">
 <html xmlns="http://www.w3.org/TR/xhtml1/transitional">
 <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=latin-1">
   <title>Zambia -- <?php echo $title ?></title>
   <link rel="stylesheet" href="ParticipantSection.css" type="text/css">
   <meta name="keywords" content="Questionnaire">
