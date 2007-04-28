8e488aabcda90e43138af424c251048700224891
trg <trg@676fd721-d978-42df-8417-bacc17f9f818>
2007-04-28 22:15:03 +0000

    Added reports and welcome page for brainstorm login pages.

---
+++ trunk/webpages/BrainstormWelcome.php
@@ -0,0 +1,15 @@
+<?php
+    global $participant,$message_error,$message2,$congoinfo;
+    $title="Welcome";
+    require ('db_functions.php'); //define database functions
+    require ('RenderErrorPart.php');  // define function to report error
+    require ('PartCommonCode.php'); // initialize db; check login;
+    //                                  set $badgeid from session
+    if (retrieve_participant_from_db($badgeid)==0) {
+        require ('renderBrainstormWelcome.php');
+        exit();
+        }
+    $message_error=$message2."<BR>Error retrieving data from DB.  No further execution possible.";
+    RenderError($title,$message_error);
+    exit();
+?>
---
+++ trunk/webpages/renderBrainstormWelcome.php
@@ -0,0 +1,42 @@
+<?php
+   global $participant,$message,$message_error,$message2,$congoinfo;
+   $title="Welcome";
+   require_once('data_functions.php');
+   require_once('ParticipantHeader.php');
+   require_once('ParticipantFooter.php');
+   participant_header($title);
+?>
+
+<?php if ($message_error!="") { ?>
+	<P class="errmsg"><?php echo $message_error; ?></P>
+	<?php } ?>
+<?php if ($message!="") { ?>
+	<P class="regmsg"><?php echo $message; ?></P>
+	<?php } ?>
+<?php
+    $chpw=($participant["password"]=="4cb9c8a8048fd02294477fcb1a41191a");
+    $chint=($participant["interested"]==0);
+?>
+<p> Here you can submit new suggestions or look at existing ideas for 
+panels, events, movies, films, presentations, speeches, concerts, etc.  
+<p> As suggestions come in and we read through them, we will rework 
+them, combine similar ideas into a single item, 
+split large ones into pieces that will fit in an hour, etc.    
+Please expect the suggestions you submit to evolve over time.  
+<p> Also, please note that we always have more suggestions than are 
+physically possible with the space and time we have, so not 
+everything will make it.   We do save good ideas for future conventions. 
+<ul> 
+<li> Search for similar ideas or get inspiration.
+<li> Email <?php echo "<a href=\"mailto:".BRAINSTORM_EMAIL."\">".BRAINSTORM_EMAIL."</a> "?> to suggest modifications on existing suggestion.
+<li> Enter a new suggestion 
+<li> See the <a href="BrainstormReportUnseen.php"> list of suggestions that have been entered recently </a> (may not be fit for young eyes, we haven't see these yet). 
+<li> See the <a href="BrainstormReportVetted.php">list of suggestions we are currently sorting through</a> (we have see these).
+<li> See the <a href="BrainstormReportAll.php">list of all suggestions</a> (we've seen some and not see others).
+<li> Email <?php echo "<a href=\"mailto:".PROGRAM_EMAIL."\">".PROGRAM_EMAIL."</a> "?> to volunteer to help process these ideas. 
+</ul>
+
+<p> Thank you and we look forward to reading your suggestions.
+
+<p>- <a href="mailto: <?php echo PROGRAM_EMAIL; ?>"><?php echo PROGRAM_EMAIL; ?> </a> </P>
+<?php participant_footer(); ?>
