aec6a518a65bdb8956a86f11d3f84bc91d72a5e9
olszowka <olszowka@676fd721-d978-42df-8417-bacc17f9f818>
2007-04-29 02:19:17 +0000

    Fixed column order in Participants.

--- trunk/Install/Upgrade_dbase/08_add_brainstorm.sql
+++ trunk/Install/Upgrade_dbase/08_add_brainstorm.sql
@@ -3,5 +3,6 @@ INSERT INTO `Phases` VALUES (6,'Brainstorm',1,'Special brainstorm pages are acce
 INSERT INTO `PermissionAtoms` VALUES (18,'Brainstorm',null,'BrainstormWelcome','Use for special brainstorm user and pages.');
 INSERT INTO `PermissionRoles` VALUES (5,'Brainstorm','Use for Brainstorm pages');
 INSERT INTO `Permissions` VALUES (21,18,6,5,null);
-INSERT INTO `Participants` VALUES ('brainstorm',null,'ecf65a5d41056d7dd4d548e3ef200476',null,null,null);
+INSERT INTO `Participants` (badgeid, pubsname, password, bestway, interested, bio)
+ VALUES ('brainstorm',null,'ecf65a5d41056d7dd4d548e3ef200476',null,null,null);
 INSERT INTO `UserHasPermissionRole` VALUES ('brainstorm',5);
