5b7bd1e8e958a09a7ea3a437d4604367fa005637
trg <trg@676fd721-d978-42df-8417-bacc17f9f818>
2007-06-04 04:12:59 +0000

    Added logic to use Types selfselect as well as Tracks selfselect.

--- trunk/webpages/SearchMySessions1.php
+++ trunk/webpages/SearchMySessions1.php
@@ -7,7 +7,7 @@
     $trackid=$_POST["track"];
     $titlesearch=stripslashes($_POST["title"]);
     $query = <<<EOD
-Select S2.sessionid, S2.trackname, S2.title, S2.duration, S2.progguiddesc, S2.persppartinfo, PSI.badgeid from ( Select S.sessionid, T.trackname, S.title, concat( if(left(duration,2)=00, '', if(left(duration,1)=0, concat(right(left(duration,2),1),'hr '), concat(left(duration,2),'hr '))), if(date_format(duration,'%i')=00, '', if(left(date_format(duration,'%i'),1)=0, concat(right(date_format(duration,'%i'),1),'min'), concat(date_format(duration,'%i'),'min')))) as duration, S.progguiddesc, S.persppartinfo from Sessions S, Tracks AS T where S.trackid = T.trackid and T.selfselect=1 and S.invitedguest=0 and (S.statusid=2 or S.statusid=7 or S.statusid=3)
+Select S2.sessionid, S2.trackname, S2.title, S2.duration, S2.progguiddesc, S2.persppartinfo, PSI.badgeid from ( Select S.sessionid, T.trackname, S.title, concat( if(left(duration,2)=00, '', if(left(duration,1)=0, concat(right(left(duration,2),1),'hr '), concat(left(duration,2),'hr '))), if(date_format(duration,'%i')=00, '', if(left(date_format(duration,'%i'),1)=0, concat(right(date_format(duration,'%i'),1),'min'), concat(date_format(duration,'%i'),'min')))) as duration, S.progguiddesc, S.persppartinfo from Sessions S, Tracks AS T, Types Y where S.trackid = T.trackid and T.selfselect=1 and S.typeid=Y.typeid and Y.selfselect=1 and S.invitedguest=0 and (S.statusid=2 or S.statusid=7 or S.statusid=3)
 EOD;
     if ($trackid!=0) {
         $query.=" and S.trackid=".$trackid;
