b595c7affa455c7ad7e6422f610ea9ee0a1578b5
trg <trg@676fd721-d978-42df-8417-bacc17f9f818>
2007-06-04 04:59:19 +0000

    latest versions of the export scripts

--- trunk/scripts/export_program_participants_arisia08.sql
+++ trunk/scripts/export_program_participants_arisia08.sql
@@ -1,17 +1,17 @@
+
+/* create the entry in CongoDump */
 select concat('insert into CongoDump set badgeid="',m.master_rid,'";') ""
   from reg_master as m, reg_properties as p, 
        con_detail as c
  where p.prop_rid=m.master_rid 
    and c.con_cid=p.prop_cid 
-   and c.con_cid=18
-   and (p.prop_name='db_ProgramStaff' 
-        or p.prop_name='db_Program' 
-        or p.prop_name='Z_EventStaff' 
-        or p.prop_name='Z_EventGuest' )
+   and c.con_cid=19
+   and p.prop_name like 'Z_%' 
    and p.prop_value=1
    ;
 
 
+/* update the badgename, firstname and lastname in CongoDump */
 select concat('update CongoDump set badgename="',m.master_badgename,
                                 '", firstname="',m.master_firstname,
                                 '", lastname="',m.master_lastname,
@@ -20,65 +20,57 @@ select concat('update CongoDump set badgename="',m.master_badgename,
        con_detail as c
  where p.prop_rid=m.master_rid 
    and c.con_cid=p.prop_cid 
-   and c.con_cid=18
-   and (p.prop_name='db_ProgramStaff' 
-        or p.prop_name='db_Program' 
-        or p.prop_name='Z_EventStaff' 
-        or p.prop_name='Z_EventGuest' )
+   and c.con_cid=19
+   and p.prop_name like 'Z_%' 
    and p.prop_value=1
    ;
 
+/* update the phone number in CongoDump */
 select concat('update CongoDump set phone="',a.loc_data,
               '" where badgeid="',m.master_rid,'";') ""
   from reg_master as m, reg_properties as p, 
        con_detail as c, reg_locations as a 
  where p.prop_rid=m.master_rid 
    and c.con_cid=p.prop_cid 
-   and c.con_cid=18
-   and (p.prop_name='db_ProgramStaff' 
-        or p.prop_name='db_Program' 
-        or p.prop_name='Z_EventStaff' 
-        or p.prop_name='Z_EventGuest' )
+   and c.con_cid=19
+   and p.prop_name like 'Z_%' 
    and a.loc_rid=m.master_rid 
    and p.prop_value=1
    and a.loc_type='PHONE'
    and a.loc_primary=1
    ;
 
+/* update the email in CongoDump */
 select concat('update CongoDump set email="',a.loc_data,
               '" where badgeid="',m.master_rid,'";') ""
   from reg_master as m, reg_properties as p, 
        con_detail as c, reg_locations as a 
  where p.prop_rid=m.master_rid 
    and c.con_cid=p.prop_cid 
-   and c.con_cid=18
-   and (p.prop_name='db_ProgramStaff' 
-        or p.prop_name='db_Program' 
-        or p.prop_name='Z_EventStaff' 
-        or p.prop_name='Z_EventGuest' )
+   and c.con_cid=19
+   and p.prop_name like 'Z_%' 
    and a.loc_rid=m.master_rid 
    and p.prop_value=1
    and a.loc_type='EMAIL'
    and a.loc_primary=1
    ;
 
+/* update the postal address in CongoDump */
 select concat('update CongoDump set postaddress="',a.loc_data,
               '" where badgeid="',m.master_rid,'";') ""
   from reg_master as m, reg_properties as p, 
        con_detail as c, reg_locations as a 
  where p.prop_rid=m.master_rid 
    and c.con_cid=p.prop_cid 
-   and c.con_cid=18
-   and (p.prop_name='db_ProgramStaff' 
-        or p.prop_name='db_Program' 
-        or p.prop_name='Z_EventStaff' 
-        or p.prop_name='Z_EventGuest' )
+   and c.con_cid=19
+   and p.prop_name like 'Z_%' 
    and a.loc_rid=m.master_rid 
    and p.prop_value=1
    and a.loc_type='POSTAL'
    and a.loc_primary=1
    ;
 
+/* update the registration information in CongoDump */
 select concat('update CongoDump set regtype="',h.hist_arg1,
               '" where badgeid="',X.master_rid,'";') ""
   from (select master_rid 
@@ -86,47 +78,42 @@ select concat('update CongoDump set regtype="',h.hist_arg1,
                con_detail as c
 	 where p.prop_rid=m.master_rid
    	   and c.con_cid=p.prop_cid
-           and c.con_cid=18
-           and (p.prop_name='db_ProgramStaff' 
-                or p.prop_name='db_Program' 
-                or p.prop_name='Z_EventStaff' 
-                or p.prop_name='Z_EventGuest' )
-	   and p.prop_value=1 and c.con_cid=18) X, 
+           and c.con_cid=19
+           and p.prop_name like 'Z_%' 
+	   and p.prop_value=1 and c.con_cid=19) X, 
 	reg_history h 
   where X.master_rid=h.hist_rid 
     and h.hist_actcode='REGISTERED' 
-    and h.hist_cid=18
+    and h.hist_cid=19
     ;
 
+/* set up an entry in Participant with the default password 
+   (if there is not already an entry present) */
 select concat('insert into Participants set badgeid="',m.master_rid,
               '", password="4cb9c8a8048fd02294477fcb1a41191a";') ""
   from reg_master as m, reg_properties as p, 
        con_detail as c
  where p.prop_rid=m.master_rid 
    and c.con_cid=p.prop_cid 
-   and c.con_cid=18
-   and (p.prop_name='db_ProgramStaff' 
-        or p.prop_name='db_Program' 
-        or p.prop_name='Z_EventStaff' 
-        or p.prop_name='Z_EventGuest' )
+   and c.con_cid=19
+   and p.prop_name like 'Z_%' 
    and p.prop_value=1
    ;
 
+/* set up an entry in ParticipantAvailability */
 select concat('insert into ParticipantAvailability set badgeid="',m.master_rid,
               '";') ""
   from reg_master as m, reg_properties as p, 
        con_detail as c, reg_locations as a 
  where p.prop_rid=m.master_rid 
    and c.con_cid=p.prop_cid 
-   and c.con_cid=18
-   and (p.prop_name='db_ProgramStaff' 
-        or p.prop_name='db_Program' 
-        or p.prop_name='Z_EventStaff' 
-        or p.prop_name='Z_EventGuest' )
+   and c.con_cid=19
+   and p.prop_name like 'Z_%' 
    and a.loc_rid=m.master_rid 
    and p.prop_value=1
    ;
 
+/* Remove any dead people from CongoDump */
 select concat('delete from CongoDump where badgeid="',m.master_rid,'";') ""
   from reg_master as m, reg_properties as p, 
        con_detail as c
@@ -136,28 +123,24 @@ select concat('delete from CongoDump where badgeid="',m.master_rid,'";') ""
    and p.prop_value=1
    ;
 
-# Setting up access rights for participants (permroleid=3)
-
+/* Setting up access rights for participants (permroleid=3) */
 select concat('insert into UserHasPermissionRole set badgeid="',m.master_rid,'", permroleid=3;') ""
   from reg_master as m, reg_properties as p, 
        con_detail as c
  where p.prop_rid=m.master_rid 
    and c.con_cid=p.prop_cid 
-   and c.con_cid=18
-   and ( p.prop_name='db_Program' 
-        or p.prop_name='Z_EventGuest' )
+   and c.con_cid=19
+   and p.prop_name='Z_%Guest' 
    and p.prop_value=1
    ;
 
-# Setting up access rights for staff (permroleid=1)
-
+/* Setting up access rights for staff (permroleid=1) */
 select concat('insert into UserHasPermissionRole set badgeid="',m.master_rid,'", permroleid=1;') ""
   from reg_master as m, reg_properties as p, 
        con_detail as c
  where p.prop_rid=m.master_rid 
    and c.con_cid=p.prop_cid 
-   and c.con_cid=18
-   and ( p.prop_name='db_ProgramStaff' 
-        or p.prop_name='Z_EventStaff' )
+   and c.con_cid=19
+   and p.prop_name='Z_%Staff' 
    and p.prop_value=1
    ;
