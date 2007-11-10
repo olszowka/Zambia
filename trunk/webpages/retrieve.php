47dcfaff5cc4220663fd869e7aed09f2d08eeb2d
trg <trg@676fd721-d978-42df-8417-bacc17f9f818>
2007-11-10 22:32:36 +0000

    Added Type to the list of things shown on the Precis Report

--- trunk/webpages/retrieve.php
+++ trunk/webpages/retrieve.php
@@ -4,7 +4,7 @@ function retrieve_select_from_db($track,$status,$type){
     global $link, $message2; 
     require_once('db_functions.php');
 
-    $query="SELECT sessionid, trackname, title, concat( if(left(duration,2)=00, '', if(left(duration,1)=0, concat(right(left(duration,2),1),'hr '), concat(left(duration,2),'hr '))), if(date_format(duration,'%i')=00, '', if(left(date_format(duration,'%i'),1)=0, concat(right(date_format(duration,'%i'),1),'min'), concat(date_format(duration,'%i'),'min')))) duration, estatten, progguiddesc, persppartinfo from Sessions, Tracks, SessionStatuses WHERE Sessions.trackid=Tracks.trackid AND Sessions.statusid=SessionStatuses.statusid"; 
+    $query="SELECT sessionid, trackname, typename, title, concat( if(left(duration,2)=00, '', if(left(duration,1)=0, concat(right(left(duration,2),1),'hr '), concat(left(duration,2),'hr '))), if(date_format(duration,'%i')=00, '', if(left(date_format(duration,'%i'),1)=0, concat(right(date_format(duration,'%i'),1),'min'), concat(date_format(duration,'%i'),'min')))) duration, estatten, progguiddesc, persppartinfo from Sessions, Tracks, Types, SessionStatuses WHERE Sessions.trackid=Tracks.trackid AND Sessions.statusid=SessionStatuses.statusid AND Sessions.typeid=Types.typeid"; 
 
 // The following three lines are for debugging only
 //    error_log("retrieve: trackid: $track");
