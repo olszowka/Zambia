7b2d1efd8319e2cb370e3a5d4a57bd386d5d0680
trg <trg@676fd721-d978-42df-8417-bacc17f9f818>
2007-11-11 23:20:23 +0000

    checking in so Peter can fix it.

---
+++ trunk/Install/Upgrade_dbase/14_interested_table.sql
@@ -0,0 +1,15 @@
+## This script adds the Interesetd table which permits the easy translation 
+## of values to meanings. 
+create table `Interested` ( 
+  `interested` tinyint(1), 
+  `interestedname` varchar(100), 
+  primary key (`interested`)
+);
+insert into Interested set interested=1, interestedname='Will attend';
+insert into Interested set interested=2, interestedname='Will not attend';
+insert into Interested set interested=0, interestedname='didnot say';
+
+alter table Participants 
+  add foreign key  (`interested`) references Interested (`interested`);
+
+insert into `PatchLog` (`patchname`) values ('14_interested_table.sql');
