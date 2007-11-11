606ca7ec503006d37f75cf800575b46b26cb1ef6
trg <trgangwer@gmail.com>
2007-11-11 22:30:58 +0000

    needs to specify badgeid=NULL

--- Install/Upgrade_dbase/11_perm_bug.sql
+++ Install/Upgrade_dbase/11_perm_bug.sql
@@ -1,2 +1,2 @@
-insert into Permissions set permatomid=18, phaseid=6, permroleid=3; 
-insert into Permissions set permatomid=18, phaseid=6, permroleid=1; 
+insert into Permissions set permatomid=18, phaseid=6, permroleid=3, badgeid=NULL; 
+insert into Permissions set permatomid=18, phaseid=6, permroleid=1, badgeid=NULL; 
