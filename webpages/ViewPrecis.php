ba930d97e4f2ae8d4ea674af94344d38bebfc7d6
trg <trgangwer@gmail.com>
2007-11-11 01:13:48 +0000

    Refactored the ViewPrecis to use retrieve_from_dbase function Required addition of another variable and conversion to "in (list)"

--- webpages/ViewPrecis.php
+++ webpages/ViewPrecis.php
@@ -4,6 +4,7 @@
     require_once('StaffHeader.php');
     require_once('StaffFooter.php');
     require_once ('render_functions.php');
+    require_once ('retrieve.php');
     $title=CON_NAME . " - Precis";
     $showlinks=$_GET["showlinks"];
     $_SESSION['return_to_page']="ViewPrecis.php?showlinks=$showlinks";
@@ -13,37 +14,18 @@
     elseif ($showlinks="0") {
             $showlinks=false;
             }
-    if (prepare_db()===false) {
-        $message="Error connecting to database.";
-        RenderError($title,$message);
-        exit ();
-        }
-   $query = <<<EOD
-SELECT sessionid, trackname, typename, title, 
-       concat( if(left(duration,2)=00, '', 
-               if(left(duration,1)=0, concat(right(left(duration,2),1),'hr '), concat(left(duration,2),'hr '))),
-               if(date_format(duration,'%i')=00, '', 
-               if(left(date_format(duration,'%i'),1)=0, concat(right(date_format(duration,'%i'),1),'min'), 
-                  concat(date_format(duration,'%i'),'min')))) Duration,
-       estatten, progguiddesc, persppartinfo
-  from Sessions, Tracks, Types, SessionStatuses 
- where Sessions.trackid=Tracks.trackid  
-   and Sessions.typeid=Types.typeid  
-   and SessionStatuses.statusid=Sessions.statusid  
-   and SessionStatuses.statusname in ('Brainstorm','Vetted')
-   and Sessions.invitedguest=0
- order by trackname, title
-EOD;
-    if (($result=mysql_query($query,$link))===false) {
-        $message="Error retrieving data from database.";
-        RenderError($title,$message);
-        exit ();
-        }
-    staff_header($title);
-    echo "<p> If you have any questions please contact ";
-    echo "<a href=\"mailto:".PROGRAM_EMAIL."\">".PROGRAM_EMAIL."</a> </p>\n";
-    RenderPrecis($result,$showlinks);
-    staff_footer();
-    exit();
+   $statusname="'Brainstorm','EditMe','Vetted'";
+   $type=0;
+   $track=0;
+   $status=0;
+    if (retrieve_select_from_db($track,$status,$statusname,$type)==0) {
+       staff_header($title);
+       echo "<p> If you have any questions please contact ";
+       echo "<a href=\"mailto:".PROGRAM_EMAIL."\">".PROGRAM_EMAIL."</a> </p>\n";
+       RenderPrecis($result,$showlinks);
+       staff_footer();
+       exit();
+       }
+    $message_error="Error retrieving from database. ".$message2;
+    RenderError($title,$message_error);
 ?> 
-
