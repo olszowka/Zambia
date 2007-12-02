f9e5dcfb605cfd297f4db5c841542fcf1cd16f03
trg <trg@676fd721-d978-42df-8417-bacc17f9f818>
2007-12-02 17:03:09 +0000

    fixed typos from last night.

--- trunk/webpages/reportqueries/genindex.sh
+++ trunk/webpages/reportqueries/genindex.sh
@@ -13,19 +13,21 @@ cat genreportheader.php | sed "s/REPORT_TITLE/Available Reports/" | \
 
 echo '<DL>' >> $DEST
 
+CSVONLY=0
+GENCSV=0
 for x in ${SRCDIR}/*query ; do
   name=`echo $x | sed "s%${SRCDIR}/%%" | sed "s/query$//"`
   eval `cat $x`
 
 
-  echo '<DT> <a href="'${name}report.php'">'$TITLE'</a></DT><DD>'$DESCRIPTION'</DD>' >> $DEST
-  case "$x" in 
-     *4*)
-         echo '<DT> <a href="'${name}report.csv'">'$TITLE - csv'</a></DT><DD>'$DESCRIPTION in csv format'</DD>' >> $DEST
-     ;;
-  esac
+  if [ $CSVONLY = 0 ] ; then 
+    echo '<DT> <a href="'${name}report.php'">'$TITLE'</a></DT><DD>'$DESCRIPTION'</DD>' >> $DEST
+  fi
+  if [ $GENCSV = 1 ] ; then 
+       echo '<DT> <a href="'${name}report.csv'">'$TITLE - csv'</a></DT><DD>'$DESCRIPTION in csv format'</DD>' >> $DEST
+  fi
 
-  DESCRIPTION="" ; QUERY="" ; TITLE="" # zero out before looping
+  CVSONLY=0; GENCVS=0 ; DESCRIPTION="" ; QUERY="" ; TITLE="" # zero out before looping
 done
 
   
