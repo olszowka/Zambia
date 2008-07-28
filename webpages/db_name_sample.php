2fca7fd7ea2fe46a983e3011513afa5fc9140ac1
phi <phi@676fd721-d978-42df-8417-bacc17f9f818>
2008-07-28 04:36:12 +0000

    add admin email and give examples for reg and program emails in default config

--- webpages/db_name_sample.php
+++ webpages/db_name_sample.php
@@ -5,9 +5,10 @@ define("DBUSERID","zambiademo");
 define("DBPASSWORD","4fandom");
 define("DBDB","zambiademo");
 define("CON_NAME","Zambia Demo");
+define("ADMIN_EMAIL","zambia@somewhere.net");
 define("BRAINSTORM_EMAIL","brain@somewhere.net");
-define("PROGRAM_EMAIL","nobody@somewhere.net");
-define("REG_EMAIL","noone@somewhere.net");
+define("PROGRAM_EMAIL","program@somewhere.net");
+define("REG_EMAIL","registration@somewhere.net");
 define("stripfancy_from","ÀÁÂÃÄÅÆÇÈÉÊË®");
 define("stripfancy_to","AAAAAAECEEEE ");
 ?>
