fa4704eba43807ea928a36055e4b26c50b3d703a
phi <phi@676fd721-d978-42df-8417-bacc17f9f818>
2008-07-28 17:16:46 +0000

    Split out brainstorm email from suggestion to just mail program about everything

--- webpages/BrainstormFooter.php
+++ webpages/BrainstormFooter.php
@@ -4,8 +4,10 @@
 
 <hr>
 <p> 
-If you would like assistance using this tool or you would like to communicate an idea that you cannot fit into this form, please contact 
-<?php echo "<a href=\"mailto:".PROGRAM_EMAIL."\">".PROGRAM_EMAIL."</a> "?>
+If you would like assistance using this tool, please contact
+<?php echo "<a href=\"mailto:".PROGRAM_EMAIL."\">".PROGRAM_EMAIL."</a> "?>. 
+If you would like to communicate an idea that you cannot fit into this form, please contact 
+<?php echo "<a href=\"mailto:".BRAINSTORM_EMAIL."\">".BRAINSTORM_EMAIL."</a> "?>.
 </body>
 </html>
 
