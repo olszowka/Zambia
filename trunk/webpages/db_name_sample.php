3ce952af07b9b161bf40f80c6b2114c753d0d4f1
olszowka <olszowka@676fd721-d978-42df-8417-bacc17f9f818>
2008-08-31 00:08:51 +0000

    New constants for participant availability change.

--- trunk/webpages/db_name_sample.php
+++ trunk/webpages/db_name_sample.php
@@ -9,6 +9,12 @@ define("ADMIN_EMAIL","zambia@somewhere.net");
 define("BRAINSTORM_EMAIL","brain@somewhere.net");
 define("PROGRAM_EMAIL","program@somewhere.net");
 define("REG_EMAIL","registration@somewhere.net");
+define("CON_NUM_DAYS",5); // code works for 1 - 8
+define("PREF_TTL_SESNS_LMT",10); // Input data verification limit for preferred total number of sessions
+define("PREF_DLY_SESNS_LMT",4); // Input data verification limit for preferred daily limit of sessions
+define("AVAILABILITY_ROWS",12); // Number of rows of availability records to render
+global $daymap;
+$daymap = array (1 => "Thursday", 2 => "Friday", 3 => "Saturday", 4 => "Sunday", 5 => "Monday", 6=> "Tuesday");
 define("stripfancy_from","ÀÁÂÃÄÅÆÇÈÉÊË®");
 define("stripfancy_to","AAAAAAECEEEE ");
 ?>
