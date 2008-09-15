7cddbcdc49b56132cfb43b7fbdd10d3bc736d74d
degu <deguspice@gmail.com>
2008-09-15 01:14:19 +0000

    Fixed a typo

--- webpages/RenderEditCreateSession.php
+++ webpages/RenderEditCreateSession.php
@@ -25,7 +25,7 @@ function RenderEditCreateSession ($action, $session, $message1, $message2) {
       echo "<P id=\"message1\"><font color=red>".$message1."</font></P>\n";
     }
     if (strlen($message2)>0) {
-      echo "<P id=\"message2\"><font color=red".$message2."</font></P>\n";
+      echo "<P id=\"message2\"><font color=red>".$message2."</font></P>\n";
       exit(); // If there is a message2, then there is a fatal error.
     }
     //error_log("Zambia: ".print_r($session,TRUE));
