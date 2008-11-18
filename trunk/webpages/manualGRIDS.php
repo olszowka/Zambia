7dc9194b6ccf3632187949d2581e257798e0840a
olszowka <olszowka@676fd721-d978-42df-8417-bacc17f9f818>
2008-11-18 05:02:43 +0000

    Created grid reports with static php and put created page with links to them.

---
+++ trunk/webpages/manualGRIDS.php
@@ -0,0 +1,20 @@
+<?php
+  $title="GRIDS Reports";
+  require_once('db_functions.php');
+  require_once('StaffHeader.php');
+  require_once('StaffFooter.php');
+  require_once('StaffCommonCode.php');
+  staff_header($title);
+  $_SESSION['return_to_page']="REPORT_LINK";
+?>
+<DL>
+<DT> <a href="eventgridstaticreport.php">Published Event Grid</a></DT>
+<DD>Display published event schedule with rooms on horizontal axis and time on vertical. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
+<DT> <a href="eventgridfullstaticreport.php">Unabridged Event Grid</a></DT>
+<DD>Display event schedule with rooms on horizontal axis and time on vertical. This includes all items regardless of publication status.</DD>
+<DT> <a href="programgridstaticreport.php">Published Programming Grid</a></DT>
+<DD>Display published schedule of programming rooms with rooms on horizontal axis and time on vertical. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
+<DT> <a href="fasttrackgridstaticreport.php">Published Fast Track Grid</a></DT>
+<DD>Display published fast track schedule with rooms on horizontal axis and time on vertical. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
+</DL>
+<?php staff_footer(); ?>
