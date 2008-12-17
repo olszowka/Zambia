d1f9af3bcfd72d3d64160042357495d3b692df7d
degu <degu@676fd721-d978-42df-8417-bacc17f9f818>
2008-12-17 20:21:03 +0000

    Ben: Changed text for everything grids, neated up formatting

--- trunk/webpages/manualGRIDS.php
+++ trunk/webpages/manualGRIDS.php
@@ -9,12 +9,35 @@
 ?>
 <DL>
 <DT> <a href="eventgridstaticreport.php">Published Event Grid</a></DT>
-<DD>Display published event schedule with rooms on horizontal axis and time on vertical. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
+  <DD>Display published event schedule with rooms on horizontal axis and
+  time on vertical. This excludes any item marked "Do Not Print" or
+  "Staff Only".</DD>
+
 <DT> <a href="eventgridfullstaticreport.php">Unabridged Event Grid</a></DT>
-<DD>Display event schedule with rooms on horizontal axis and time on vertical. This includes all items regardless of publication status.</DD>
+  <DD>Display event schedule with rooms on horizontal axis and time on
+  vertical. This includes all items regardless of publication
+  status.</DD>
+
 <DT> <a href="programgridstaticreport.php">Published Programming Grid</a></DT>
-<DD>Display published schedule of programming rooms with rooms on horizontal axis and time on vertical. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
+  <DD>Display published schedule of programming rooms with rooms on
+  horizontal axis and time on vertical. This excludes any item marked
+  "Do Not Print" or "Staff Only".</DD>
+
 <DT> <a href="fasttrackgridstaticreport.php">Published Fast Track Grid</a></DT>
-<DD>Display published fast track schedule with rooms on horizontal axis and time on vertical. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
+  <DD>Display published fast track schedule with rooms on horizontal
+  axis and time on vertical. This excludes any item marked "Do Not
+  Print" or "Staff Only".</DD>
+
+<DT> <a href="staffpubgridstaticreport.php">Published Grid</a></DT>
+  <DD>Display published schedule with rooms on horizontal axis and
+  time on vertical. This excludes any item marked "Do Not Print" or
+  "Staff Only".</DD>
+
+<DT> <a href="staffallgridstaticreport.php">Everything Grid</a></DT>
+  <DD>Display the entire schedule with rooms on horizontal axis and
+  time on vertical. This includes all items regardless of publication
+  status.</DD>
+
+
 </DL>
 <?php staff_footer(); ?>
