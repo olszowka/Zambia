74c9a04c212ee58dbe84ad6574599d2191471f61
olszowka <olszowka@users.noreply.github.com>
2009-01-25 22:39:07 +0000

    Ben added script to support Google Analytics

--- webpages/ParticipantFooter.php
+++ webpages/ParticipantFooter.php
@@ -9,6 +9,20 @@ If you need help or to tell us something that doesn't fit here, please email <a 
 // This line is for debugging only
 // echo "<PRE>".print_r($_SESSION['permission_set_specific'])."</PRE><BR>";
 ?>
+
+<!---  Google Analytics Tracking Report, see Ben for details --->
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-1841041-6");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+<!--- End of Google Analytics --->
+
 </body>
 </html>
 
