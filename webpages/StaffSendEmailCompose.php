a847d8812cc65faee5d3b2b991211978ceb46563
olszowka <olszowka@users.noreply.github.com>
2009-01-29 00:15:16 +0000

    Functional but barebones bulk email

--- webpages/StaffSendEmailCompose.php
+++ webpages/StaffSendEmailCompose.php
@@ -1,7 +1,14 @@
 <?php
+// This page has two completely different entry points from a user flow standpoint:
+//   1) Beginning of send email flow -- start to specify parameters
+//   2) After verify -- 'back' can change parameters -- 'send' fire off email sending code
 require_once('email_functions.php');
 require_once('db_functions.php');
+require_once('render_functions.php');
 require_once('StaffCommonCode.php'); //reset connection to db and check if logged in
+require_once "Swift/Swift.php";
+require_once "Swift/Swift/Connection/SMTP.php";
+global $title, $message, $link;
 if (isset($_POST['sendto'])) { // page has been visited before
 // restore previous values to form
         $email=get_email_from_post();
@@ -9,5 +16,66 @@ if (isset($_POST['sendto'])) { // page has been visited before
     else { // page hasn't just been visited
         $email=set_email_defaults();
         }
-render_send_email($email);
+$message_warning="";
+if ($_POST['navigate']!='send') {
+    render_send_email($email,$message_warning);
+    exit(0);
+    }
+// put code to send email here.
+// render_send_email_engine($email,$message_warning);
+$title="Staff Send Email";
+$subst_list=array("\$BADGEID\$","\$FIRSTNAME\$","\$LASTNAME\$","\$EMAILADDR\$","\$PUBNAME\$","\$BADGENAME\$");
+$email=get_email_from_post();
+$swift =& new Swift(new Swift_Connection_SMTP(SMTP_ADDRESS)); // Is machine name of SMTP host defined in db_name.php
+$log =& Swift_LogContainer::getLog();
+$log->setLogLevel(0); // 0 is minimum logging; 4 is maximum logging
+$query="SELECT emailtoquery FROM EmailTo where emailtoid=".$email['sendto'];
+if (!$result=mysql_query($query,$link)) {
+    db_error($title,$query,$staff=true); // outputs messages regarding db error
+    exit(0);
+    }
+$emailto=mysql_fetch_array($result,MYSQL_ASSOC);
+$query=$emailto['emailtoquery'];
+if (!$result=mysql_query($query,$link)) {
+    db_error($title,$query,$staff=true); // outputs messages regarding db error
+    exit(0);
+    }
+$i=0;
+while ($recipientinfo[$i]=mysql_fetch_array($result,MYSQL_ASSOC)) {
+    $i++;
+    }
+$recipient_count=$i;
+$query="SELECT emailfromaddress FROM EmailFrom where emailfromid=".$email['sendfrom'];
+if (!$result=mysql_query($query,$link)) {
+    db_error($title,$query,$staff=true); // outputs messages regarding db error
+    exit(0);
+    }
+$emailfrom=mysql_result($result,0);
+$x=$email['sendcc'];
+$query="SELECT emailaddress FROM EmailCC where emailccid=$x";
+if (!$result=mysql_query($query,$link)) {
+    db_error($title,$query,$staff=true); // outputs messages regarding db error
+    exit(0);
+    }
+$emailcc=mysql_result($result,0);
+for ($i=0; $i<$recipient_count; $i++) {
+    $repl_list=array($recipientinfo[$i]['badgeid'],$recipientinfo[$i]['firstname'],$recipientinfo[$i]['lastname']);
+    $repl_list=array_merge($repl_list,array($recipientinfo[$i]['email'],$recipientinfo[$i]['pubsname'],$recipientinfo[$i]['badgename']));
+    $emailverify['body']=str_replace($subst_list,$repl_list,$email['body']);
+    $message =& new Swift_Message($email['subject'],$emailverify['body']);
+    echo ($recipientinfo[$i]['pubsname']." - ".$recipientinfo[$i]['email'].": ");
+    $recipients =& new Swift_RecipientList();
+    $recipients->addTo($recipientinfo[$i]['email']); // define the To: field
+    if ($emailcc!="") {
+        $recipients->addBcc($emailcc); // define the BCC: field
+        }
+    if ($swift->send($message, $recipients, $emailfrom)) {
+            echo "Sent<BR>";
+            }
+        else {
+            echo "Failed<BR>";
+            }
+    }
+//$log =& Swift_LogContainer::getLog();
+//echo $log->dump(true);
 ?>
