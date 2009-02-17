01c6a06dbc27bfef98ed1f59a0550d1c4813b2f7
polszowka <polszowka@676fd721-d978-42df-8417-bacc17f9f818>
2009-02-17 06:02:39 +0000

    Just modified a comment to test commit process

--- trunk/webpages/db_name_sample.php
+++ trunk/webpages/db_name_sample.php
@@ -11,7 +11,7 @@ define("PROGRAM_EMAIL","program@somewhere.net");
 define("REG_EMAIL","registration@somewhere.net");
 define("CON_NUM_DAYS",5); // code works for 1 - 8
 define("CON_START_DATIM","2009-08-06 00:00:00"); // Intended for use by report scripts; currently used by static
-        // grids only
+        // grids only (just a checkin test)
 define("SMTP_ADDRESS","smtp-out.netbusters.com");
 define("PREF_TTL_SESNS_LMT",10); // Input data verification limit for preferred total number of sessions
 define("PREF_DLY_SESNS_LMT",5); // Input data verification limit for preferred daily limit of sessions
