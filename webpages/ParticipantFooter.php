a7e0de553b96244a080582edc5f685c54b824931
polszowka <olszowka@users.noreply.github.com>
2009-04-07 01:23:38 +0000

    Implement mechanism to make google analytics more easily configured.

--- webpages/ParticipantFooter.php
+++ webpages/ParticipantFooter.php
@@ -4,25 +4,12 @@
 
 <hr>
 <p> 
-If you need help or to tell us something that doesn't fit here, please email <a href="mailto: <?php echo PROGRAM_EMAIL."\">".PROGRAM_EMAIL ?></a>.
+If you need help or to tell us something that doesn't fit here, please email
 <?php
-// This line is for debugging only
-// echo "<PRE>".print_r($_SESSION['permission_set_specific'])."</PRE><BR>";
+   $x=PROGRAM_EMAIL;
+   echo "<A HREF=\"mailto:$x\">$x</A>.\n"; 
+   include('google_analytics.php');
 ?>
-
-<!---  Google Analytics Tracking Report, see Ben for details --->
-
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-1841041-6");
-pageTracker._initData();
-pageTracker._trackPageview();
-</script>
-<!--- End of Google Analytics --->
-
 </body>
 </html>
 
