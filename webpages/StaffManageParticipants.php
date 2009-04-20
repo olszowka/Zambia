c45639dd87aa90291c24410e3303fe9abc1fbde3
polszowka <olszowka@users.noreply.github.com>
2009-04-20 01:48:08 +0000

    2 separate changes: Create set of pages for manual entry of Participants. Create admin report section and report in it for tracking who has staff privs.

--- webpages/StaffManageParticipants.php
+++ webpages/StaffManageParticipants.php
@@ -13,6 +13,10 @@ On this page you will find the online tools for managing Participants.</p>
 <DL>
    <DT id="managebios"><A HREF="StaffManageBios.php">Manage biographies.</A></DT>
    <DD id="managebios">Manage and edit participants' biographies.</DD>
+<?php if(may_I('create_participant')) { ?>
+   <DT><A HREF="StaffEditCreateParticipants.php?action=create">Enter Participants</A></DT> 
+   <DD>Manually create new participants in the database and enter their data</DD>
+<?php } ?>
    <DT><A HREF="AdminParticipants.php">Administer participants</A></DT>
    <DD>Use this tool to modify a participant's "interested" flag, change his password, or delete him from all sessions.</DD>
    <DT><A HREF="InviteParticipants.php">Invite a participant to a session</A></DT>
