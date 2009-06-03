fc87f7fad48e2680d1d241c5abab98b7865b0611
hbalen <hbalen@676fd721-d978-42df-8417-bacc17f9f818>
2009-06-03 01:40:07 +0000

    tracks needed for anticipation that were not covered already

---
+++ branches/anticipation/Install/Upgrade_dbase/27_add_tracks.sql
@@ -0,0 +1,9 @@
+INSERT INTO Tracks
+	(trackid, trackname, display_order, selfselect)
+	VALUES
+	(19, "Quizzes, panel games et cetera", 19, 1),
+	(20, "Kaffeeklatsch", 20, 1),
+	(21, "Reading", 21, 1);
+	
+INSERT INTO PatchLog (patchname) VALUES ('27_add_tracks.sql');
+	
