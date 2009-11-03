bfde17fe7b33b57ef63d233c0008c6dea1ef97a7
polszowka <olszowka@users.noreply.github.com>
2009-11-03 00:05:42 +0000

    Fix bug which messed up updating of participant panel ranks and related data.

--- webpages/PartPanelInterests_Render.php
+++ webpages/PartPanelInterests_Render.php
@@ -27,27 +27,28 @@ function render_session_interests($badgid,$session_interest_count,$message,$mess
     echo "<P>You are limited to 4 sessions each of preferences 1-4.  There is no limit to the number of sessions for which you can express preference 5.</P>\n";
     echo "<H3>List of Sessions in Which I'm Interested in Participating</H3>\n";
     echo "<TABLE>\n";
+	$j=1; //use $j so that skipped sessions don't skip numbering
     for ($i=1; $i<=$session_interest_count; $i++) {
         if (!$session_interests[$i]['title']) continue;
         echo "    <TR>\n";
         echo "        <TD rowspan=5 class=\"border0000 hilit\" id=\"sessidtcell\">{$session_interests[$i]['sessionid']}";
-        echo "            <INPUT type=\"hidden\" name=\"sessionid$i\" value=\"{$session_interests[$i]['sessionid']}\"></TD>\n";
+        echo "            <INPUT type=\"hidden\" name=\"sessionid$j\" value=\"{$session_interests[$i]['sessionid']}\"></TD>\n";
         echo "        <TD class=\"border0000 hilit vatop\">{$session_interests[$i]['trackname']}</TD>\n";
         echo "        <TD colspan=2 class=\"border0000 hilit vatop\">".htmlspecialchars($session_interests[$i]['title'],ENT_NOQUOTES)."</TD>\n";
         echo "        <TD class=\"border0000 hilit vatop\">Duration: {$session_interests[$i]['duration']}</TD>\n";
         echo "        </TR>\n";
         echo "    <TR>\n";
-        echo "        <TD class=\"border0000 usrinp\">Rank: <INPUT type=\"text\" size=3 name=\"rank$i\" value=\"{$session_interests[$i]['rank']}\"></TD>\n";
-        echo "        <TD class=\"border0000 usrinp\">I'd like to moderate this panel:<INPUT type=\"checkbox\" value=1 name=\"mod$i\" ";
+        echo "        <TD class=\"border0000 usrinp\">Rank: <INPUT type=\"text\" size=3 name=\"rank$j\" value=\"{$session_interests[$i]['rank']}\"></TD>\n";
+        echo "        <TD class=\"border0000 usrinp\">I'd like to moderate this panel:<INPUT type=\"checkbox\" value=1 name=\"mod$j\" ";
         echo "            ".(($session_interests[$i]['willmoderate'])?"checked":"")."></TD>\n";
-        echo "        <TD colspan=2 class=\"border0000 usrinp\">Remove this panel from my list:<INPUT type=\"checkbox\" value=1 name=\"delete$i\"></TD>\n";
+        echo "        <TD colspan=2 class=\"border0000 usrinp\">Remove this panel from my list:<INPUT type=\"checkbox\" value=1 name=\"delete$j\"></TD>\n";
         echo "        </TR>\n";
         echo "    <TR>\n";
         echo "        <TD  class=\"border0000 usrinp\" colspan=4>My notes regarding this panel for Programming and other panel participants: ";
-        echo "            <TEXTAREA height=5em cols=80 name=\"comments$i\" id=\"intCmnt\">". htmlspecialchars( $session_interests[$i]['comments'],ENT_COMPAT)."</TEXTAREA></TD>\n";
+        echo "            <TEXTAREA height=5em cols=80 name=\"comments$j\" id=\"intCmnt\">". htmlspecialchars( $session_interests[$i]['comments'],ENT_COMPAT)."</TEXTAREA></TD>\n";
         echo "        </TR>\n";
         echo "    <TR>\n";
-        echo "        <TD colspan=4 class=\"border0010\">".htmlspecialchars($session_interests[$i]['progguiddesc'],ENT_NOQUOTES)."</TD>\n";
+        echo "        <TD colspan=4 class=\"border0010\">".htmlspecialchars($session_interests[$j]['progguiddesc'],ENT_NOQUOTES)."</TD>\n";
         echo "        </TR>\n";
         echo "    <TR>\n";
         echo "        <TD colspan=4 class=\"border0000\">".htmlspecialchars($session_interests[$i]['persppartinfo'],ENT_NOQUOTES)."</TD>\n";
@@ -58,6 +59,7 @@ function render_session_interests($badgid,$session_interest_count,$message,$mess
         echo "    <TR>\n";
         echo "        <TD colspan=6 class=\"border0000\" id=\"smallspacer\">&nbsp;</TD>\n";
         echo "        </TR>\n";
+		$j++;
         }
     echo "    </TABLE>\n";
     echo "<DIV class=\"submit\" id=\"submit2\"><BUTTON class=\"SubmitButton\" type=\"submit\" name=\"submitranks\">Save</BUTTON></DIV>\n";
