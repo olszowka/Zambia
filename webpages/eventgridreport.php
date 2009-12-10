b95083299e31de19c5dd286b8d66360885bb3162
polszowka <olszowka@users.noreply.github.com>
2009-12-10 03:47:58 +0000

    Fix return to link.

--- webpages/eventgridreport.php
+++ webpages/eventgridreport.php
@@ -6,7 +6,7 @@
     require_once('StaffCommonCode.php');
     global $link;
     $ConStartDatim=CON_START_DATIM; // make it a variable so it can be substituted
-    $_SESSION['return_to_page']="eventgridstaticreport.php";
+    $_SESSION['return_to_page']="eventgridreport.php";
 
     function topofpage() {
         staff_header($title);
