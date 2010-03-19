3d5b2681db5d7d036ec561d7d7c71db06150e6b7
mrdkap <mrdkap@676fd721-d978-42df-8417-bacc17f9f818>
2010-03-19 20:46:44 +0000

    Updated allpartschedbyparttime (all participants schedule by participant, then by time) to be on demand.

---
+++ branches/FFF/webpages/allpartschedbyparttimereport.php
@@ -0,0 +1,86 @@
+<?php
+    $title="";
+    require_once('db_functions.php');
+    require_once('StaffHeader.php');
+    require_once('StaffFooter.php');
+    require_once('StaffCommonCode.php');
+    global $link;
+    $ConStartDatim=CON_START_DATIM; // make it a variable so it can be substituted
+    $_SESSION['return_to_page']="allpartschedbyparttimereport.php";
+
+    function topofpage() {
+        staff_header("Full Participant Schedule by time");
+        date_default_timezone_set('US/Eastern');
+        echo "<P align=center> Generated: ".date("D M j G:i:s T Y")."</P>\n";
+        echo "<P>The schedule sorted by participant.</P>\n";
+        }
+
+    function noresults() {
+        echo "<P>This report retrieved no results matching the criteria.</P>\n";
+        staff_footer();
+        }
+
+    $query = <<<EOD
+SELECT 
+    if ((P.pubsname is NULL), ' ', concat(' ',P.pubsname,' (',P.badgeid,')')) as 'Participants', 
+    if ((moderator=1),'moderator', ' ') as Moderator,
+    DATE_FORMAT(ADDTIME('2009-01-16 00:00:00',starttime),'%a %l:%i %p') as 'Start Time', 
+      concat(if(left(duration,2)=00, '', if(left(duration,1)=0, 
+      concat(right(left(duration,2),1),'hr '), concat(left(duration,2),'hr '))), 
+      if(date_format(duration,'%i')=00, '', if(left(date_format(duration,'%i'),1)=0, 
+      concat(right(date_format(duration,'%i'),1),'min'), 
+      concat(date_format(duration,'%i'),'min')))) Duration,
+    concat('<a href=MaintainRoomSched.php?selroom=',R.roomid,'>', R.roomname,'</a>') as Roomname,
+    Function, 
+    Trackname,
+    concat('<a href=StaffAssignParticipants.php?selsess=',S.sessionid,'>', S.sessionid,'</a>') as Sessionid,
+    concat('<a href=EditSession.php?id=',S.sessionid,'>',S.title,'</a>') Title
+  FROM 
+      Sessions S
+    JOIN Schedule SCH USING (sessionid)
+    JOIN Rooms R USING (roomid)
+    LEFT JOIN ParticipantOnSession POS ON SCH.sessionid=POS.sessionid 
+    LEFT JOIN Participants P ON POS.badgeid=P.badgeid 
+    LEFT JOIN Tracks T ON T.trackid=S.trackid 
+  WHERE
+    S.typeid not in (10, 12)
+  ORDER BY
+    cast(P.badgeid as unsigned),
+    SCH.starttime
+EOD;
+    if (($result=mysql_query($query,$link))===false) {
+        $message="Error retrieving data from database.<BR>";
+        $message.=$query;
+        RenderError($title,$message);
+        exit ();
+        }
+    if (0==($rows=mysql_num_rows($result))) {
+        topofpage();
+        noresults();
+        exit();
+        }
+    for ($i=1; $i<=$rows; $i++) {
+        $class_array[$i]=mysql_fetch_assoc($result);
+        }
+    $header_array=array_keys($class_array[1]);
+    $columns=count($header_array);
+    $headers="";
+    foreach ($header_array as $header_name) {
+      $headers.="<TH>";
+      $headers.=$header_name;
+      $headers.="</TH>\n";
+      }
+    topofpage();
+    echo "<TABLE BORDER=1>";
+    echo "<TR>" . $headers . "</TR>";
+    for ($i=1; $i<=$rows; $i++) {
+        echo "<TR>";
+        foreach ($header_array as $header_name) {
+            echo "<TD>";
+            echo $class_array[$i][$header_name];
+            echo "</TD>\n";
+	    }
+        echo "</TR>\n";
+        }
+    echo "</TABLE>";
+    staff_footer();
