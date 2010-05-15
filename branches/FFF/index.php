696a5773b8cf83d771a89e344f830cde8a26cc30
mrdkap <mrdkap@676fd721-d978-42df-8417-bacc17f9f818>
2010-05-15 04:10:55 +0000

    Added an index page, so people can't just randomly surf the directories.

---
+++ branches/FFF/index.php
@@ -0,0 +1,43 @@
+<?php
+require_once ('Local/db_name.php');
+$ConName=CON_NAME; // make it a variable so it can be substituted
+$ProgramEmail=PROGRAM_EMAIL; // make it a variable so it can be substituted
+$HeaderTemplateFile="Local/HeaderTemplate.html";
+$FooterTemplateFile="Local/FooterTemplate.html";
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<head>
+  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=latin-1\">
+<?php
+echo "  <title>Zambia -- $ConName</title>\n";
+if (file_exists($HeaderTemplateFile)) {
+  readfile($HeaderTemplateFile);
+ } else {
+?>
+  <link rel="stylesheet" href="Common.css" type="text/css">
+</head>
+<body>
+<?php } 
+echo "<H1>$ConName Programming</H1>\n<hr>\n";
+echo "<H2>Check out the below links to learn about the great programming we will have at $ConName!</H2>\n";
+?>
+<UL>
+  <LI><A HREF="webpages/Postgrid.php">Schedule Grid</A></LI>
+  <LI><A HREF="webpages/Descriptions.php">Class Descriptions</A></LI>
+  <LI><A HREF="webpages/Schedule.php">Schedule</A></LI>
+  <LI><A HREF="webpages/Bios.php">Presenter Bios</A></LI>
+  <LI><A HREF="webpages/">Presenter Login</A></LI>
+</UL>
+<?php
+if (file_exists($FooterTemplateFile)) {
+  readfile($FooterTemplateFile);
+ } else {
+  echo "<hr>\n<P>If you have questions or wish to communicate an idea, please contact ";
+  echo "<A HREF=\"mailto:$ProgramEmail\">$ProgramEmail</A>.\n</P>";
+ }
+include ('webpages/google_analytics.php');
+?>
+</body>
+</html>
