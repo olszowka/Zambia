6fcb95ee6aacac894c24181b096c63c1014236c9
mrdkap <mrdkap@676fd721-d978-42df-8417-bacc17f9f818>
2010-05-16 07:04:15 +0000

    Put all the grids in the grids.php file, to simplify, and modified the manualGRIDS.php file to work the variables available.  Still manual, because I create the arrays by hand in the file, although they are easily editable.  Need some way to include the grids in the flow database, and that could possibly replace the manualGRIDS.php file, but I don't yet see a clear path there.  The grids are also now dotted so the non-colourized ones still make sense.

--- branches/FFF/webpages/manualGRIDS.php
+++ branches/FFF/webpages/manualGRIDS.php
@@ -1,43 +1,52 @@
 <?php
-  require_once('StaffCommonCode.php');
-  $title="Grids";
-  staff_header($title);
-  $_SESSION['return_to_page']="genindex.php";
+require_once('StaffCommonCode.php');
+global $link;
+$_SESSION['return_to_page']="genindex.php";
+$title="All Grids";
+$description="<P>All the grids are listed below, in the grid.</P>\n";
+$additionalinfo="<P>The type of grid is listed as the headers and the area of interest it pertains to is listed down the side.\n";
+$additionalinfo.="The choice of color or not is inside each grid element.</P>\n";
+$additionalinfo.="<P>Also useful is the <A HREF=StaffBios.php>Bios for Presenters</A> (<A HREF=Bios.php>public version</A>),\n";
+$additionalinfo.="the <A HREF=StaffDescriptions.php>Descriptions of scheduled Precis</A> (<A HREF=Descriptions.php>public version</A>),\n";
+$additionalinfo.="the <A HREF=StaffPostgrid.php>Grid</A>, (<A HREF=StaffPostvolgrid.php>with volunteer</A> or <A HREF=Postgrid.php>public version</A>),\n";
+$additionalinfo.="and the <A HREF=StaffSchedule.php>Scheduled Precis</A> (<A HREF=Schedule.php>public version</A>) in time order.</P>\n";
+
+## Replacement for the query
+$how_array['Description']="";
+$how_array['Start Time']="starttime=y";
+$how_array['Start Time<br>Unabridged']="starttime=y&unpublished=y";
+$how_array['Start Time<br>Staff Only']="starttime=y&staffonly=y";
+$how_array['Time Filled']="timefilled=y";
+$how_array['Time Filled<br>Unabridged']="timefilled=y&unpublished=y";
+$how_array['Time Filled<br>Staff Only']="timefilled=y&staffonly=y";
+$how_array['Time Semi-filled']="standard=y";
+$how_array['Time Semi-filled<br>Unabridged']="unpublished=y";
+$how_array['Time Semi-filled<br>Staff Only']="staffonly=y";
+
+$type_array['Complete']="standard=y&";
+$type_array['Fast Track']="fasttrack=y&";
+$type_array['Event']="events=y&";
+$type_array['GoH']="goh=y&";
+$type_array['Programming']="programming=y&";
+
+//build the returned array
+$header_array=array_keys($how_array);
+$body_array=array_keys($type_array);
+$rows=0;
+foreach ($body_array as $y_element) {
+  $rows++;
+  foreach ($header_array as $x_element) {
+    if ($x_element == "Description") {
+      $grid_array[$rows]["$x_element"]="<B>".$y_element."</B>";
+    } else {
+      $grid_array[$rows]["$x_element"]="<A HREF=grid.php?".$type_array["$y_element"].$how_array["$x_element"].">Color</A> / \n";
+      $grid_array[$rows]["$x_element"].="<A HREF=grid.php?".$type_array["$y_element"].$how_array["$x_element"]."&nocolor=y>No Color</A>\n";
+    }
+  }
+}
+
+## Page Rendering
+topofpagereport($title,$description,$additionalinfo);
+renderhtmlreport($rows,$header_array,$grid_array);
+
 ?>
-<p align=center> Generated: Tue Feb 16 17:07:42 EST 2010
-<P>For report changes email zambia@arisia.org.
-<DL>
-<DT> <a href="StaffBios.php">Bios for Presenters</a> (<a href="Bios.php">public version</a>)</DT><DD>For the public, with links between the bios, their classes, the schedule and a link to the grid.</DD>
-<DT> <a href="StaffDescriptions.php">Descriptions Page</a> (<a href="Descriptions.php">public version</a>)</DT><DD>For the public, with links between the descriptons, the bios of the presenters, the schedule, and a link to the grid.</DD>
-<DT> <a href="StaffSchedule.php">Schedule Listings</a> (<a href="Schedule.php">public version</a>)</DT><DD>For the public, with links between the schedule elements to their descriptions, the bios of the presenters, and a link to the grid.</DD>
-<DT> <a href="StaffPostgrid.php">Postable Grid</a> (<a href="Postgrid.php">public version</a>)</DT><DD>For the public, with links between the classes, the schedule, their descriptions and the bios of the presenters.</DD>
-<DT> <a href="StaffPostvolgrid.php">Postable Volunteer Grid</a></DT><DD>For the volunteer coordinators, with links between the classes, the schedule, and their descriptions. Volunteer and Announcer listed.</DD>
-<DT> <a href="completepubgridcolor.php">Published Color Grid</a></DT><DD>Display published schedule with rooms on horizontal axis and time on vertical, keyed by color. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="completepubgrid.php">Published Grid</a></DT><DD>Display published schedule with rooms on horizontal axis and time on vertical. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="completepubgridtimecolor.php">Published Time Filled Color Grid</a></DT><DD>Display published schedule with rooms on horizontal axis and regular time on vertical, keyed by color. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="completepubgridtime.php">Published Time Filled Grid</a></DT><DD>Display published schedule with rooms on horizontal axis and regular time on vertical. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="completepubgridtimeunfilledcolor.php">Published Time Semi-filled Color Grid</a></DT><DD>Display published schedule with rooms on horizontal axis and regular time on vertical, keyed by color (only). This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="eventgridcolor.php">Published Color Event Grid</a></DT><DD>Display published event schedule with rooms on horizontal axis and time on vertical, keyed by color. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="eventgridfullcolor.php">Unabridged Color Event Grid</a></DT><DD>Display entire event schedule with rooms on horizontal axis and time on vertical, keyed by color. This includes all items marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="eventgridfull.php">Unabridged Event Grid</a></DT><DD>Display entire event schedule with rooms on horizontal axis and time on vertical. This includes all items marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="eventgridfulltimecolor.php">Unabridged Time Filled Color Event Grid</a></DT><DD>Display entire event schedule with rooms on horizontal axis and regular time on vertical, keyed by color. This includes all items marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="eventgridfulltime.php">Unabridged Time Filled Event Grid</a></DT><DD>Display entire event schedule with rooms on horizontal axis and regular time on vertical. This includes all items marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="eventgridfulltimeunfilledcolor.php">Unabridged Time Semi-filled Color Event Grid</a></DT><DD>Display entire event schedule with rooms on horizontal axis and regular time on vertical, keyed by color (only). This includes all items marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="eventgrid.php">Published Event Grid</a></DT><DD>Display published event schedule with rooms on horizontal axis and time on vertical. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="eventgridtimecolor.php">Published Time Filled Color Event Grid</a></DT><DD>Display published event schedule with rooms on horizontal axis and regular time on vertical, keyed by color. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="eventgridtime.php">Published Time Filled Event Grid</a></DT><DD>Display published event schedule with rooms on horizontal axis and regular time on vertical. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="eventgridtimeunfilledcolor.php">Published Time Semi-filled Color Event Grid</a></DT><DD>Display published event schedule with rooms on horizontal axis and regular time on vertical, keyed by color (only). This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="everythinggridcolor.php">Unabridged Color Complete Grid</a></DT><DD>Display complete schedule of all rooms with rooms on horizontal axis and time on vertical, keyed by color. This includes all items marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="everythinggrid.php">Unabridged Complete Grid</a></DT><DD>Display complete schedule of all rooms with rooms on horizontal axis and time on vertical. This includes all items marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="everythinggridtimecolor.php">Unabridged Time Filled Color Complete Grid</a></DT><DD>Display complete schedule of all rooms with rooms on horizontal axis and regular time on vertical, keyed by color. This includes all items marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="everythinggridtime.php">Unabridged Time Filled Complete Grid</a></DT><DD>Display complete schedule of all rooms with rooms on horizontal axis and regular time on vertical. This includes all items marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="everythinggridtimeunfilledcolor.php">Unabridged Time Semi-filled Color Complete Grid</a></DT><DD>Display complete schedule of all rooms with rooms on horizontal axis and regular time on vertical, keyed by color (only). This includes all items marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="gohgrid.php">GOH Grid</a></DT><DD>Display unabridged schedule of all events with any GOHs participanting with rooms on horizontal axis and time on vertical. This includes all items marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="programgridcolor.php">Published Color Programming Grid</a></DT><DD>Display published schedule of programming rooms with rooms on horizontal axis and time on vertical, keyed by color. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="programgrid.php">Published Programming Grid</a></DT><DD>Display published schedule of programming rooms with rooms on horizontal axis and time on vertical. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="programgridtimecolor.php">Published Time Filled Color Programming Grid</a></DT><DD>Display published schedule of programming rooms with rooms on horizontal axis and regular time on vertical, keyed by color. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="programgridtime.php">Published Time Filled Programming Grid</a></DT><DD>Display published schedule of programming rooms with rooms on horizontal axis and regular time on vertical. This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="programgridtimeunfilledcolor.php">Published Time Semi-filled Color Programming Grid</a></DT><DD>Display published schedule of programming rooms with rooms on horizontal axis and regular time on vertical, keyed by color (only). This excludes any item marked "Do Not Print" or "Staff Only".</DD>
-<DT> <a href="staffonlygrid.php">Staff Only Grid</a></DT><DD>Display only the items that are "Staff-Only" or "Do-Not-Publish" with rooms on horizontal axis and time on vertical.</DD>
-</DL>
-<?php staff_footer(); ?>
