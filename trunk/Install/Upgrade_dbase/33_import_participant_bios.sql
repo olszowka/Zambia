2dbc29a1ff22ba9481b09cc299717b2ad79f1834
polszowka <polszowka@676fd721-d978-42df-8417-bacc17f9f818>
2010-08-01 17:03:34 +0000

    Add table to facilitate import of participant bios from previous cons.

---
+++ trunk/Install/Upgrade_dbase/33_import_participant_bios.sql
@@ -0,0 +1,8 @@
+## This script creates 1 new table for capture of participant data from previous cons and importing into current con.
+CREATE TABLE PreviousParticipants (
+    badgeid varchar(15) NOT NULL,
+    bio text,
+    PRIMARY KEY (badgeid`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO PatchLog (patchname) VALUES ('33_import_participant_bios.sql');
+
