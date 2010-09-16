b79dec95433b0c017bbc06c7ed2859ddbe492bf8
polszowka <polszowka@676fd721-d978-42df-8417-bacc17f9f818>
2010-09-16 16:22:38 +0000

    Make share email default to "yes".

--- trunk/Install/Upgrade_dbase/34_share_email.sql
+++ trunk/Install/Upgrade_dbase/34_share_email.sql
@@ -1,4 +1,4 @@
 ## This script adds support to track whether participants give permission to share their email address with other participants.
 ## It adds a column to Participants.
-Alter Table Participants Add Column `share_email` tinyint(11) Default Null After pubsname;
+Alter Table Participants Add Column `share_email` tinyint(11) Default 1 After pubsname;
 Insert Into `PatchLog` (`patchname`) Values ('34_share_email.sql');
