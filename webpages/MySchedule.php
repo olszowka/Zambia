381f4cb92d828454fefd425b9494a579a1efbde8
polszowka <olszowka@users.noreply.github.com>
2010-11-15 06:31:49 +0000

    Complete change related to revisions 727, 729, 731 & 732.  Participant email addresses are now displayed on "My Schedule" page for particpants who have elected to share them.

--- webpages/MySchedule.php
+++ webpages/MySchedule.php
@@ -2,6 +2,7 @@
     $title="My Schedule";
     require ('PartCommonCode.php'); // initialize db; check login;
     $CON_START_DATIM=CON_START_DATIM; //make it a variable so it will be substituted
+    $PROGRAM_EMAIL=PROGRAM_EMAIL; //make it a variable so it will be substituted
     require_once('ParticipantHeader.php');
     // require_once('renderMySessions2.php');
     if (!may_I('my_schedule')) {
@@ -11,13 +12,26 @@
         }
     // set $badgeid from session
     $query= <<<EOD
-    SELECT POS.sessionid, trackname, title, roomname, pocketprogtext,
-    DATE_FORMAT(ADDTIME('$CON_START_DATIM', starttime),'%a %l:%i %p') as 'Start Time',
-    left(duration,5) as 'Duration', persppartinfo, notesforpart FROM
-    ParticipantOnSession POS, Sessions S, Rooms R, Schedule SCH, Tracks T
-    where badgeid="$badgeid" and POS.sessionid = S.sessionid and
-    R.roomid = SCH.roomid and S.sessionid = SCH.sessionid and S.trackid = T.trackid
-    ORDER BY starttime
+SELECT
+            POS.sessionid,
+            T.trackname,
+            S.title,
+            R.roomname,
+            S.progguiddesc,
+            DATE_FORMAT(ADDTIME('$CON_START_DATIM', SCH.starttime),'%a %l:%i %p') as 'Start Time',
+            left(duration,5) as 'S.Duration',
+            S.persppartinfo,
+            S.notesforpart
+    FROM
+            ParticipantOnSession POS
+       JOIN Sessions S USING (sessionid)
+       JOIN Schedule SCH USING (sessionid)
+       JOIN Rooms R USING (roomid)
+       JOIN Tracks T USING (trackid)
+    WHERE
+            POS.badgeid="$badgeid"
+    ORDER BY
+            SCH.starttime
 EOD;
     //error_log("Zambia: $query");
     if (!$result=mysql_query($query,$link)) {
@@ -27,14 +41,23 @@ EOD;
         }
     $schdrows=mysql_num_rows($result);
     for ($i=0; $i<$schdrows; $i++) {
-        list($schdarray[$i]["sessionid"],$schdarray[$i]["trackname"],
-            $schdarray[$i]["title"],$schdarray[$i]["roomname"],$schdarray[$i]["pocketprogtext"],
-            $schdarray[$i]["starttime"],$schdarray[$i]["duration"],$schdarray[$i]["persppartinfo"],
-            $schdarray[$i]["notesforpart"])=mysql_fetch_array($result, MYSQL_NUM);
+        list(
+            $schdarray[$i]["sessionid"],
+            $schdarray[$i]["trackname"],
+            $schdarray[$i]["title"],
+            $schdarray[$i]["roomname"],
+            $schdarray[$i]["progguiddesc"],
+            $schdarray[$i]["starttime"],
+            $schdarray[$i]["duration"],
+            $schdarray[$i]["persppartinfo"],
+            $schdarray[$i]["notesforpart"])
+                =mysql_fetch_array($result, MYSQL_NUM);
         }
     $query= <<<EOD
 SELECT
-            POS.sessionid, CD.badgename, P.pubsname, POS.moderator, PSI.comments
+            POS.sessionid, CD.badgename, P.pubsname, 
+            IF (P.share_email=1,CD.email,null) 'email',
+            POS.moderator, PSI.comments
     FROM
             ParticipantOnSession POS
        JOIN CongoDump CD USING(badgeid)
@@ -52,8 +75,14 @@ EOD;
         }
     $partrows=mysql_num_rows($result);
     for ($i=0; $i<$partrows; $i++) {
-        list($partarray[$i]["sessionid"],$partarray[$i]["badgename"],$partarray[$i]["pubsname"],
-	    $partarray[$i]["moderator"],$partarray[$i]["comments"])=mysql_fetch_array($result, MYSQL_NUM);
+        list(
+            $partarray[$i]["sessionid"],
+            $partarray[$i]["badgename"],
+            $partarray[$i]["pubsname"],
+            $partarray[$i]["email"],
+            $partarray[$i]["moderator"],
+	        $partarray[$i]["comments"])
+	            =mysql_fetch_array($result, MYSQL_NUM);
         }
     $query="SELECT message FROM CongoDump C LEFT JOIN RegTypes R on C.regtype=R.regtype ";
     $query.="WHERE C.badgeid=\"$badgeid\"";
@@ -83,40 +112,43 @@ EOD;
         }
     participant_header($title);
     echo "<P>Below is the list of all the panels for which you are scheduled.  If you need any changes";
-    echo " to this schedule please contact <A 
-HREF=\"mailto:<?php echo PROGRAM_EMAIL; ?>\"><?php echo 
-PROGRAM_EMAIL; ?></A>.\n";
+    echo " to this schedule please contact <A HREF=\"mailto:$PROGRAM_EMAIL\"> Programming </A>.\n";
     echo "<P>In order to put together the entire schedule, we had to schedule some panels outside of the times that certain panelists requested.  If this happened to you, we would love to have you on the panel, but understand if you cannot make it.  Please let us know if you cannot.\n";
-    echo "<P>Several of the panels we are running this year were extremely popular with over 20 potential panelists signing up.  Choosing whom to place on those panels was difficult.  There is always a possibility that one of the panelists currently scheduled will be unavailable so feel free to check with us to see if a space has opened up on a panel on hwhich you'd still like to participate.\n";
-    echo "<P>To facilitate communication yet also preserve privacy, we provide you the option of putting your contact information in the comments field for each panel (under the <A HREF=\"./my_sessions2.php\">\"My Panel Interests\"</A> tab).  That will expose it to other panelists who can then email or call you as appropriate to discuss the panel in advance.  If you check back in a day or two you may find other panelists' information.\n";
+    echo "<P>Several of the panels we are running this year were extremely popular with over 20 potential panelists signing up.  Choosing whom to place on those panels was difficult.  There is always a possibility that one of the panelists currently scheduled will be unavailable so feel free to check with us to see if a space has opened up on a panel on which you'd still like to participate.\n";
     echo "<P>Your registration status is <SPAN class=\"hilit\">$regmessage.</SPAN>\n";
-    echo "<P>Thank you -- <A HREF=\"mailto: <?php echo PROGRAM_EMAIL; 
-?>\"> Programming </a>\n";
+    echo "<P>Thank you -- <A HREF=\"mailto:$PROGRAM_EMAIL\"> Programming </a>\n";
     echo "    <TABLE>\n";
     echo "        <COL><COL width=\"30%\"><COL width=\"20%\"><COL><COL width=\"6%\"><COL><COL width=\"18%\">\n";
     for ($i=0; $i<$schdrows; $i++) {
         echo "        <TR>\n";
-        echo "            <TD class=\"hilit\">".$schdarray[$i]["sessionid"]."</TD>\n";
-        echo "            <TD class=\"hilit\">".htmlspecialchars($schdarray[$i]["title"])."</TD>\n";
-        echo "            <TD class=\"hilit\">".$schdarray[$i]["roomname"]."</TD>\n";
-        echo "            <TD class=\"hilit\">".$schdarray[$i]["trackname"]."</TD>\n";
-        echo "            <TD class=\"hilit\">&nbsp;</TD>\n";
-        echo "            <TD class=\"hilit\">".$schdarray[$i]["starttime"]."</TD>\n";
-        echo "            <TD class=\"hilit\">Duration: ".$schdarray[$i]["duration"]."</TD>\n";
+        echo "            <TD class=\"sched_hd\">".$schdarray[$i]["sessionid"]."</TD>\n";
+        echo "            <TD class=\"sched_hd\">".htmlspecialchars($schdarray[$i]["title"])."</TD>\n";
+        echo "            <TD class=\"sched_hd\">".$schdarray[$i]["roomname"]."</TD>\n";
+        echo "            <TD class=\"sched_hd\">".$schdarray[$i]["trackname"]."</TD>\n";
+        echo "            <TD class=\"sched_hd\">&nbsp;</TD>\n";
+        echo "            <TD class=\"sched_hd\">".$schdarray[$i]["starttime"]."</TD>\n";
+        echo "            <TD class=\"sched_hd\">Duration: ".$schdarray[$i]["duration"]."</TD>\n";
         echo "            </TR>\n";
-        echo "        <TR><TD>&nbsp;</TD>\n";
-        echo "            <TD colspan=6 class=\"border0010\">".htmlspecialchars($schdarray[$i]["pocketprogtext"])."</TD>\n";
-        echo "            </TR>\n";
-        echo "        <TR><TD>&nbsp;</TD>\n";
-        echo "            <TD colspan=6 class=\"border0010\">".htmlspecialchars($schdarray[$i]["persppartinfo"])."</TD>\n";
-        echo "            </TR>\n";
-        echo "        <TR><TD>&nbsp;</TD>\n";
-        echo "            <TD colspan=6 class=\"border0010\">".htmlspecialchars($schdarray[$i]["notesforpart"])."</TD>\n";
-        echo "            </TR>\n";
+        if (($x=$schdarray[$i]["progguiddesc"])!='') {
+            echo "        <TR><TD>&nbsp;</TD>\n";
+            echo "            <TD colspan=6 class=\"border0010\">".htmlspecialchars($x)."</TD>\n";
+            echo "            </TR>\n";
+            }
+        if (($x=$schdarray[$i]["persppartinfo"])!='') {
+            echo "        <TR><TD>&nbsp;</TD>\n";
+            echo "            <TD colspan=6 class=\"border0010\">".htmlspecialchars($x)."</TD>\n";
+            echo "            </TR>\n";
+            }
+        if (($x=$schdarray[$i]["notesforpart"])!='') {
+            echo "        <TR><TD>&nbsp;</TD>\n";
+            echo "            <TD colspan=6 class=\"border0010\">".htmlspecialchars($x)."</TD>\n";
+            echo "            </TR>\n";
+            }
         echo "        <TR><TD colspan=7 class=\"smallspacer\">&nbsp;</TD></TR>\n";
         echo "        <TR><TD>&nbsp;</TD>\n";
         echo "            <TD class=\"usrinp\">Panelists' Publication Names (Badge Names)</TD>\n";
-        echo "            <TD colspan=5 class=\"usrinp\">Their Comments</TD>\n";
+        echo "            <TD class=\"usrinp\">Email addresses</TD>\n";
+        echo "            <TD colspan=4 class=\"usrinp\">Comments</TD>\n";
         echo "            </TR>\n";
         echo "        <TR><TD colspan=7 class=\"smallspacer\">&nbsp;</TD></TR>\n";
         for ($j=0; $j<$partrows; $j++) {
@@ -136,7 +168,8 @@ PROGRAM_EMAIL; ?></A>.\n";
                 echo " <I>mod</I> ";
                 }
             echo "</TD>\n";
-            echo "            <TD colspan=5 class=\"$class\">".htmlspecialchars(fix_slashes($partarray[$j]["comments"]));
+            echo "            <TD class=\"$class\">".htmlspecialchars(fix_slashes($partarray[$j]["email"]));
+            echo "            <TD colspan=4 class=\"$class\">".htmlspecialchars(fix_slashes($partarray[$j]["comments"]));
             echo "</TD>\n";
             echo "            </TR>\n";
             }
