6839f4910cb29b772f86119b46e7391452b9e044
mrdkap <mrdkap@676fd721-d978-42df-8417-bacc17f9f818>
2011-02-10 21:08:44 +0000

    Logistics page for at-con logistics.  Probably could be further broken down by track, at some point for larger cons, but, in the meantime, it's either broken by time, or by room.  CSV able, or nicely printable, and the records are all in their little tables for visual inspection.

---
+++ branches/FFF/webpages/LogisticsPrint.php
@@ -0,0 +1,140 @@
+<?php
+require_once('StaffCommonCode.php');
+global $link;
+$ConStartDatim=CON_START_DATIM; // make it a variable so it can be substituted
+$Grid_Spacer=GRID_SPACER; // make it a variable so it can be substituted
+
+## LOCALIZATIONS
+$_SESSION['return_to_page']="LogisticsPrint.php";
+$title="Logistics";
+$description="<P>Logistics information for each of the rooms.</P>\n";
+$additionalinfo="<P>List <A HREF=\"LogisticsPrint.php?order=room\">by room </A>\n";
+$additionalinfo.="(<A HREF=\"LogisticsPrint.php?order=room&print_p=y\">print</A>)\n";
+$additionalinfo.="(<A HREF=\"LogisticsPrint.php?order=room&csv=y\">CSV</A>)\n";
+$additionalinfo.="or <A HREF=\"LogisticsPrint.php?order=time\">by time</A>\n";
+$additionalinfo.="(<A HREF=\"LogisticsPrint.php?order=time&print_p=y\">print</A>)\n";
+$additionalinfo.="(<A HREF=\"LogisticsPrint.php?order=time&csv=y\">CSV</A>).</P>\n";
+
+/* This query returns the room names, start time, sessionid, title,
+ services, features, and any other tech notes for printing */
+$query = <<<EOD
+SELECT
+    concat('<A HREF="MaintainRoomSched.php?selroom=',R.roomid,'">',R.roomname,'</A>') AS Room,
+    DATE_FORMAT(ADDTIME('$ConStartDatim',starttime),'%a&nbsp;%l:%i&nbsp;%p') as 'Start Time', 
+    concat('<A HREF=StaffAssignParticipants.php?selsess=',SCH.sessionid,'>',SCH.sessionid,'</A>') AS Sessionid,
+    concat('<a href=EditSession.php?id=',SCH.sessionid,'>',S.title,'</a>') Title,
+    Z.roomsetname as 'Room Set',
+    if((X.servicelist!=''),X.servicelist,'') as 'Services',
+    if((Y.featurelist!=''),Y.featurelist,'') as 'Features',
+    if((servicenotes!=''),servicenotes,'') as 'Hotel and Tech Notes'
+  FROM
+      Schedule SCH
+    JOIN Sessions S USING (sessionid)
+    JOIN Rooms R USING (roomid)
+    LEFT JOIN  (SELECT
+           S.sessionid, 
+           GROUP_CONCAT(DISTINCT servicename SEPARATOR ', ') as 'servicelist' 
+        FROM
+            Sessions S, 
+            SessionHasService SS, 
+            Services SE
+        WHERE
+          S.sessionid=SS.sessionid and
+          SE.serviceid=SS.serviceid
+        GROUP BY
+           S.sessionid) X USING (sessionid)
+    LEFT JOIN (SELECT
+           S.sessionid, 
+           GROUP_CONCAT(DISTINCT featurename SEPARATOR ', ') as 'featurelist'
+        FROM
+            Sessions S, 
+            SessionHasFeature SF, 
+            Features F
+        WHERE
+          S.sessionid=SF.sessionid and
+          F.featureid=SF.featureid
+        GROUP BY
+          S.sessionid) Y USING (sessionid)
+    LEFT JOIN (SELECT
+	  S.sessionid,
+	  RS.roomsetname
+        FROM
+	    Sessions S,
+	    RoomSets RS
+        WHERE
+	  S.roomsetid=RS.roomsetid) Z USING (sessionid)
+  ORDER BY
+
+EOD;
+
+if ($_GET["order"]=="time") {
+  $query.="    starttime,\n    roomname";
+ } else {
+  $query.="    roomname,\n    starttime";
+ }
+
+// Retrieve query
+list($rows,$header_array,$roomset_array)=queryreport($query,$link,$title,$description,0);
+
+$checkfield="";
+$newtableline=0;
+for ($i=1; $i<=$rows; $i++) {
+  if ($_GET["order"]=="time") {
+    if ($roomset_array[$i]['Start Time'] != $checkfield) {
+      $checkfield=$roomset_array[$i]['Start Time'];
+      $breakon[$newtableline++]=$i;
+    }
+  } else {
+    if ($roomset_array[$i]['Room'] != $checkfield) {
+      $checkfield=$roomset_array[$i]['Room'];
+      $breakon[$newtableline++]=$i;
+    }
+  }
+ }
+$breakon[$newtableline]=$i;
+    
+
+// Page Rendering
+/* Check for the csv variable, to see if we should be dropping a table,
+ instead of displaying one.  If so, feed a continuous table, otherwise
+ split up the tables on "skip" spaces, to make them flow more naturally.
+ Include the $additionalinfo regularly, so one doesn't have to scroll
+ all the way back to the top, and it gives a nice visual break. */
+if ($_GET["csv"]=="y") {
+  topofpagecsv("Logistics_grid.csv");
+  echo rendercsvreport(1,$rows,$header_array,$roomset_array);
+ } elseif ($_GET["print_p"]=="y") {
+  require_once('../tcpdf/config/lang/eng.php');
+  require_once('../tcpdf/tcpdf.php');
+  $logo="../../../images/nelaLogoHeader.gif";
+  $pdf = new TCPDF('l', 'mm', 'letter', true, 'UTF-8', false);
+  $pdf->SetCreator('Zambia');
+  $pdf->SetAuthor('Programming Team');
+  $pdf->SetTitle('Logistics Grid');
+  $pdf->SetSubject('Logistics Grid');
+  $pdf->SetKeywords('Zambia, Rooms, Logistics, Services, Features, Tech Notes, Grid');
+  $pdf->SetHeaderData($logo, 70, CON_NAME, "nelaonline.org/Zambia-FFF36");
+  $pdf->setHeaderFont(Array("helvetica", '', 10));
+  $pdf->setFooterFont(Array("helvetica", '', 8));
+  $pdf->SetDefaultMonospacedFont("courier");
+  $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+  $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+  $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+  $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+  $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+  $pdf->setLanguageArray($l);
+  $pdf->setFontSubsetting(true);
+  $pdf->SetFont('helvetica', '', 12, '', true);
+  for ($i=0; $i<$newtableline; $i++) {
+    $htmlstring=renderhtmlreport($breakon[$i],$breakon[$i+1]-1,$header_array,$roomset_array);
+    $pdf->AddPage();
+    $pdf->writeHTML($htmlstring, true, false, true, false, '');
+  }
+  $pdf->Output(CON_NAME.'-grid.pdf', 'I');
+ } else {
+  topofpagereport($title,$description,$additionalinfo);
+  for ($i=0; $i<$newtableline; $i++) {
+    echo renderhtmlreport($breakon[$i],$breakon[$i+1]-1,$header_array,$roomset_array);
+  }
+  correct_footer();
+ }
