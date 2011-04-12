ceafd2b76591f4f9906b234c75774454305d42e3
polszowka <polszowka@676fd721-d978-42df-8417-bacc17f9f818>
2011-04-12 04:06:21 +0000

    Work on db patch scripts to support new flexible roles functionality.

--- branches/ArisiaFFFMerge/Install/Upgrade_dbase/37_participant_roles_to_activities.sql
+++ branches/ArisiaFFFMerge/Install/Upgrade_dbase/37_participant_roles_to_activities.sql
@@ -1,11 +1,23 @@
 ## This script renames some tables and columns to make room for new flexible particpant roles functionality
-ALTER TABLE ParticipantRoles RENAME TO ParticipantActivities;
-ALTER TABLE ParticipantHasRole RENAME TO ParticipantHasActivity;
-ALTER TABLE ParticipantHasRole DROP FOREIGN KEY ParticipantHasRole_ibfk_2;
-ALTER TABLE ParticipantActivities CHANGE COLUMN roleid activityid INT NOT NULL AUTO_INCREMENT PRIMARY KEY;
-ALTER TABLE ParticipantActivities CHANGE COLUMN rolename activityname VARCHAR(50);
-ALTER TABLE ParticipantHasActivity CHANGE COLUMN roleid activityid INT NOT NULL;
-ALTER TABLE ParticipantHasActivity DROP INDEX roleid;
-ALTER TABLE ParticipantHasActivity ADD FOREIGN KEY ParticipantHasActivity_ibfk_activityid (activityid) REFERENCES ParticipantActivities(activityid);
+DROP TABLE IF EXISTS ParticipantHasActivity, ParticipantActivities;
+CREATE TABLE ParticipantActivities (
+	activityid INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	activityname VARCHAR(50),
+	display_order INT) ENGINE=INNODB DEFAULT CHARSET=utf8;
+CREATE TABLE ParticipantHasActivity (
+	badgeid VARCHAR(15) NOT NULL DEFAULT '',
+	activityid INT NOT NULL,
+	PRIMARY KEY (badgeid, activityid),
+	KEY activityid (activityid),
+	CONSTRAINT ParticipantHasActivity_ibfk_1 FOREIGN KEY (badgeid) REFERENCES Participants (badgeid),
+	CONSTRAINT ParticipantHasActivity_ibfk_2 FOREIGN KEY (activityid) REFERENCES ParticipantActivities (activityid)
+	) ENGINE=INNODB DEFAULT CHARSET=utf8;
+INSERT INTO ParticipantActivities
+	(activityid, activityname, display_order)
+	SELECT roleid, rolename, display_order FROM Roles ORDER BY roleid;
+INSERT INTO ParticipantHasActivity
+	(badgeid, activityid)
+	SELECT badgeid, roleid FROM ParticipantHasRole ORDER BY badgeid, roleid;
+DROP TABLE ParticipantHasRole;
+DROP TABLE Roles;
 INSERT INTO PatchLog (patchname) VALUES ('37_participant_roles_to_activities.sql');
-
---
+++ branches/ArisiaFFFMerge/Install/Upgrade_dbase/38_new_particpant_roles.sql
@@ -0,0 +1,29 @@
+## This script renames some tables and columns to make room for new flexible particpant roles functionality
+DROP TABLE IF EXISTS SessionRoleDefaults, SessionRoleOverrides;
+DROP TABLE IF EXISTS ParticipantRoles;
+CREATE TABLE ParticipantRoles (
+	roleid INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	rolename VARCHAR(50),
+	display_order INT,
+	is_published TINYINT,
+	is_unique TINYINT,
+	is_self_nominated TINYINT,
+    check_conflicts TINYINT) ENGINE=INNODB DEFAULT CHARSET=utf8;
+CREATE TABLE SessionRoleDefaults (
+	typeid INT NOT NULL,
+	roleid INT NOT NULL,
+	PRIMARY KEY (typeid, roleid),
+	FOREIGN KEY (typeid) REFERENCES `Types` (typeid),
+	KEY roleid (roleid),
+	FOREIGN KEY (roleid) REFERENCES ParticipantRoles (roleid)
+	) ENGINE=INNODB DEFAULT CHARSET=utf8;
+CREATE TABLE SessionRoleOverrides (
+	sessionid INT NOT NULL,
+	roleid INT NOT NULL,
+	PRIMARY KEY (sessionid, roleid),
+	FOREIGN KEY (sessionid) REFERENCES Sessions (sessionid),
+	KEY roleid (roleid),
+	FOREIGN KEY (roleid) REFERENCES ParticipantRoles (roleid)
+	) ENGINE=INNODB DEFAULT CHARSET=utf8;
+ALTER TABLE Sessions ADD COLUMN has_role_override TINYINT DEFAULT 0;
+INSERT INTO PatchLog (patchname) VALUES ('38_new_particpant_roles.sql');
