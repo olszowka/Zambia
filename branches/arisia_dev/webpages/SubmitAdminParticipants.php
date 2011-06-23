0f7207cb2036559228f5118a200fa0eebd657a9f
polszowka <polszowka@676fd721-d978-42df-8417-bacc17f9f818>
2011-06-23 04:07:47 +0000

    Get this stuff into the arisia_dev branch dammit.

--- branches/arisia_dev/webpages/SubmitAdminParticipants.php
+++ branches/arisia_dev/webpages/SubmitAdminParticipants.php
@@ -1,45 +1,108 @@
 <?php
-function SubmitAdminParticipants () {
-    global $link;
-    $interested = $_POST["interested"];
-    $wasinterested = $_POST["wasinterested"];
-    $password = $_POST["password"];
-    $cpassword = $_POST["cpassword"];
-    $partid = $_POST["partid"];
-    $pubsname = stripslashes($_POST["pubsname"]);
-    if ($password=="" and $cpassword=="") {
-            $update_password=false;
-            }
-        elseif ($password==$cpassword) {
-            $update_password=true;
-            }
-        else {
-            $message="Passwords do not match each other.  Database not updated.";
-            echo "<P class=\"errmsg\">".$message."\n";
-            return;
-            }
+require_once('db_functions.php');
+require_once('StaffCommonCode.php');
+function update_participant() {
+    global $link,$message_error;
+    $partid = $_GET["badgeid"];
+    $password = $_GET["password"];
+    $bio = stripslashes($_GET["bio"]);
+    $pubsname = stripslashes($_GET["pname"]);
+    $staffnotes = stripslashes($_GET["staffnotes"]);
+    $interested = $_GET["interested"];
     $query = "UPDATE Participants SET ";
-    if ($update_password==true) {
-        $query=$query."password=\"".md5($password)."\", ";
+    if ($password) {
+        $query.="password=\"".md5($password)."\", ";
         }
-    $query.="interested=".$interested.", ";
-    $query.="pubsname=\"".mysql_real_escape_string($pubsname)."\" ";
-    $query.="WHERE badgeid=\"$partid\"";                               //"
-    if (!mysql_query($query,$link)) {
-        $message=$query."<BR>Error updating database.  Database not updated.";
-        echo "<P class=\"errmsg\">".$message."\n";
+    if ($bio) {
+        $query.="bio=\"".mysql_real_escape_string($bio)."\", ";
+        }
+    if ($pubsname) {
+        $query.="pubsname=\"".mysql_real_escape_string($pubsname)."\", ";
+        }
+    if ($staffnotes) {
+        $query.="staff_notes=\"".mysql_real_escape_string($staffnotes)."\", ";
+        }
+    if ($interested) {
+        $query.="interested=".mysql_real_escape_string($interested).", ";
+        }
+	$query = substr($query,0,-2); //drop two characters at end: ", "
+    $query.=" WHERE badgeid=\"".mysql_real_escape_string($partid)."\"";
+    if (!mysql_query_with_error_handling($query)) {
+        echo "<p class=\"errmsg\">".$message_error."</p>";
         return;
         }
-    $message="Database updated successfully.<BR>";
-    if ($wasinterested==1 and $interested==2) {
+    $message="<p class=\"regmsg\">Database updated successfully.</p>";
+    if ($interested==2) {
         $query="DELETE FROM ParticipantOnSession where badgeid = \"$partid\"";
-        if (!mysql_query($query,$link)) {
-            $message=$query."<BR>Error updating database.  Database not updated.";
-            echo "<P class=\"errmsg\">".$message."\n";
-            return;
-            }
-        $message.="Participant removed from ".mysql_affected_rows($link)." session(s).";
+	    if (!mysql_query_with_error_handling($query)) {
+	        echo "<p class=\"errmsg\">".$message_error."</p>";
+	        return;
+	        }
+        $message.="<p class=\"regmsg\">Participant removed from ".mysql_affected_rows($link)." session(s).</p>";
         }
-    echo "<P class=\"regmsg\">".$message."\n";
+    echo $message;
     }
+
+function perform_search() {
+	$searchString = mysql_real_escape_string(stripslashes($_GET["searchString"]));
+	if ($searchString=="")
+		exit();
+	if (is_numeric($searchString)) {
+			$query["searchParticipants"] = <<<EOD
+			SELECT
+			        P.badgeid, P.pubsname, P.interested, P.bio, P.staff_notes, CD.firstname, CD.lastname, CD.badgename
+			    FROM
+						 Participants P
+					JOIN CongoDump CD ON P.badgeid = CD.badgeid
+			    WHERE
+			        P.badgeid = "$searchString"
+			    ORDER BY
+			        CD.lastname, CD.firstname
+EOD;
+			}
+		else {
+			$searchString='%'.$searchString.'%';
+			$query["searchParticipants"] = <<<EOD
+			SELECT
+			        P.badgeid, P.pubsname, P.interested, P.bio, P.staff_notes, CD.firstname, CD.lastname, CD.badgename
+			    FROM
+						 Participants P
+					JOIN CongoDump CD ON P.badgeid = CD.badgeid
+			    WHERE
+			           P.pubsname LIKE "$searchString"
+					OR CD.lastname LIKE "$searchString"
+					OR CD.firstname LIKE "$searchString"
+					OR CD.badgename LIKE "$searchString"
+			    ORDER BY
+			        CD.lastname, CD.firstname
+EOD;
+			}
+	$xml=mysql_query_XML($query);
+	$xsl = new DomDocument;
+	$xsl->load('xsl/AdminParticipants.xsl');
+	$xslt = new XsltProcessor();
+	$xslt->importStylesheet($xsl);
+	if ($html = $xslt->transformToXML($xml)) {
+			header("Content-Type: text/html"); 
+		    echo $html;
+			}
+		else {
+		    trigger_error('XSL transformation failed.', E_USER_ERROR);
+			}
+	exit();
+}
+// Start here.  Should be AJAX requests only
+if (!$ajax_request_action=$_GET["ajax_request_action"])
+	exit();
+switch ($ajax_request_action) {
+	case "perform_search":
+		perform_search();
+		break;
+	case "update_participant":
+		update_participant();
+		break;
+	default:
+		exit();
+	}
+
 ?>
