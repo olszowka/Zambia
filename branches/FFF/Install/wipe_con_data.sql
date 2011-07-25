3513d8126d024d8ee76459b217f43693354a8740
mrdkap <mrdkap@676fd721-d978-42df-8417-bacc17f9f818>
2011-07-25 02:52:53 +0000

    Updated the order of the wipe, and added a few pieces of information.

--- branches/FFF/Install/wipe_con_data.sql
+++ branches/FFF/Install/wipe_con_data.sql
@@ -13,19 +13,23 @@ truncate table ParticipantOnSession;
 truncate table ParticipantSessionInterest;
 truncate table ParticipantSuggestions;
 truncate table TimeCard;
+truncate table NotesOnParticipants;
+truncate table SessionEditHistory;
+truncate table PersonalFlow;
+truncate table Tasklist;
 truncate table Participants;
 truncate table CongoDump;
 truncate table Schedule;
-truncate table NotesOnParticipants;
 truncate table Feedback;
 truncate table CommentsOnProgramming;
 truncate table CommentsOnParticipants;
 truncate table CommentsOnSessions;
 truncate table SessionHasFeature;
 truncate table SessionHasService;
-truncate table SessionEditHistory;
 truncate table SessionHasPubChar;
 truncate table Sessions;
+truncate table PreconHours;
+truncate table Rooms;
 -- you don't want to disable constraints. Really! 
 -- you want to run down the real problem instead  
 -- because you missed zeroing a table and you need to zero it or eek!
@@ -33,8 +37,6 @@ truncate table Sessions;
 -- Now you want to pull a backup using:
 --    mysqldump -u username -p dbasename > EmptyDbase.dump
 -- 
--- you might very well want the following: 
-INSERT INTO `Participants` (badgeid, pubsname, password, bestway, interested, bio)
- VALUES ('brainstorm',null,'ecf65a5d41056d7dd4d548e3ef200476',null,null,null);
-INSERT INTO `UserHasPermissionRole` VALUES ('brainstorm',5);
+-- you might very well want to pull in the Initial_Users.sql file now.
+
 
