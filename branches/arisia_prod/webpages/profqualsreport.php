d18ca96d110db02d9527a46ecb244c78a9c53d51
polszowka <polszowka@676fd721-d978-42df-8417-bacc17f9f818>
2011-10-13 03:18:30 +0000

    New report for professional qualifications; other tweaks

---
+++ branches/arisia_prod/webpages/profqualsreport.php
@@ -0,0 +1,53 @@
+<?php
+	global $message_error;
+	$title = "Professional Credentials Report";
+    require_once('db_functions.php');
+    require_once('StaffCommonCode.php'); //reset connection to db and check if logged in
+    $ConStartDatim=CON_START_DATIM; // make it a variable so it can be substituted
+    $query="SET group_concat_max_len=25000";
+    if (!mysql_query_with_error_handling($query)) {
+        RenderError($title,$message_error);
+        exit();
+        }
+	$queryArray["participants"]=<<<EOD
+	SELECT
+			CONCAT(CD.lastname,", ",CD.firstname) AS name, P.pubsname, CD.badgename,
+			CD.badgeid 
+		FROM
+				CongoDump CD
+		   JOIN Participants P USING (badgeid)
+		WHERE
+			P.interested = 1
+		ORDER BY
+			CD.lastname, CD.firstname
+EOD;
+	$queryArray["credentials"]=<<<EOD
+	SELECT
+			P.badgeid, C.credentialname
+		FROM
+				Participants P
+		   JOIN ParticipantHasCredential PHC USING (badgeid)
+		   JOIN Credentials C USING (credentialid)
+		WHERE
+			P.interested = 1
+		ORDER BY
+			P.badgeid, C.display_order
+EOD;
+	if (($resultXML=mysql_query_XML($queryArray))===false) {
+	    RenderError($title,$message_error);
+        exit();
+        }
+	staff_header($title);
+	date_default_timezone_set('US/Eastern');
+	echo "<P align=center> Generated: ".date("D M j G:i:s T Y")."</P>\n";
+	echo "<P>List of all users with their permission roles</P>\n";
+	//echo($resultXML->saveXML()); //for debugging only
+	$xsl = new DomDocument;
+	$xsl->load('xsl/profqualsreport.xsl');
+	$xslt = new XsltProcessor();
+	$xslt->importStylesheet($xsl);
+	$html = $xslt->transformToXML($resultXML);
+	echo(mb_ereg_replace("<(div|iframe|script|textarea)([^>]*/[ ]*)>", "<\\1\\2></\\1>", $html, "i"));
+	// some browsers does not support empty div, iframe, script and textarea tags
+	staff_footer();
+?>
---
+++ branches/arisia_prod/webpages/xsl/profqualsreport.xsl
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<!--
+	profqualsreport
+	Created by Peter Olszowka on 2011-10-12.
+	Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+-->
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output encoding="UTF-8" indent="yes" method="xml" />
+<xsl:template match="/">
+    <table class="report">
+        <tr>
+            <th class="report">Last name, first name</th>
+            <th class="report">Name for Publications</th>
+            <th class="report">Badge Name</th>
+            <th class="report">Badge Id</th>
+            <th class="report">Professional Qualifications</th>
+        </tr>
+        <xsl:apply-templates select="/doc/query[@queryName='participants']/row"/>
+    </table>
+</xsl:template>
+<xsl:template match="/doc/query[@queryName='participants']/row">
+    <xsl:variable name="badgeid" select="@badgeid"/>
+    <tr>
+        <td class="report"><xsl:value-of select="@name"/></td>
+        <td class="report"><xsl:value-of select="@pubsname"/></td>
+        <td class="report"><xsl:value-of select="@badgename"/></td>
+        <td class="report"><xsl:value-of select="@badgeid"/></td>
+        <td class="report">
+            <xsl:for-each select="/doc/query[@queryName = 'credentials']/row[@badgeid = $badgeid]">
+                <div><xsl:value-of select="@credentialname"/></div>
+            </xsl:for-each>
+        </td>
+    </tr>
+</xsl:template>
+</xsl:stylesheet>
