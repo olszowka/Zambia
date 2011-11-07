3c004ee3c8cb7a549e65712d0cd4eb9bb6f35257
mrdkap <mrdkap@676fd721-d978-42df-8417-bacc17f9f818>
2011-11-07 16:02:13 +0000

    Fixed BrainstormSearchSession to be reentrant, with the search consistantly at the top, so you can re-search for what you want if your search is too narrow, or broad.  It remembers what you searched for before, just in case, and still gives you an empty list first, but everything if you don't specify anything in your search, once you click the search button.  It now uses the same RenderPrecis that the other Brainstorm displays use, for consistancy sake, and fixes the misleading check-boxes and save option from previous.

--- branches/FFF/webpages/BrainstormSearchSession.php
+++ branches/FFF/webpages/BrainstormSearchSession.php
@@ -1,46 +1,100 @@
 <?php
-    global $participant,$message_error,$message2,$congoinfo;
-    global $partAvail,$availability;
-    $title="Search Panels";
-    require ('BrainstormCommonCode.php'); // initialize db; check login;
-    //                                  set $badgeid from session
-    if (!may_I('BS_sear_sess')) {
-        $message_error="You do not currently have permission to view this page.<BR>\n";
-        RenderError($title,$message_error);
-        exit();
-        }
-    brainstorm_header($title);
+require_once('BrainstormCommonCode.php');
 
+$title="Search Panels";
+$description="<P>Clicking Search without making any selections will display all panels.</P>";
+$additionalinfo="";
+
+// Test to see if searching is allowed
+if (!may_I('BS_sear_sess')) {
+  $message_error="You do not currently have permission to view this page.<BR>\n";
+  RenderError($title,$message_error);
+  exit();
+ }
+
+// Variables, passed in
+if (isset($_POST["track"])) {
+  $trackid=$_POST["track"];
+ } else {
+  $trackid=0;
+ }
+$titlesearch=stripslashes($_POST["title"]);
+
+// Header, followed by search, followed by (if there is a search) the results of the search.
+topofpagereport($title,$description,$additionalinfo);
+
+// If there are any specific words for this event
+if (file_exists("../Local/Verbiage/BrainstormSearchSession_0")) {
+  echo file_get_contents("../Local/Verbiage/BrainstormSearchSession_0");
+ }
 ?>
 
-<FORM method=POST action="BrainstormSearchSession_POST.php">
-  <table>
-    <COL><COL><COL><COL><COL>
-    <tr> <!-- trow -->
-
-        <td>Track: </td>
-        <td>
-          <SELECT class="tcell" name="track">
-            <?php $query = "SELECT trackid, trackname FROM Tracks WHERE selfselect=1 ORDER BY display_order"; populate_select_from_query($query, '0', "ANY",false); ?>
-          </SELECT>
-        </td>
-
-        <td>Title Search:</td>
-        <td> <INPUT name="title"> </INPUT> </td>
-
-    </tr> <!-- trow -->
-
-    <td colspan=5, align=right>
+<FORM name="brainstormsearchsession" method=POST action="BrainstormSearchSession.php">
+<INPUT type="hidden" name="issearch" value="1">
+<TABLE>
+  <TR>
+    <TD>Track:</TD>
+    <TD><SELECT class="tcell" name="track">
+          <?php $query = "SELECT trackid, trackname FROM Tracks WHERE selfselect=1 ORDER BY display_order"; populate_select_from_query($query, $trackid, "ANY", false); ?>
+        </SELECT></TD>
+    <TD>Title Search:</TD>
+    <TD> <INPUT type="text" name="title" value="<?php echo $titlesearch; ?>"></TD>
+  </TR>
+  <TR>													      
+    <TD colspan=5, align=right>
         <BUTTON type=submit value="search">Search</BUTTON>
-    </td><p>&nbsp;</p>
-
-  </tr>
-</table>
-
-<P>Clicking Search without making any selections will display all panels.</P>
-<?php if (file_exists("../Local/Verbiage/BrainstormSearchSession_0")) {
-    echo file_get_contents("../Local/Verbiage/BrainstormSearchSession_0");
-?>
+    </TD>
+  </TR>
+</TABLE>
 </FORM>
-</BODY>
-</HTML>
+
+<?php
+
+// Stop here if there wasn't a previous search using the "issearch" variable.
+if (!isset($_POST["issearch"])) {
+  correct_footer();
+  exit();
+}
+
+$query = <<<EOD
+SELECT
+    sessionid,
+    trackname,
+    null typename,
+    title,
+    CASE
+      WHEN HOUR(duration) < 1 THEN concat(date_format(duration,'%i'),'min')
+      WHEN MINUTE(duration)=0 THEN concat(date_format(duration,'%k'),'hr')
+      ELSE concat(date_format(duration,'%k'),'hr ',date_format(duration,'%i'),'min')
+      END AS Duration,
+    estatten,
+    pocketprogtext,
+    progguiddesc,
+    persppartinfo
+  FROM
+      Sessions
+    JOIN Tracks USING (trackid)
+    JOIN SessionStatuses USING (statusid)
+    JOIN Types USING (typeid)
+  WHERE
+    statusname in ('Edit Me','Brainstorm','Vetted','Assigned','Scheduled') and
+    typename in ('Panel','Class','Presentation','Author Reading','Lounge','SIG/BOF/MnG','Social','EVENT','Performance')
+EOD;
+if ($trackid!=0) {
+  $query.=" and trackid=".$trackid;
+ }
+if ($titlesearch!="") {
+  $query.=" AND title LIKE \"%".mysql_real_escape_string($titlesearch,$link)."%\" ";
+}
+$query.=" ORDER BY trackname, title";
+
+if (!$result=mysql_query($query,$link)) {
+  $message=$query."<BR>Error querying database.<BR>";
+  RenderError($title,$message);
+  exit();
+ }
+
+RenderPrecis($result,$showlinks);
+correct_footer();
+exit();
+?>
