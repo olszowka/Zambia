75cec6141560f767c4091b0780964867d9042316
mrdkap <mrdkap@676fd721-d978-42df-8417-bacc17f9f818>
2011-11-22 16:14:40 +0000

    New Volunteer editing/updating pages that actually work, keep track of time before, during and after the con, going back to the original design of the pages.

---
+++ branches/FFF/webpages/VolunteerCheckIn.php
@@ -0,0 +1,108 @@
+<?php
+require_once('StaffCommonCode.php');
+global $link;
+$TimecardDB=TIMECARDDB;  // make it a variable so it can be substituted
+
+// Tests for the substituted variables
+if ($TimecardDB=="TIMECARDDB") {unset($TimecardDB);}
+
+$title="Volunteer Check In";
+$description="<P align=\"center\">Check in the Volunteer in question.</P>";
+
+// Should be generated from PermissionAtoms or PermissionRoles, somehow
+$permission_array=array('Programming', 'General', 'Liaison', 'Watch', 'Registration', 'Vendor', 'Events', 'Logistics', 'Sales', 'Fasttrack');
+
+foreach ($permission_array as $perm) {
+  if (may_I($perm)) {$inrole_array[]="'$perm'";}
+}
+
+if (isset($inrole_array)) {
+  $inrole_string=implode(",",$inrole_array);
+ } else {
+  $inrole_string="'P-Volunteer','G-Volunteer'";
+ }
+
+$query=<<<EOF
+SELECT
+    badgeid,
+    pubsname 
+  FROM
+      Participants
+    JOIN UserHasPermissionRole USING (badgeid)
+    JOIN PermissionRoles USING (permroleid)
+  WHERE
+    permrolename in ($inrole_string)
+  ORDER BY
+    pubsname
+EOF;
+
+if (isset($_POST['badgeid'])) {
+  $badgeid=$_POST['badgeid'];
+ } elseif (isset($_GET['badgeid'])) {
+  $badgeid=$_GET['badgeid'];
+ } else {
+  topofpagereport($title,$description,$additionalinfo);
+  echo "<FORM name=\"whichvol\" method=POST action=\"VolunteerCheckIn.php\">";
+  echo "  <DIV style=\"text-align:center\">\n    <LABEL for=\"badgeid\">Volunteer: </LABEL>\n";
+  echo "    <SELECT name=\"badgeid\">\n";
+  populate_select_from_query($query,$_SESSION['badgeid'],"",false);
+  echo "    </SELECT>\n  </DIV>\n";
+  echo "  <DIV style=\"text-align:center\">\n";
+  echo "    <BUTTON type=\"submit\" name=\"submit\" class=\"SubmitButtion\">Choose</BUTTON>\n";
+  echo "  </DIV>\n</FORM>\n";
+  correct_footer();
+  exit();
+ }
+
+if (isset($_POST['voltimein'])) {
+  $element_array = array('badgeid', 'voltimein', 'volinbadgeid');
+  $value_array=array($badgeid,$_POST['voltimein'],$_SESSION['badgeid']);
+  $message.=submit_table_element($link, $title, "$TimecardDB.TimeCard", $element_array, $value_array);
+  /* if (isset($_SESSION['return_to_page'])) {
+    header("Location: ".$_SESSION['return_to_page']); // Redirect back to what send you here
+   } else {
+    header("Location: VolunterCheckIn.php"); // Redirect back to here, with a blank slate
+   } */
+  topofpagereport($title,$description,$additionalinfo);
+  echo "<P>Database: $TimecardDB.Timecard</P>\n";
+  correct_footer();
+  exit();
+ }
+
+$query=<<<EOF
+SELECT
+    pubsname 
+  FROM
+      Participants
+  WHERE
+    badgeid='$badgeid'
+EOF;
+
+// Retrieve query
+list($elements,$header_array,$element_array)=queryreport($query,$link,$title,$description,0);
+
+$pubsname=$element_array[1]['pubsname'];
+
+topofpagereport($title,$description,$additionalinfo);
+?>
+<FORM name="volcheckin" method=POST action="VolunteerCheckIn.php">
+  <INPUT type="hidden" name="badgeid" value="<?php echo $badgeid; ?>">
+  <INPUT type="hidden" name="voltimein" value="<?php echo date('Y-m-d H:i:s'); ?>">
+  <DIV style="text-align:center">
+    <BUTTON class="SubmitButtion" type="submit" name="submit">Check in <?php echo "$pubsname"; ?> now.</BUTTON>
+  </DIV>
+</FORM>
+<hr>
+<FORM name="volfixcheckin" method=POST action="VolunteerCheckIn.php">
+  <INPUT type="hidden" name="badgeid" value="<?php echo $badgeid; ?>">
+  <DIV style="text-align:center">
+    <LABEL for="voltimein">Actual start time for <?php echo "$pubsname: " ?></LABEL>
+    <INPUT type="text" size="20" name="voltimein" value="<?php echo date('Y-m-d H:i:s'); ?>"
+  </DIV>
+  <DIV style="text-align:center">
+    <BUTTON class="SubmitButton" type="submit" name="submit">Check in <?php echo "$pubsname"; ?> as of the above date.</BUTTON>
+  </DIV>
+</FORM>
+<?php
+correct_footer();
+?>
---
+++ branches/FFF/webpages/VolunteerCheckOut.php
@@ -0,0 +1,109 @@
+<?php
+require_once('StaffCommonCode.php');
+global $link;
+$TimecardDB=TIMECARDDB;  // make it a variable so it can be substituted
+
+// Tests for the substituted variables
+if ($TimecardDB=="TIMECARDDB") {unset($TimecardDB);}
+
+$title="Volunteer Check Out";
+$description="<P align=\"center\">Check out the Volunteer's Check In instance.</P>";
+
+// Should be generated from PermissionAtoms or PermissionRoles, somehow
+$permission_array=array('Programming', 'General', 'Liaison', 'Watch', 'Registration', 'Vendor', 'Events', 'Logistics', 'Sales', 'Fasttrack');
+
+foreach ($permission_array as $perm) {
+  if (may_I($perm)) {$inrole_array[]="'$perm'";}
+}
+
+if (isset($inrole_array)) {
+  $inrole_string=implode(",",$inrole_array);
+ } else {
+  $inrole_string="'P-Volunteer','G-Volunteer'";
+ }
+
+$query=<<<EOF
+SELECT
+    voltimeid,
+    concat(pubsname, " in at: ",DATE_FORMAT(voltimein,'%a %l:%i %p (%k:%i)')) as 'Who'
+  FROM
+      Participants
+    JOIN UserHasPermissionRole USING (badgeid)
+    JOIN PermissionRoles USING (permroleid)
+    JOIN $TimecardDB.TimeCard USING (badgeid)
+  WHERE
+    permrolename in ($inrole_string) AND
+    voltimeout IS NULL
+  ORDER BY
+    pubsname
+EOF;
+
+if (isset($_POST['voltimeid'])) {
+  $voltimeid=$_POST['voltimeid'];
+ } elseif (isset($_GET['voltimeid'])) {
+  $voltimeid=$_GET['voltimeid'];
+ } else {
+  topofpagereport($title,$description,$additionalinfo);
+  echo "<FORM name=\"whichvol\" method=POST action=\"VolunteerCheckOut.php\">";
+  echo "  <DIV style=\"text-align:center\">\n    <LABEL for=\"voltimeid\">Volunteer: </LABEL>\n";
+  echo "    <SELECT name=\"voltimeid\">\n";
+  populate_select_from_query($query,$_SESSION['badgeid'],"",false);
+  echo "    </SELECT>\n  </DIV>\n";
+  echo "  <DIV style=\"text-align:center\">\n";
+  echo "    <BUTTON type=\"submit\" name=\"submit\" class=\"SubmitButtion\">Choose</BUTTON>\n";
+  echo "  </DIV>\n</FORM>\n";
+  correct_footer();
+  exit();
+ }
+
+if (isset($_POST['voltimeout'])) {
+  $pairedvalue_array=array("voltimeout='".$_POST['voltimeout']."'","voloutbadgeid='".$_SESSION['badgeid']."'");
+  $message.=update_table_element($link, $title, "$TimecardDB.TimeCard", $pairedvalue_array, "voltimeid", $_POST['voltimeid']);
+  if (isset($_SESSION['return_to_page'])) {
+    header("Location: ".$_SESSION['return_to_page']); // Redirect back to what send you here
+  } else {
+    header("Location: VolunterCheckOut.php"); // Redirect back to here, with a blank slate
+  }
+  exit();
+ }
+
+$query=<<<EOF
+SELECT
+    pubsname,
+    DATE_FORMAT(voltimein,'%a %l:%i %p (%k:%i)') AS "inat"
+  FROM
+      $TimecardDB.TimeCard
+    JOIN Participants USING (badgeid)
+  WHERE
+    voltimeid='$voltimeid'
+EOF;
+
+// Retrieve query
+list($elements,$header_array,$element_array)=queryreport($query,$link,$title,$description,0);
+
+$pubsname=$element_array[1]['pubsname'];
+$inat="<B>".$element_array[1]['inat']."</B> for $pubsname";
+
+topofpagereport($title,$description,$additionalinfo);
+?>
+<FORM name="volcheckout" method=POST action="VolunteerCheckOut.php">
+  <INPUT type="hidden" name="voltimeid" value="<?php echo $voltimeid; ?>">
+  <INPUT type="hidden" name="voltimeout" value="<?php echo date('Y-m-d H:i:s'); ?>">
+  <DIV style="text-align:center">
+    <BUTTON class="SubmitButtion" type="submit" name="submit">Check out <?php echo "$pubsname"; ?> now.</BUTTON>
+  </DIV>
+</FORM>
+<hr>
+<FORM name="volfixcheckout" method=POST action="VolunteerCheckOut.php">
+  <INPUT type="hidden" name="voltimeid" value="<?php echo $voltimeid; ?>">
+  <DIV style="text-align:center">
+    <LABEL for="voltimeout">Actual end time for shift starting at <?php echo "$inat: " ?></LABEL>
+    <INPUT type="text" size="20" name="voltimeout" value="<?php echo date('Y-m-d H:i:s'); ?>"
+  </DIV>
+  <DIV style="text-align:center">
+    <BUTTON class="SubmitButton" type="submit" name="submit">Check out <?php echo "$pubsname"; ?> as of the above date.</BUTTON>
+  </DIV>
+</FORM>
+<?php
+correct_footer();
+?>
