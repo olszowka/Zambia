7d49b5ffc8cf38cb47aae5867c9029626c5ebc77
mrdkap <mrdkap@676fd721-d978-42df-8417-bacc17f9f818>
2011-12-13 17:06:38 +0000

    Added the alternate View Counts of Sessions report to the StaffManageSessions page, and the volunteer Postgrid and Descriptions to the Index page.

--- branches/FFF/webpages/StaffManageSessions.php
+++ branches/FFF/webpages/StaffManageSessions.php
@@ -15,7 +15,7 @@ topofpagereport($title,$description,$additionalinfo);
    <DD>Used for creating new sessions.  They are intially created in status "edit me".  Once created, a second persion edits for content (and uniqueness). This person promotes the session to status "Brainstorm".  A third set of eyes does a basic grammar and spelling edit and promotes the session to status "Vetted".   At that time it is ready for general viewing by prospective panelists.</DD>
    <DT><A HREF="EditSession.php">Edit an Existing Session</A></DT>
    <DD>Rapidly access a Session from the list of Sessions to Edit or Update.</DD>
-   <DT><A HREF="genreport.php?reportname=viewsessioncountreport">View Counts of Sessions</A></DT>
+   <DT><A HREF="genreport.php?reportname=viewsessioncountreport">View Counts of Sessions</A>(<A HREF="genreport.php?reportname=viewrollupsessioncountreport">Alternate View Counts of Sessions</A>)</DT>
    <DD>A quick report broken down by status and then by track to give an idea of where we are.</DD>
    <DT><A HREF="genreport.php?reportname=ViewAllSessions">View All Sessions:</A></DT>
    <DD>A tabular report on all sessions organized by track.  Key information on each session is visible from the top level and a link takes you down into the details for any session.</DD>
