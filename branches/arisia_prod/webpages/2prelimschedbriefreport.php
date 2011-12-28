5fff72e44a1668577f44d26670f5e93c22ff7771
polszowka <polszowka@676fd721-d978-42df-8417-bacc17f9f818>
2011-12-28 13:09:29 +0000

    Tweak report to latest framework.

--- branches/arisia_prod/webpages/2prelimschedbriefreport.php
+++ branches/arisia_prod/webpages/2prelimschedbriefreport.php
@@ -4,11 +4,7 @@
     require_once('db_functions.php');
     require_once('StaffCommonCode.php'); //reset connection to db and check if logged in
     $ConStartDatim=CON_START_DATIM; // make it a variable so it can be substituted
-    $query="SET group_concat_max_len=25000";
-    if (!mysql_query_with_error_handling($query)) {
-        RenderError($title,$message_error);
-        exit();
-        }
+    $_SESSION['return_to_page']="2prelimschedbriefreport.php";
 	$queryArray["schedule"]=<<<EOD
 SELECT
 			DATE_FORMAT(ADDTIME('$ConStartDatim',SCH.starttime),'%a %l:%i %p') AS starttime,
@@ -32,10 +28,10 @@ EOD;
 	staff_header($title);
 	date_default_timezone_set('US/Eastern');
 	echo "<P align=center> Generated: ".date("D M j G:i:s T Y")."</P>\n";
-	echo "<P>Preliminary list of all scheduled \"Programming\" sessions.</P>\n";
+	echo "<P>Preliminary list of all scheduled \"Programming\" sessions. [On&nbsp;Demand]</P>\n";
 	//echo($resultXML->saveXML()); //for debugging only
 	$xsl = new DomDocument;
-	$xsl->load('xsl/2prelimschedbriefreport.xsl');
+	$xsl->load('reportxsl/2prelimschedbrief.xsl');
 	$xslt = new XsltProcessor();
 	$xslt->importStylesheet($xsl);
 	$html = $xslt->transformToXML($resultXML);
