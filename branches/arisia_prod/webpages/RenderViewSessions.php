208f6a310553a6d7bdf47d6876dfb93410d4ae91
luthian <luthian@676fd721-d978-42df-8417-bacc17f9f818>
2012-08-23 15:17:09 +0000

    Increase font size of precis reports and remove table striping.

--- branches/arisia_prod/webpages/RenderViewSessions.php
+++ branches/arisia_prod/webpages/RenderViewSessions.php
@@ -4,7 +4,7 @@
     $title="Session Query Results";
       staff_header($title);
 ?>
-    <TABLE class="table table-condensed table-striped">
+    <TABLE class="table table-condensed">
         <COL><COL><COL><COL><COL><COL><COL><COL>
         <TR>
             <TH class="border1121">Sess.<BR>ID</TH>
