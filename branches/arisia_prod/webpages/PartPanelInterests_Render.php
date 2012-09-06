9261b512369fc53f20a13a81349fe132331fb403
luthian <luthian@676fd721-d978-42df-8417-bacc17f9f818>
2012-09-06 02:30:38 +0000

    Started work on Panel Interests page. Wrapped each "panel block" in a white block (using three classes. Ugh). For some reason, though, the right border isn't showing.

--- branches/arisia_prod/webpages/PartPanelInterests_Render.php
+++ branches/arisia_prod/webpages/PartPanelInterests_Render.php
@@ -30,27 +30,27 @@ function render_session_interests($badgid,$session_interest_count,$message,$mess
 	$j=1; //use $j so that skipped sessions don't skip numbering
     for ($i=1; $i<=$session_interest_count; $i++) {
         if (!$session_interests[$i]['title']) continue;
-        echo "    <TR>\n";
+        echo "    <TR class=\"topBorder\">\n";
         echo "        <TD rowspan=5 class=\"border0000 hilit\" id=\"sessidtcell\">{$session_interests[$i]['sessionid']}";
         echo "            <INPUT type=\"hidden\" name=\"sessionid$j\" value=\"{$session_interests[$i]['sessionid']}\"></TD>\n";
         echo "        <TD class=\"border0000 hilit vatop\">{$session_interests[$i]['trackname']}</TD>\n";
         echo "        <TD colspan=2 class=\"border0000 hilit vatop\">".htmlspecialchars($session_interests[$i]['title'],ENT_NOQUOTES)."</TD>\n";
         echo "        <TD class=\"border0000 hilit vatop\">Duration: {$session_interests[$i]['duration']}</TD>\n";
         echo "        </TR>\n";
-        echo "    <TR>\n";
+        echo "    <TR class=\"edgeBorder\">\n";
         echo "        <TD class=\"border0000 usrinp\">Rank: <INPUT type=\"text\" size=3 name=\"rank$j\" value=\"{$session_interests[$i]['rank']}\"></TD>\n";
         echo "        <TD class=\"border0000 usrinp\">I'd like to moderate this panel:<INPUT type=\"checkbox\" value=1 name=\"mod$j\" ";
         echo "            ".(($session_interests[$i]['willmoderate'])?"checked":"")."></TD>\n";
         echo "        <TD colspan=2 class=\"border0000 usrinp\">Remove this panel from my list:<INPUT type=\"checkbox\" value=1 name=\"delete$j\"></TD>\n";
         echo "        </TR>\n";
-        echo "    <TR>\n";
+        echo "    <TR class=\"edgeBorder\">\n";
         echo "        <TD  class=\"border0000 usrinp\" colspan=4>Use this space to convince us why you would be fabulous on this panel. ";
         echo "            <TEXTAREA height=5em cols=80 name=\"comments$j\" id=\"intCmnt\">". htmlspecialchars( $session_interests[$i]['comments'],ENT_COMPAT)."</TEXTAREA></TD>\n";
         echo "        </TR>\n";
-        echo "    <TR>\n";
+        echo "    <TR class=\"edgeBorder\">\n";
         echo "        <TD colspan=4 class=\"border0010\">".htmlspecialchars($session_interests[$i]['progguiddesc'],ENT_NOQUOTES)."</TD>\n";
         echo "        </TR>\n";
-        echo "    <TR>\n";
+        echo "    <TR class=\"bottomBorder\">\n";
         echo "        <TD colspan=4 class=\"border0000\">".htmlspecialchars($session_interests[$i]['persppartinfo'],ENT_NOQUOTES)."</TD>\n";
         echo "        </TR>\n";
         echo "    <TR>\n";
