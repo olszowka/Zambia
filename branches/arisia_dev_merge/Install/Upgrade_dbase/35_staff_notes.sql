3d8655fc7b18712919fa4dccc7bb5355b25f64c5
polszowka <polszowka@676fd721-d978-42df-8417-bacc17f9f818>
2012-10-18 02:45:38 +0000

    Remove obsolete branch.

--- branches/arisia_dev_merge/Install/Upgrade_dbase/35_staff_notes.sql
+++
@@ -1,4 +0,0 @@
-## This script adds support for staff recording notes about each participant.
-## It adds a column to Participants.
-ALTER TABLE Participants ADD COLUMN `staff_notes` TEXT NULL AFTER `share_email`;
-INSERT INTO `PatchLog` (`patchname`) VALUES ('35_staff_notes.sql');
