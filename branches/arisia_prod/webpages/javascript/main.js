962138c62b090933861a3cbbf15b44db9008bdd4
polszowka <polszowka@676fd721-d978-42df-8417-bacc17f9f818>
2013-02-02 20:39:23 +0000

    Previous change merged incorrectly.  Need cases for assign participant and administer participant.

--- branches/arisia_prod/webpages/javascript/main.js
+++ branches/arisia_prod/webpages/javascript/main.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
 	//this function is run whenever any page finishes loading if JQuery has been loaded
 	//debugger;
 	switch (thisPage) {
+		case "Administer Participants":
+			initializeAdminParticipants();
+			break;
 		case "Assign Participants":
 			initAssignParticipants();
 			break;
