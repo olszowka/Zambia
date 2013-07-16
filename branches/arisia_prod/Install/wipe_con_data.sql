ef6a07c045f20d0956a15228a88683cd3ea2aa6f
danfranklinusa <danfranklinusa@676fd721-d978-42df-8417-bacc17f9f818>
2013-07-16 04:15:12 +0000

    Add more participant tables to truncate

--- branches/arisia_prod/Install/wipe_con_data.sql
+++ branches/arisia_prod/Install/wipe_con_data.sql
@@ -1,16 +1,17 @@
 -- this script can be run to wipe any con specific data out of a dbase
 -- it is used to build the script for building an empty dbase
-drop table save;
-drop table Roomtemp;
+drop table if exists save;
+drop table if exists Roomtemp;
 truncate table UserHasPermissionRole;
 truncate table ParticipantAvailability;
 truncate table ParticipantAvailabilityTimes;
+truncate table ParticipantAvailabilityDays;
+truncate table ParticipantHasCredential;
 truncate table ParticipantHasRole;
 truncate table ParticipantInterests;
 truncate table ParticipantOnSession;
 truncate table ParticipantSessionInterest;
 truncate table ParticipantSuggestions;
-truncate table Participants;
 truncate table CongoDump;
 truncate table Schedule;
 truncate table SessionHasFeature;
@@ -18,6 +19,9 @@ truncate table SessionHasService;
 truncate table SessionEditHistory;
 truncate table SessionHasPubChar;
 truncate table Sessions;
+truncate table Participants;
+truncate table PreviousParticipants;
+
 -- you don't want to disable constraints. Really! 
 -- you want to run down the real problem instead  
 -- because you missed zeroing a table and you need to zero it or eek!
@@ -29,4 +33,5 @@ truncate table Sessions;
 INSERT INTO `Participants` (badgeid, pubsname, password, bestway, interested, bio)
  VALUES ('brainstorm',null,'ecf65a5d41056d7dd4d548e3ef200476',null,null,null);
 INSERT INTO `UserHasPermissionRole` VALUES ('brainstorm',5);
+
 
