16f342e0b60ffc3f9b31c5b16d561e5235d13964
polszowka <polszowka@676fd721-d978-42df-8417-bacc17f9f818>
2015-11-21 21:08:12 +0000

    Update Assign Participants to newer technology and add ability to edit Notes for Program Committee field.

--- branches/arisia_prod/webpages/StaffAssignParticipants.php
+++ branches/arisia_prod/webpages/StaffAssignParticipants.php
@@ -1,4 +1,5 @@
 <?php
+//$Header$
 $title="Assign Participants";
 require_once('db_functions.php');
 require_once('StaffHeader.php');
@@ -14,14 +15,14 @@ if (isset($_POST["numrows"])) {
     }
 
 if (isset($_POST["selsess"])) { // room was selected by this form
-        $selsessionid=$_POST["selsess"];
+        $selsessionid=filter_var($_POST["selsess"], FILTER_VALIDATE_INT);
         if ($selsessionid != 0) {
           $topsectiononly=false;
           //unset($_SESSION['return_to_page']); // since edit originated with this page, do not return to another.          
         }
       }
     elseif (isset($_GET["selsess"])) { // room was select by external page such as a report
-        $selsessionid=$_GET["selsess"];
+        $selsessionid=filter_var($_GET["selsess"], FILTER_VALIDATE_INT);
         $topsectiononly=false;
         }
     else {
@@ -40,7 +41,7 @@ if (!$Sresult=mysql_query($query,$link)) {
     staff_footer();
     exit();
     }
-echo "<FORM name=\"selsesform\" class=\"form-inline\" method=POST action=\"StaffAssignParticipants.php\">\n";
+echo "<FORM id=\"selsesformtop\" name=\"selsesform\" class=\"form-inline\" method=POST action=\"StaffAssignParticipants.php\">\n";
 echo "<DIV><LABEL for=\"selsess\">Select Session:</LABEL>\n";
 echo "<SELECT id=\"sessionDropdown\" class=\"span6\"name=\"selsess\">\n";
 echo "     <OPTION value=0".(($selsessionid==0)?"selected":"").">Select Session</OPTION>\n";
@@ -59,41 +60,15 @@ if ($topsectiononly) {
     staff_footer();
     exit();
     }
-$query = <<<EOD
-SELECT title, progguiddesc, persppartinfo, notesforpart, notesforprog FROM Sessions
-WHERE sessionid=$selsessionid
+$queryArray["sessionInfo"]=<<<EOD
+SELECT
+		sessionid, title, progguiddesc, persppartinfo, notesforpart, notesforprog
+	FROM
+		Sessions
+	WHERE
+		sessionid=$selsessionid
 EOD;
-if (!$result=mysql_query($query,$link)) {
-    $message=$query."<BR>Error querying database. Unable to continue.<BR>";
-    echo "<P class\"alert alert-error\">".$message."\n";
-    staff_footer();
-    exit();
-    }
-echo "<HR><H2>$selsessionid - ".htmlspecialchars(mysql_result($result,0,"title"))."</H2>";    
-echo "<P class=\"label\">Program Guide Text:</p>";
-echo "<span class=\"\">";
-echo htmlspecialchars(mysql_result($result,0,"progguiddesc"));
-echo "\n</span>";
-if (mysql_result($result,0,"persppartinfo") != "") {
-  echo "<P class=\"label\">Prospective Participant Info:";
-  echo "<P class=\"\">";
-}
-echo htmlspecialchars(mysql_result($result,0,"persppartinfo"));
-echo "\n";
-if (mysql_result($result,0,"notesforpart") != "") {
-  echo "<P class=\"label\">Notes for Participant:";
-  echo "<P class=\"text-info\">";
-}
-echo htmlspecialchars(mysql_result($result,0,"notesforpart"));
-echo "\n";
-if (mysql_result($result,0,"notesforprog") != "") {
-  echo "<P class=\"label\">Notes for Program Staff:";
-  echo "<P class=\"text-alert\">";
-}
-echo htmlspecialchars(mysql_result($result,0,"notesforprog"));
-echo "\n";
-echo "<HR>\n";
-$query = <<<EOD
+$queryArray["participantInterest"]=<<<EOD
 SELECT
 		POS.badgeid AS posbadgeid,
 		COALESCE(POS.moderator, 0) AS moderator,
@@ -115,7 +90,7 @@ SELECT
 						) AS R USING (badgeid)
 		LEFT JOIN	ParticipantSessionInterest AS PSI ON R.badgeid = PSI.badgeid AND R.sessionid = PSI.sessionid
 		LEFT JOIN	ParticipantOnSession AS POS ON R.badgeid = POS.badgeid AND R.sessionid = POS.sessionid
-		LEFT JOIN	ParticipantHasRole AS PHR ON P.badgeid = PHR.badgeid and PHR.roleid = 10
+		LEFT JOIN	ParticipantHasRole AS PHR ON P.badgeid = PHR.badgeid and PHR.roleid = 10 /* moderator */
 	WHERE
 			POS.sessionid=$selsessionid
 		OR	POS.sessionid is null
@@ -125,113 +100,46 @@ SELECT
 		rank ASC,
 		P.pubsname ASC;
 EOD;
-if (!$result=mysql_query($query,$link)) {
+$queryArray["otherParticipants"]=<<<EOD
+SELECT
+        P.pubsname,
+        P.badgeid,
+        CD.lastname
+    FROM
+        Participants P
+    JOIN
+        CongoDump CD USING(badgeid)
+    WHERE
+            P.interested=1
+        AND NOT EXISTS (
+			SELECT *
+				FROM
+					ParticipantSessionInterest
+				WHERE
+						sessionid=$selsessionid
+					AND badgeid = P.badgeid
+            )
+    ORDER BY
+            IF(instr(P.pubsname,CD.lastname)>0,CD.lastname,substring_index(P.pubsname,' ',-1)),CD.firstname
+EOD;
+if (($resultXML=mysql_query_XML($queryArray))===false) {
     $message=$query."<BR>Error querying database. Unable to continue.<BR>";
     echo "<P class\"alert alert-error\">".$message."\n";
     staff_footer();
     exit();
     }
-$query = <<<EOD
-SELECT
-            P.pubsname,
-            P.badgeid,
-            CD.lastname
-    FROM
-            Participants P
-    JOIN
-            CongoDump CD USING(badgeid)
-    WHERE
-            P.interested=1
-      AND   P.badgeid not in
-                   (Select badgeid
-                        from ParticipantSessionInterest
-                       where sessionid=$selsessionid)
-    ORDER BY
-            IF(instr(P.pubsname,CD.lastname)>0,CD.lastname,substring_index(P.pubsname,' ',-1)),CD.firstname
-EOD;
-if (!$Presult=mysql_query($query,$link)) {
-    $message=$query."<BR>Error querying database. Unable to continue.<BR>";
-    RenderError($title,$message);
-    exit();
-    }
-$i=0;
-$modid=0;
-while ($bigarray[$i] = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    if ($bigarray[$i]["moderator"]==1) {
-        $modid=$bigarray[$i]["badgeid"];
-        }
-    $i++;
-    }
-$numrows=$i;
-echo "<div class=\"row-fluid\">";
-echo "<FORM name=\"selsesform\" class=\"form-inline\" method=POST action=\"StaffAssignParticipants.php\">\n";
-echo "<BUTTON type=\"submit\" name=\"update\" class=\"btn btn-primary pull-right\">Update</BUTTON>\n";
-echo "<LABEL for=\"moderator\" class=\"radio\">";
-echo "<INPUT type=\"radio\" name=\"moderator\" id=\"moderator\" value=\"0\"".(($modid==0)?"checked":"").">";
-echo "No Moderator Selected</LABEL>";
-echo "<TABLE class=\"table table-condensed\">\n";
-for ($i=0;$i<$numrows;$i++) {
-    echo "   <TR ";
-    echo (($bigarray[$i]["moderator"])?"class=\"success\"":"");
-    echo ">\n";
-    echo "      <TD style=\"width: 7em;\"><LABEL class=\"checkbox\"><INPUT type=\"checkbox\" name=\"asgn".$bigarray[$i]["badgeid"]."\" ";
-    echo (($bigarray[$i]["posbadgeid"])?"checked":"")." value=\"1\">Assigned</LABEL>";
-    echo "<INPUT type=\"hidden\" name=\"row$i\" value=\"".$bigarray[$i]["badgeid"]."\">";
-    echo "<INPUT type=\"hidden\" name=\"wasasgn".$bigarray[$i]["badgeid"]."\" value=\"";
-    echo ((isset($bigarray[$i]["posbadgeid"]))?1:0)."\">";
-    echo "         </TD>\n";
-    echo "      <TD class=\"\">".$bigarray[$i]["badgeid"]."</TD>\n";
-    echo "      <TD class=\"\">".$bigarray[$i]["pubsname"]."&nbsp;&nbsp;&nbsp;<button type=\"button\" rel=\"popover\" class=\"btn btn-info btn-mini\" data-content=\"".htmlspecialchars($bigarray[$i]["bio"])."\" data-original-title=\"Bio for ".$bigarray[$i]["pubsname"]."\">Bio</button></TD>\n";
-    echo "      <TD class=\"\">Rank: ".(($bigarray[$i]["rank"]==99)?"None":$bigarray[$i]["rank"])."</TD>\n";
-	if ($bigarray[$i]["willmoderate"]==1 && $bigarray[$i]["roleid"]) {
-			echo "      <td title=\"Volunteered to moderate this panel and in general\">Mod this or any</td>\n";
-			}
-		elseif ($bigarray[$i]["willmoderate"]==1) {
-			echo "      <td title=\"Volunteered to moderate this panel, but not in general\">Mod this</td>\n";
-			}
-		elseif ($bigarray[$i]["roleid"]) {
-			echo "      <td title=\"Volunteered to moderate in general, but not this panel\">Mod any but not this</td>\n";
-			}
-		else {
-			echo "      <td>&nbsp;</td>\n";
-			}
-    echo "      </TR>\n";
-    echo "   <TR ";
-    echo (($bigarray[$i]["moderator"])?"class=\"success\"":"");
-    echo ">\n";
-    echo "      <TD class=\"\"><LABEL class=\"radio\"><INPUT type=\"radio\" name=\"moderator\" id=\"moderator\"value=\"".$bigarray[$i]["badgeid"]."\" ";
-    echo (($bigarray[$i]["moderator"])?"checked":"").">Moderator</TD>";
-    echo "      <TD colspan=4 class=\"alert alert-info\">".htmlspecialchars($bigarray[$i]["comments"]);
-    echo "</TD>\n";
-    echo "   <TR ";
-    echo (($bigarray[$i]["moderator"])?"class=\"success\"":"");
-    echo ">\n";
-	if ($bigarray[$i]["staff_notes"]) {
-    echo "   <TR ";
-    echo (($bigarray[$i]["moderator"])?"class=\"success\"":"");
-    echo ">\n";
-		echo "      <td>&nbsp;</td><td colspan=\"4\" class=\"alert alert-success\">".htmlspecialchars($bigarray[$i]["staff_notes"]);
-		echo "</td></tr>\n";
-		}
-    echo "   <TR><TD colspan=6>&nbsp;</TD></TR>\n";
-    }
-echo "</TABLE>";
-echo "<INPUT type=\"hidden\" name=\"selsess\" value=\"$selsessionid\">\n";
-echo "<INPUT type=\"hidden\" name=\"numrows\" value=\"$numrows\">\n";
-echo "<INPUT type=\"hidden\" name=\"wasmodid\" value=\"$modid\">\n";
-echo "<DIV class=\"clearfix\"><BUTTON type=\"submit\" name=\"update\" class=\"btn btn-primary pull-right\">Update</BUTTON></DIV>\n";
-echo "<HR>\n";
-echo "<DIV><LABEL for=\"asgnpart\">Assign participant not indicated as interested or invited.</LABEL><BR>\n";
-echo "<SELECT id=\"partDropdown\" name=\"asgnpart\">\n";
-echo "     <OPTION value=0 selected>Assign Participant</OPTION>\n";
-while (list($pubsname,$badgeid)= mysql_fetch_array($Presult, MYSQL_NUM)) {
-    echo "     <OPTION value=\"".$badgeid."\">";
-    echo htmlspecialchars($pubsname)." - ";
-    echo htmlspecialchars($badgeid)."</OPTION>\n";
-    }
-echo "</SELECT>\n";
-echo "<BUTTON type=\"submit\" name=\"update\" class=\"btn btn-primary\">Add</BUTTON></DIV>\n";
-
-echo "</FORM><BUTTON id=\"BioBtn\" type=\"button\" class=\"btn btn-info\" data-loading-text=\"Fetching Bio...\">Show Bio</BUTTON>&nbsp;</div>\n";
+$parametersNode = $resultXML->createElement("parameters");
+$docNode = $resultXML->getElementsByTagName("doc")->item(0);
+$parametersNode = $docNode->appendChild($parametersNode);
+if (may_I('EditSesNtsAsgnPartPg')) {
+	$parametersNode->setAttribute("editSessionNotes", "true");
+}
+echo($resultXML->saveXML()); //for debugging only
+$xsl = new DomDocument;
+$xsl->load('xsl/StaffAssignParticipants_sessionInfo.xsl');
+$xslt = new XsltProcessor();
+$xslt->importStylesheet($xsl);
+$html = $xslt->transformToXML($resultXML);
+echo(mb_ereg_replace("<(div|iframe|script|textarea)([^>]*/[ ]*)>", "<\\1\\2></\\1>", $html, "i"));
 staff_footer();
 ?>
---
+++ branches/arisia_prod/webpages/xsl/StaffAssignParticipants_sessionInfo.xsl
@@ -0,0 +1,193 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<!--
+    $Header$
+	StaffAssignParticipants_sessionInfo.xsl
+	Created by Peter Olszowka on 2015-10-16; Updated 2015-10-16
+	Copyright (c) 2011-2015 Peter Olszowka. All rights reserved.
+-->
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+	<xsl:output encoding="UTF-8" indent="yes" method="xml" />
+	<xsl:template match="/">
+		<xsl:variable name="editSessionNotes" select="doc/parameters/@editSessionNotes = 'true'" />
+		<hr />
+		<h2>
+			<xsl:value-of select="doc/query[@queryName='sessionInfo']/row/@sessionid" />
+			<xsl:text> - </xsl:text>
+			<xsl:value-of select="doc/query[@queryName='sessionInfo']/row/@title" />
+		</h2>
+		<form id="selsesform" name="selsesform" class="form-inline" method="post" action="StaffAssignParticipants.php">
+			<xsl:if test="doc/query[@queryName='sessionInfo']/row/@progguiddesc">
+				<div class="row-fluid stAsPa-sesInfRow">
+					<div class="span12">
+						<span class="label">Program Guide Text:</span>
+						<span><xsl:value-of select="doc/query[@queryName='sessionInfo']/row/@progguiddesc" /></span>
+					</div>
+				</div>
+			</xsl:if>
+			<xsl:if test="doc/query[@queryName='sessionInfo']/row/@persppartinfo">
+				<div class="row-fluid stAsPa-sesInfRow">
+					<div class="span12">
+						<span class="label">Prospective Participant Info:</span>
+						<span><xsl:value-of select="doc/query[@queryName='sessionInfo']/row/@persppartinfo" /></span>
+					</div>
+				</div>
+			</xsl:if>
+			<xsl:if test="doc/query[@queryName='sessionInfo']/row/@notesforpart">
+				<div class="row-fluid stAsPa-sesInfRow">
+					<div class="span12">
+						<span class="label">Notes for Participant:</span>
+						<span><xsl:value-of select="/doc/query[@queryName='sessionInfo']/row/@notesforpart" /></span>
+					</div>
+				</div>
+			</xsl:if>
+			<xsl:if test="doc/query[@queryName='sessionInfo']/row/@notesforprog or $editSessionNotes">
+				<div class="row-fluid stAsPa-sesInfRow">
+					<div class="span12">
+						<span class="label">Notes for Program Staff:</span>
+						<xsl:if test="$editSessionNotes">
+							<button type="button" id="editNPS_BUT" class="btn btn-mini">Edit</button>
+						</xsl:if>
+						<span id="NPS_SPN"><xsl:value-of select="doc/query[@queryName='sessionInfo']/row/@notesforprog" /></span>
+					</div>
+				</div>
+			</xsl:if>
+			<hr />
+			<div class="row-container">
+				<div class="row-fluid">
+					<div class="span4">
+						<label for="no-moderator" class="radio">
+							<input type="radio" name="moderator" id="no-moderator" value="0">
+								<xsl:if test="count(doc/query[@queryName='participantInterest']/row[@moderator=1]/@moderator) = 0">
+									<xsl:attribute name="checked">checked</xsl:attribute>
+								</xsl:if>
+							</input>
+							<xsl:text>No Moderator Selected</xsl:text>
+						</label>
+					</div>
+					<div class="span2 offset3">
+						<button type="submit" name="update" class="btn btn-primary">Update</button>
+					</div>
+				</div>
+			</div>
+			<xsl:apply-templates select="doc/query[@queryName='participantInterest']/row" />
+			<input type="hidden" name="selsess" value="{doc/query[@queryName='sessionInfo']/row/@sessionid}" />
+			<input type="hidden" name="numrows" value="{count(doc/query[@queryName='participantInterest']/row)}" />
+			<input type="hidden" name="wasmodid" value="{doc/query[@queryName='participantInterest']/row[@moderator=1]/@badgeid}" />
+			<hr />
+			<div class="row-container">
+				<label for="partDropdown">Assign participant not indicated as interested or invited.</label>
+			</div>
+			<div class="row-container">
+				<div class="row-fluid">
+					<div class="span3">
+						<select id="partDropdown" name="asgnpart">
+							<option value="0" selected="selected">Assign Participant</option>
+							<xsl:apply-templates select="doc/query[@queryName='otherParticipants']/row" />
+						</select>
+					</div>
+					<div class="span1">
+						<button type="submit" name="update" class="btn btn-primary">Add</button>
+					</div>
+					<div class="span2">
+						<button type="button" id="BioBtn" class="btn btn-info" data-loading-text="Fetching Bio...">Show Bio</button>
+					</div>
+				</div>
+			</div>
+		</form>
+	</xsl:template>
+	<xsl:template match="doc/query[@queryName='participantInterest']/row">
+		<div class="row-container">
+			<div>
+				<xsl:choose>
+					<xsl:when test="@moderator='1'"><xsl:attribute name="class">row-fluid success-bg</xsl:attribute></xsl:when>
+					<xsl:otherwise><xsl:attribute name="class">row-fluid</xsl:attribute></xsl:otherwise>
+				</xsl:choose>
+				<div class="span2">
+					<label class="checkbox">
+						<input type="checkbox" value="1">
+							<xsl:attribute name="name">asgn<xsl:value-of select="@badgeid" /></xsl:attribute>
+							<xsl:if test="@posbadgeid">
+								<xsl:attribute name="checked">checked</xsl:attribute>
+							</xsl:if>
+						</input>
+						<xsl:text>Assigned</xsl:text>
+					</label>
+					<input type="hidden" name="row{position()}" value="{@badgeid}" />
+					<input type="hidden" name="wasasgn{@badgeid}">
+						<xsl:choose>
+							<xsl:when test="@posbadgeid"><xsl:attribute name="value">1</xsl:attribute></xsl:when>
+							<xsl:otherwise><xsl:attribute name="value">0</xsl:attribute></xsl:otherwise>
+						</xsl:choose>
+					</input>									   
+				</div>
+				<div class="span1"><xsl:value-of select="@badgeid" /></div>
+				<div class="span4">
+					<xsl:value-of select="@pubsname" />
+					<xsl:text disable-output-escaping="yes">&amp;nbsp;&amp;nbsp;&amp;nbsp;</xsl:text>
+					<button type="button" rel="popover" class="btn btn-info btn-mini" data-content="{@bio}" data-original-title="Bio for {@pubsname}">Bio</button>
+				</div>
+				<div class="span2">
+					<xsl:text>Rank: </xsl:text>
+					<xsl:choose>
+						<xsl:when test="@rank='99'"><xsl:text>None</xsl:text></xsl:when>
+						<xsl:otherwise><xsl:value-of select="@rank" /></xsl:otherwise>
+					</xsl:choose>
+				</div>
+				<div class="span3">
+					<xsl:choose>
+						<xsl:when test="@willmoderate='1' and @roleid">
+							<xsl:attribute name="title">Volunteered to moderate this panel and in general</xsl:attribute>
+							<xsl:text>Mod this or any</xsl:text>
+						</xsl:when>
+						<xsl:when test="@willmoderate='1'">
+							<xsl:attribute name="title">Volunteered to moderate this panel, but not in general</xsl:attribute>
+							<xsl:text>Mod this</xsl:text>
+						</xsl:when>
+						<xsl:when test="@roleid">
+							<xsl:attribute name="title">Volunteered to moderate in general, but not this panel</xsl:attribute>
+							<xsl:text>Mod any but not this</xsl:text>
+						</xsl:when>
+						<xsl:otherwise><xsl:text disable-output-escaping="yes">&amp;nbsp;</xsl:text></xsl:otherwise>
+					</xsl:choose>
+				</div>
+			</div>
+			<div>
+				<xsl:choose>
+					<xsl:when test="@moderator='1'"><xsl:attribute name="class">row-fluid success-bg</xsl:attribute></xsl:when>
+					<xsl:otherwise><xsl:attribute name="class">row-fluid</xsl:attribute></xsl:otherwise>
+				</xsl:choose>
+				<div class="span2">
+					<label class="radio">
+						<input type="radio" name="moderator" id="moderator-{position()}" value="{@badgeid}">
+							<xsl:if test="@moderator='1'">
+								<xsl:attribute name="checked">checked</xsl:attribute>
+							</xsl:if>
+						</input>
+						<xsl:text>Moderator</xsl:text>
+					</label>
+				</div>
+				<div>
+					<xsl:choose>
+						<xsl:when test="@moderator='1'"><xsl:attribute name="class">span10</xsl:attribute></xsl:when>
+						<xsl:otherwise><xsl:attribute name="class">span10 info-bg</xsl:attribute></xsl:otherwise>
+					</xsl:choose>
+					<xsl:value-of select="@comments"/>
+				</div>	
+			</div>
+			<xsl:if test="@staff_notes">
+				<div>
+					<xsl:choose>
+						<xsl:when test="@moderator='1'"><xsl:attribute name="class">row-fluid success-bg</xsl:attribute></xsl:when>
+						<xsl:otherwise><xsl:attribute name="class">row-fluid</xsl:attribute></xsl:otherwise>
+					</xsl:choose>
+					<div class="span10 offset2 warning-bg">
+						<xsl:value-of select="@staff_notes"/>
+					</div>	
+				</div>
+			</xsl:if>
+		</div>
+	</xsl:template>
+	<xsl:template match="doc/query[@queryName='otherParticipants']/row">
+		<option value="{@badgeid}"><xsl:value-of select="@pubsname" /><xsl:text> - </xsl:text><xsl:value-of select="@badgeid" /></option>
+	</xsl:template>
+</xsl:stylesheet>
