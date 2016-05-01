db72d6a172cd45b27efa4da322982d717bdeaa1c
polszowka <polszowka@676fd721-d978-42df-8417-bacc17f9f818>
2016-05-01 05:35:59 +0000

    Implement tracking of history of changes to participants on sessions.

--- branches/Track_changes/webpages/StaffAssignParticipants_FNC.php
+++ branches/Track_changes/webpages/StaffAssignParticipants_FNC.php
@@ -1,7 +1,15 @@
 <?php 
 function SubmitAssignParticipants() {
+	// NOTES
+	// U - added to panel not as moderator
+	// V - added to panel as moderator
+	// W - removed from panel was moderator
+	// X - removed from panel was not moderator
+	// Y - changed to moderator
+	// Z - changed to not moderator
     global $link;
 	$message="";
+	$userbadgeid = $_SESSION['badgeid'];
 //    print_r($_POST);
     $asgnpart=filter_var($_POST["asgnpart"], FILTER_VALIDATE_INT);
     $numrows=$_POST["numrows"];
@@ -9,35 +17,56 @@ function SubmitAssignParticipants() {
     $wasmodid=$_POST["wasmodid"];
     $selsessionid=filter_var($_POST["selsess"], FILTER_VALIDATE_INT);
 	// use XSL position for row#.  That starts at 1.
+	// If moderator is changing, must inactivate old one before inserting new one because trigger will not
+	//     permit more than 1 moderator for the session, even momentarily.
+	$firstQueryArray = array("SET AUTOCOMMIT = 0;", "START TRANSACTION;", "SET @mynow = NOW();");
+	$queryArray = [];
     for ($i=1; $i<=$numrows; $i++) {
-        $badgeid=filter_var($_POST["row$i"], FILTER_VALIDATE_INT);
+        $badgeid=mysql_real_escape_string($_POST["row$i"], $link);
         $ismod=($moderator==$badgeid);
         $isasgn=(isset($_POST["asgn$badgeid"]) or $ismod);
         $wasasgn=($_POST["wasasgn$badgeid"]==1);
         $wasmod=($wasmodid==$badgeid);
-//echo "i: $i | isasgn: $isasgn | wasasgn: $wasasgn | ismod: $ismod | wasmod: $wasmod <BR>\n";        
-        if (!$isasgn and $wasasgn) {
-                $query="DELETE FROM ParticipantOnSession where badgeid=\"$badgeid\" ";
-                $query.="and sessionid=$selsessionid";
+//echo "i: $i | isasgn: $isasgn | wasasgn: $wasasgn | ismod: $ismod | wasmod: $wasmod <BR>\n";
+		if ($wasmod and !$ismod) { // W or Z
+				$firstQueryArray[] = "UPDATE ParticipantOnSessionHistory SET inactivatedbybadgeid=\"$userbadgeid\", inactivatedts=@mynow WHERE " .
+				    "sessionid=$selsessionid AND badgeid=\"$badgeid\" AND inactivatedts IS NULL;";
+				if ($isasgn) { // Z
+					$firstQueryArray[] = "INSERT INTO ParticipantOnSessionHistory (badgeid, sessionid, moderator, createdts, createdbybadgeid) " .
+					    "VALUES (\"$badgeid\", $selsessionid, 0, @mynow, \"$userbadgeid\");";
+					}
+				}
+			else {
+				if ($wasasgn and (!$isasgn or ($isasgn and $ismod and !$wasmod))) { // X or Y (inact only)
+					$queryArray[] = "UPDATE ParticipantOnSessionHistory SET inactivatedbybadgeid=\"$userbadgeid\", inactivatedts=@mynow WHERE " .
+					    "sessionid=$selsessionid AND badgeid=\"$badgeid\" AND inactivatedts IS NULL;";
+					}
+				if ($isasgn and (!$wasasgn or (!$wasmod and $ismod))) { // V or U or Y (ins only)
+					$queryArray[] = "INSERT INTO ParticipantOnSessionHistory (badgeid, sessionid, moderator, createdts, createdbybadgeid) " .
+						"VALUES (\"$badgeid\", $selsessionid, " . ($ismod ? "1" : "0") . ", @mynow, \"$userbadgeid\");";
+					}
                 }
-            elseif (!$wasasgn and $isasgn) {
-                $query="INSERT INTO ParticipantOnSession set badgeid=\"$badgeid\", "; 
-                $query.="sessionid=$selsessionid, moderator=".($ismod?1:0);
-                }
-            elseif (($ismod and !$wasmod) or (!$ismod and $wasmod)) {
-                $query="UPDATE ParticipantOnSession set moderator=".($ismod?1:0);
-                $query.=" WHERE badgeid=\"$badgeid\" and sessionid=$selsessionid";
-                }
-            else {
-                continue;
-                }
-        if (!mysql_query($query, $link)) {
-            $message=$query."<BR>Error updating database.<BR>";
-            echo "<P class=\"alert alert-error\">".$message."\n";
-            staff_footer();
-            exit();
-            }
-        }
+		}
+	$queryArray[] = "COMMIT;";
+	$queryArray[] = "SET AUTOCOMMIT = 0;";
+	foreach ($firstQueryArray as $query) {
+		if (!mysql_query($query, $link)) {
+			$foo = mysql_error($link);
+			$message=$query."<BR>Error updating database.<BR>";
+			echo "<P class=\"alert alert-error\">".$message."\n";
+			staff_footer();
+			exit();
+			}
+		}
+	foreach ($queryArray as $query) {
+		if (!mysql_query($query, $link)) {
+			$foo = mysql_error($link);
+			$message=$query."<BR>Error updating database.<BR>";
+			echo "<P class=\"alert alert-error\">".$message."\n";
+			staff_footer();
+			exit();
+			}
+		}
 	$message="Participant assignments updated. ";	
     if ($asgnpart!=0) {
         $query="INSERT INTO ParticipantSessionInterest SET badgeid=\"".$asgnpart."\", ";
