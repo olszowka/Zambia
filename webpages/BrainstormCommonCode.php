51b786ab6c9ebfeb4c9bc25ed8ca41113907ac58
polszowka <olszowka@users.noreply.github.com>
2016-06-18 21:59:34 +0000

    Merge recent changes from trunk to Arisia_prod

--- webpages/BrainstormCommonCode.php
+++ webpages/BrainstormCommonCode.php
@@ -6,7 +6,7 @@
     global $badgeid;
     $badgeid=$_SESSION['badgeid'];
     $_SESSION['role']="Brainstorm";
-    if (!(may_I("public_login"))) {
+    if (!(may_I("public_login") || may_I("Participant") || may_I("Staff"))) {
         $message="You are not authorized to access this page.";
         require ('login.php');
         exit();
