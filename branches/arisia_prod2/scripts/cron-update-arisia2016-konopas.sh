d313f5ecd212d100eebfb5d31e9c130d396afcc8
polszowka <polszowka@676fd721-d978-42df-8417-bacc17f9f818>
2016-06-21 03:48:46 +0000

    Get desired Arisia-specific variations into branch.

---
+++ branches/arisia_prod2/scripts/cron-update-arisia2016-konopas.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#	$Header$
+#	Copyright (c) 2011-2016 Peter Olszowka. All rights reserved. See copyright document for more details.
+
+ts=`date '+%Y-%m-%d_%H:%M'`
+cm="konopas.appcache"
+raw="data/arisia2016.js"
+tgt="$raw.gz"
+tmp="$tgt.$ts"
+
+zambia_url="http://zambia.arisia.org/konOpas.php"
+# update_url="https://konopas-server.appspot.com/arisia2015/update_prog"
+
+cd /home/hosting/public_html/schedule.arisia.org/
+
+curl "$zambia_url" > "$raw" 2>/dev/null
+
+gzip -c "$raw" > "$tmp" 2>/dev/null
+
+if ! zcmp "$tmp" "$tgt" >/dev/null 2>&1
+then
+	cp "$tmp" "$tgt"
+	d=`date`
+	sed -i "s/^# .*/# $d/" $cm 2>/dev/null
+#	curl "$update_url" >/dev/null 2>&1
+else
+	rm "$tmp"
+fi
