6d13b4bd50e87b137a463b4e38436328077886be
polszowka <polszowka@676fd721-d978-42df-8417-bacc17f9f818>
2016-07-30 02:42:27 +0000

    On "My Profile" page, genericize references to registration system and make URL to access it a configurable parameter.

--- trunk/webpages/my_contact.php
+++ trunk/webpages/my_contact.php
@@ -1,7 +1,8 @@
 <?php
-	// my_contact.php
-	// Created by Peter Olszowka on ?; Updated 2015-08-29
-	// Copyright (c) 2008-2015 Peter Olszowka. All rights reserved.
+	// $Header$
+	// Created by Peter Olszowka
+	// Copyright (c) 2008-2016 Peter Olszowka. All rights reserved.
+	// See copyright document for more details.
     global $participant, $message, $message_error, $message2, $congoinfo;
     $title="My Profile";
     require ('PartCommonCode.php'); // initialize db; check login;
@@ -42,9 +43,10 @@ EOD;
 		$optionsNode->setAttribute("enable_bestway_question",1);
 	$optionsNode->setAttribute("maxBioLen",MAX_BIO_LEN);
 	$optionsNode->setAttribute("enableBioEdit",may_I('EditBio'));
+	$optionsNode->setAttribute("reg_url", REG_URL);
 	participant_header($title);
 	$resultXML = appendCustomTextArrayToXML($resultXML);
-	//echo($resultXML->saveXML()); //for debugging only
+	echo(mb_ereg_replace("<(div|iframe|script|textarea)([^>]*/[ ]*)>", "<\\1\\2></\\1>", $resultXML->saveXML(), "i")); //for debugging only
 	$xsl = new DomDocument;
 	$xsl->load('xsl/my_profile.xsl');
 	$xslt = new XsltProcessor();
