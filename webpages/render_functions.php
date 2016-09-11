3c9a1ad9a00150acb98e3cc578d3c043c9dac7ef
polszowka <olszowka@users.noreply.github.com>
2016-09-11 21:20:38 +0000

    Add session status to search sessions results.

--- webpages/render_functions.php
+++ webpages/render_functions.php
@@ -10,7 +10,7 @@ function RenderPrecis($result,$showlinks) {
 	echo "<p>If a room name and time are listed, then the session is on the schedule; otherwise, not.</p>";
 	echo "<hr>\n";
 	echo "<table class=\"table table-condensed\">\n";
-	while (list($sessionid,$trackname,$typename,$title,$duration,$estatten,$progguiddesc,$persppartinfo,$roomname,$starttime)
+	while (list($sessionid,$trackname,$typename,$title,$duration,$estatten,$progguiddesc,$persppartinfo,$starttime,$roomname,$statusname)
 		 =mysql_fetch_array($result, MYSQL_NUM)) {
 		echo "<tr>\n";
 		echo "  <td rowspan=\"3\" class=\"border0000\" id=\"sessidtcell\" style=\"font-weight:bold\">";
@@ -41,13 +41,14 @@ function RenderPrecis($result,$showlinks) {
 			else
 				echo "&nbsp;";
 		echo "</td>\n";
+		echo "    <td class=\"border0000\" style=\"font-weight:bold\">$statusname</td>\n";
 		echo "    <td class=\"border0000\" style=\"font-weight:bold\"><a href=\"SessionHistory.php?selsess=$sessionid\">History</a></td>\n";
 		echo "</tr>\n";
-		echo "<tr><td colspan=\"7\" class=\"border0010\">".htmlspecialchars($progguiddesc,ENT_NOQUOTES)."</td></tr>\n";
+		echo "<tr><td colspan=\"8\" class=\"border0010\">".htmlspecialchars($progguiddesc,ENT_NOQUOTES)."</td></tr>\n";
 		if ($persppartinfo) {
-			echo "<tr><td colspan=\"7\" class=\"border0000\"><span class=\"alert\" style=\"padding: 0\">".htmlspecialchars($persppartinfo,ENT_NOQUOTES)."</span></td></tr>\n";
+			echo "<tr><td colspan=\"8\" class=\"border0000\"><span class=\"alert\" style=\"padding: 0\">".htmlspecialchars($persppartinfo,ENT_NOQUOTES)."</span></td></tr>\n";
 			}
-		echo "<tr><td colspan=\"7\" class=\"border0020\">&nbsp;</td></tr>\n";
+		echo "<tr><td colspan=\"8\" class=\"border0020\">&nbsp;</td></tr>\n";
 		}
 	echo "</table>\n";
 }
