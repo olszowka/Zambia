1dd340eee4d5a3fd57cc914f8b4a427ec4fdefbd
Peter Olszowka <polszowka@cargurus.com>
2017-12-16 23:32:51 -0500

    Change CSV report of bios for pubs to include only Program Participants and Event Organizers from published schedule.

--- webpages/pubsbiocsvreport.php
+++ webpages/pubsbiocsvreport.php
@@ -1,20 +1,21 @@
 <?php
+//	Copyright (c) 2011-2017 Peter Olszowka. All rights reserved. See copyright document for more details.
 require_once('db_functions.php');
 require_once('StaffCommonCode.php'); //reset connection to db and check if logged in
 $query=<<<EOD
 SELECT
-        P.badgeid, CD.lastname, CD.firstname,
-	    CD.badgename, P.pubsname, P.bio 
-	FROM
-	    Participants P JOIN
-	    CongoDump CD USING (badgeid) JOIN
-	    (SELECT DISTINCT badgeid 
-	       FROM ParticipantOnSession POS JOIN 
-	            Schedule SCH USING (sessionid)
-	     ) as X
-	   USING (badgeid) 
-	ORDER BY
-	    IF(instr(P.pubsname,CD.lastname)>0,CD.lastname,substring_index(P.pubsname,' ',-1)),CD.firstname
+        P.badgeid, CD.lastname, CD.firstname, CD.badgename, P.pubsname, P.bio
+    FROM
+             Participants P
+        JOIN CongoDump CD USING (badgeid)
+    WHERE
+            EXISTS ( SELECT *
+                FROM
+                    UserHasPermissionRole UHPR
+                WHERE
+                        UHPR.badgeid = P.badgeid
+                    AND UHPR.permroleid IN (3, 6) /* Program Participant, Event Organizer */
+                )
 EOD;
 // order by: if lastname is part of pubsname, order by it, otherwise, order by last word/token in pubsname
 if (!$result=mysql_query($query,$link)) {
