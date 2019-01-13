0da2155a49a9679360a14e9179d55babf77340e8
Peter Olszowka <polszowka@cargurus.com>
2019-01-13 00:32:20 -0500

    Partial work transforming CSV reports to new report engine.

--- webpages/reports/eventparticipantscsv.php
+++ webpages/reports/eventparticipantscsv.php
@@ -1,16 +1,15 @@
 <?php
-//	Copyright (c) 2011-2018 Peter Olszowka. All rights reserved. See copyright document for more details.
-// Report Name: Event Participants
-// Report Description: Export CSV file of event (division) schedule with participants
-// Report Categories: Reports downloadable as CSVs: 50
-require_once('db_functions.php');
-require_once('StaffCommonCode.php'); //reset connection to db and check if logged in
-$ConStartDatim=CON_START_DATIM; // make it a variable so it can be substituted
-$query = "SET group_concat_max_len=25000";
-if (!$result = mysqli_query_exit_on_error($query)) {
-    exit(); // should have exited already
-}
-$query=<<<EOD
+// Copyright (c) 2011-2019 Peter Olszowka. All rights reserved. See copyright document for more details.
+$report = [];
+$report['name'] = 'Event Participants';
+$report['description'] = 'Export CSV file of event (division) schedule with participants';
+$report['categories'] = array(
+    'Reports downloadable as CSVs' => 50
+);
+$report['csv_output'] = true;
+$report['group_concat_expand'] = false;
+$report['queries'] = [];
+$report['queries']['master'] =<<<'EOD'
 SELECT
         S.title, S.sessionid, R.roomname,
         DATE_FORMAT(ADDTIME('$ConStartDatim', SCH.starttime),'%a') as day, 
@@ -36,37 +35,5 @@ SELECT
     WHERE
         S.divisionid = 3; /* Events */
 EOD;
-if (!$result = mysqli_query_exit_on_error($query)) {
-    exit(); // should have exited already
-}
-if (mysqli_num_rows($result) == 0) {
-    $title = "Pocket Program CSV Report";
-    staff_header($title);
-    $message = "Report returned no records.";
-    echo "<p>" . $message . "\n";
-    staff_footer();
-    exit();
-}
-header('Content-disposition: attachment; filename=event_schedule_participants.csv');
-header('Content-type: text/csv');
-echo "title,sessionid,room,day,time,duration,track,\"pubs name\", \"last name\", \"first name\"\n";
-while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
-    $betweenValues = false;
-    foreach ($row as $value) {
-        if ($betweenValues) {
-            echo ",";
-        }
-        if (strpos($value, "\"") !== false) {
-            $value = str_replace("\"", "\"\"", $value);
-            echo "\"$value\"";
-        } elseif (strpos($value, ",") !== false or strpos($value, "\n") !== false) {
-            echo "\"$value\"";
-        } else {
-            echo $value;
-        }
-        $betweenValues = true;
-    }
-    echo "\n";
-}
-exit();
-?>
+$report['output_filename'] = 'event_schedule_participants.csv';
+$report['column_headings'] = 'title,sessionid,room,day,time,duration,track,"pubs name", "last name", "first name"';
