a15d3b0a88ac150e079f7b3ade5367b7e9b3c6c5
Peter Olszowka <polszowka@cargurus.com>
2019-01-13 00:19:32 -0500

    Partial work transforming CSV reports to new report engine.

--- webpages/reports/allroomschedcsv.php
+++ webpages/reports/allroomschedcsv.php
@@ -1,52 +1,39 @@
 <?php
-// Copyright (c) 2015-2018 Peter Olszowka. All rights reserved. See copyright document for more details.
-// Report Name: Participant Schedule by Room
-// Report Description: Export CSV file of full participant schedule by room, time
-// Report Categories: Reports downloadable as CSVs: 30
-require_once('db_functions.php');
-require_once('StaffCommonCode.php'); //reset connection to db and check if logged in
-require_once('csv_report_functions.php');
-global $title;
-$title = "Send CSV file of Program Packet Merge";
-$ConStartDatim = CON_START_DATIM; // make it a variable so it can be substituted
-$query = "SET group_concat_max_len=25000";
-if (!$result = mysqli_query_exit_on_error($query)) {
-    exit(); // should have exited already
-}
-mysqli_free_result($result);
-$query = <<<EOD
+// Copyright (c) 2015-2019 Peter Olszowka. All rights reserved. See copyright document for more details.
+$report = [];
+$report['name'] = 'Participant Schedule by Room';
+$report['description'] = 'Export CSV file of full participant schedule by room, time';
+$report['categories'] = array(
+    'Reports downloadable as CSVs' => 30
+);
+$report['csv_output'] = true;
+$report['group_concat_expand'] = true;
+$report['queries'] = [];
+$report['queries']['master'] =<<<'EOD'
 SELECT
-            R.roomname,
-            R.function,
-            DATE_FORMAT(ADDTIME('$ConStartDatim',starttime),'%a %l:%i %p') as 'Start Time', 
-            CASE
-                WHEN HOUR(S.duration) < 1 THEN CONCAT(DATE_FORMAT(S.duration,'%i'),'min')
-                WHEN MINUTE(S.duration)=0 THEN CONCAT(DATE_FORMAT(S.duration,'%k'),'hr')
-                ELSE CONCAT(DATE_FORMAT(S.duration,'%k'),'hr ',DATE_FORMAT(S.duration,'%i'),'min')
-                END AS 'duration',
-            T.Trackname,
-            S.sessionid,
-            S.title,
-            GROUP_CONCAT(CONCAT(P.pubsname,' (',P.badgeid,')') SEPARATOR '; ') AS 'Participants' 
+        R.roomname,
+        R.function,
+        DATE_FORMAT(ADDTIME('$ConStartDatim',starttime),'%a %l:%i %p') as 'Start Time', 
+        CASE
+            WHEN HOUR(S.duration) < 1 THEN CONCAT(DATE_FORMAT(S.duration,'%i'),'min')
+            WHEN MINUTE(S.duration)=0 THEN CONCAT(DATE_FORMAT(S.duration,'%k'),'hr')
+            ELSE CONCAT(DATE_FORMAT(S.duration,'%k'),'hr ',DATE_FORMAT(S.duration,'%i'),'min')
+            END AS 'duration',
+        T.Trackname,
+        S.sessionid,
+        S.title,
+        GROUP_CONCAT(CONCAT(P.pubsname,' (',P.badgeid,')') SEPARATOR '; ') AS 'Participants' 
     FROM
-            Sessions S
-       JOIN Schedule SCH USING (sessionid)
-       JOIN Rooms R USING (roomid)
-  LEFT JOIN ParticipantOnSession POS ON SCH.sessionid=POS.sessionid
-  LEFT JOIN Participants P ON POS.badgeid=P.badgeid
-  LEFT JOIN Tracks T ON T.trackid=S.trackid
+                Sessions S
+           JOIN Schedule SCH USING (sessionid)
+           JOIN Rooms R USING (roomid)
+      LEFT JOIN ParticipantOnSession POS ON SCH.sessionid=POS.sessionid
+      LEFT JOIN Participants P ON POS.badgeid=P.badgeid
+      LEFT JOIN Tracks T ON T.trackid=S.trackid
     GROUP BY
             SCH.scheduleid 
     ORDER BY
             R.roomname, SCH.starttime
 EOD;
-if (!$result = mysqli_query_exit_on_error($query)) {
-    exit(); // should have exited already
-}
-echo_if_zero_rows_and_exit($result);
-header('Content-disposition: attachment; filename=allroomsched.csv');
-header('Content-type: text/csv');
-echo "Room Name, Room Function, Start Time, Duration, Track, Session ID, Title, Participants\n";
-render_query_result_as_csv($result);
-exit();
-?>
+$report['output_filename'] = 'allroomsched.csv';
+$report['column_headings'] = 'Room Name, Room Function, Start Time, Duration, Track, Session ID, Title, Participants';
