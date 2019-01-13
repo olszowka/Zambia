aff6b9d0be0dc9044519a4a548e20995dbce3742
Peter Olszowka <polszowka@cargurus.com>
2019-01-13 14:57:48 -0500

    Partial work transforming CSV reports to new report engine.

--- webpages/reports/pocketprogramcsv.php
+++ webpages/reports/pocketprogramcsv.php
@@ -1,62 +1,50 @@
 <?php
-// Copyright (c) 2015-2018 Peter Olszowka. All rights reserved. See copyright document for more details.
-// Report Name: Pocket Program
-// Report Description: Export CSV file of public schedule for generating pocket program
-// Report Categories: Reports downloadable as CSVs: 80
-require_once('db_functions.php');
-require_once('StaffCommonCode.php'); //reset connection to db and check if logged in
-require_once('csv_report_functions.php');
-$ConStartDatim = CON_START_DATIM; // make it a variable so it can be substituted
-global $title;
-$title = "Pocket Program CSV Report";
-$query = "SET group_concat_max_len=25000";
-if (!$result = mysqli_query_exit_on_error($query)) {
-    exit(); // should have exited already
-}
-$query = <<<EOD
+// Copyright (c) 2009-2019 Peter Olszowka. All rights reserved. See copyright document for more details.
+$report = [];
+$report['name'] = 'Pocket Program';
+$report['description'] = 'Export CSV file of public schedule for generating pocket program';
+$report['categories'] = array(
+    'Reports downloadable as CSVs' => 80
+);
+$report['csv_output'] = true;
+$report['group_concat_expand'] = true;
+$report['queries'] = [];
+$report['queries']['master'] =<<<'EOD'
 SELECT
-            S.sessionid, 
-            DATE_FORMAT(ADDTIME('$ConStartDatim',starttime),'%a') as Day, 
-            DATE_FORMAT(ADDTIME('$ConStartDatim',starttime),'%l:%i %p') as 'Time', 
-            concat(if(left(duration,2)=00, '', 
-                      if(left(duration,1)=0, 
-                         concat(right(left(duration,2),1), 'hr '),
-                       concat(left(duration,2),'hr '))), 
-                   if(date_format(duration,'%i')=00, '', 
-                      if(left(date_format(duration,'%i'),1)=0, 
-                         concat(right(date_format(duration,'%i'),1),'min'), 
-                         concat(date_format(duration,'%i'),'min')))) Duration, 
-            roomname, 
-            trackname as TRACK, 
-	        typename as TYPE,
-            K.kidscatname,
-            title, 
-            progguiddesc as 'Long Text', 
-            group_concat(' ',pubsname, if (moderator=1,' (m)','')) as 'PARTIC' 
+        S.sessionid, 
+        DATE_FORMAT(ADDTIME('$ConStartDatim',starttime),'%a') as Day, 
+        DATE_FORMAT(ADDTIME('$ConStartDatim',starttime),'%l:%i %p') as 'Time', 
+        concat(if(left(duration,2)=00, '', 
+                  if(left(duration,1)=0, 
+                     concat(right(left(duration,2),1), 'hr '),
+                   concat(left(duration,2),'hr '))), 
+               if(date_format(duration,'%i')=00, '', 
+                  if(left(date_format(duration,'%i'),1)=0, 
+                     concat(right(date_format(duration,'%i'),1),'min'), 
+                     concat(date_format(duration,'%i'),'min')))) Duration, 
+        roomname, 
+        trackname as TRACK, 
+        typename as TYPE,
+        K.kidscatname,
+        title, 
+        progguiddesc as 'Long Text', 
+        group_concat(' ',pubsname, if (moderator=1,' (m)','')) as 'PARTIC' 
     FROM
-            Sessions S
-       JOIN Schedule SCH USING (sessionid)
-       JOIN Rooms R USING (roomid)
-       JOIN Tracks T USING (trackid)
-       JOIN Types Ty USING (typeid)
-	   JOIN KidsCategories K USING (kidscatid)
-  LEFT JOIN ParticipantOnSession POS ON SCH.sessionid=POS.sessionid 
-  LEFT JOIN Participants P ON POS.badgeid=P.badgeid
+                Sessions S
+           JOIN Schedule SCH USING (sessionid)
+           JOIN Rooms R USING (roomid)
+           JOIN Tracks T USING (trackid)
+           JOIN Types Ty USING (typeid)
+           JOIN KidsCategories K USING (kidscatid)
+      LEFT JOIN ParticipantOnSession POS ON SCH.sessionid=POS.sessionid 
+      LEFT JOIN Participants P ON POS.badgeid=P.badgeid
     WHERE 
-            S.pubstatusid = 2
+        S.pubstatusid = 2
     GROUP BY
-            SCH.sessionid
+        SCH.sessionid
     ORDER BY 
-            SCH.starttime, 
-            R.roomname
+        SCH.starttime, 
+        R.roomname;
 EOD;
-if (!$result = mysqli_query_exit_on_error($query)) {
-    exit(); // should have exited already
-}
-echo_if_zero_rows_and_exit($result);
-header('Content-disposition: attachment; filename=pocketprogram.csv');
-header('Content-type: text/csv');
-echo "sessionid,day,time,duration,room,track,type,\"kids category\",title,description,participants\n";
-render_query_result_as_csv($result);
-exit();
-?>
+$report['output_filename'] = 'pocketprogram.csv';
+$report['column_headings'] = 'sessionid,day,time,duration,room,track,type,"kids category",title,description,participants';
