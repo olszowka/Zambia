f691194cd03bcc13aa79c0a7585440d6d30a3713
Peter Olszowka <polszowka@cargurus.com>
2019-01-13 15:01:13 -0500

    Partial work transforming CSV reports to new report engine.

--- webpages/reports/publongdesccsv.php
+++ webpages/reports/publongdesccsv.php
@@ -1,7 +1,7 @@
 <?php
-// Copyright (c) 2009-2018 Peter Olszowka. All rights reserved. See copyright document for more details.
-// Report Name: Pocket Program
-// Report Description: Export CSV file of public schedule for generating pocket program
+// Copyright (c) 2009-2019 Peter Olszowka. All rights reserved. See copyright document for more details.
+// Report Name: Long Description
+// Report Description: Export CSV file of yet another full public schedule
 // Report Categories: Reports downloadable as CSVs: 90
 require_once('db_functions.php');
 require_once('StaffCommonCode.php'); //reset connection to db and check if logged in
@@ -16,28 +16,28 @@ if (!$result = mysqli_query_exit_on_error($query)) {
 mysqli_free_result($result);
 $query = <<<EOD
 SELECT
-	          S.sessionid,
-	          T.trackname,
-	          TY.typename,
-	          DV.divisionname,
-	          PS.pubstatusname,
-	          S.pubsno,
-	          group_concat(PC.pubcharname SEPARATOR ' ') pubcharacteristics,
-	          K.kidscatname,
-	          S.title,
-	          S.progguiddesc AS 'Description'
+        S.sessionid,
+        T.trackname,
+        TY.typename,
+        DV.divisionname,
+        PS.pubstatusname,
+        S.pubsno,
+        group_concat(PC.pubcharname SEPARATOR ' ') pubcharacteristics,
+        K.kidscatname,
+        S.title,
+        S.progguiddesc AS 'Description'
 	FROM
-	          Schedule SCH
-	     JOIN Sessions S USING(sessionid)
-	     JOIN Tracks T USING(trackid)
-	     JOIN Types TY USING(typeid)
-	     JOIN Divisions DV USING(divisionid)
-	     JOIN PubStatuses PS USING(pubstatusid)
-	     JOIN KidsCategories K USING(kidscatid)
-	LEFT JOIN SessionHasPubChar SHPC USING(sessionid)
-	LEFT JOIN PubCharacteristics PC USING(pubcharid)
-	    WHERE PS.pubstatusname = 'Public'
-	 GROUP BY scheduleid
+                  Schedule SCH
+             JOIN Sessions S USING(sessionid)
+             JOIN Tracks T USING(trackid)
+             JOIN Types TY USING(typeid)
+             JOIN Divisions DV USING(divisionid)
+             JOIN PubStatuses PS USING(pubstatusid)
+             JOIN KidsCategories K USING(kidscatid)
+        LEFT JOIN SessionHasPubChar SHPC USING(sessionid)
+        LEFT JOIN PubCharacteristics PC USING(pubcharid)
+    WHERE PS.pubstatusname = 'Public'
+    GROUP BY scheduleid
 EOD;
 if (!$result = mysqli_query_exit_on_error($query)) {
     exit(); // should have exited already
