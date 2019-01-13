e450e94bf6f994e9d08a5d190011253e678d86b9
Peter Olszowka <polszowka@cargurus.com>
2019-01-13 00:36:13 -0500

    Partial work transforming CSV reports to new report engine.

--- webpages/reports/palmcsv.php
+++ webpages/reports/palmcsv.php
@@ -1,49 +1,36 @@
 <?php
-// Copyright (c) 2009-2018 Peter Olszowka. All rights reserved. See copyright document for more details.
-// Report Name: Palm Calendar
-// Report Description: Export CSV file of for Palm device calendars
-// Report Categories: Reports downloadable as CSVs: 60
-require_once('db_functions.php');
-require_once('StaffCommonCode.php'); //reset connection to db and check if logged in
-require_once('csv_report_functions.php');
-global $title;
-$title = "Send CSV file of Schedule for PDA Upload";
-$ConStartDatim = CON_START_DATIM; // make it a variable so it can be substituted
-$query = "SET group_concat_max_len=25000";
-if (!$result = mysqli_query_exit_on_error($query)) {
-    exit(); // should have exited already
-}
-mysqli_free_result($result);
-$query = <<<EOD
+// Copyright (c) 2009-2019 Peter Olszowka. All rights reserved. See copyright document for more details.
+$report = [];
+$report['name'] = 'Palm Calendar';
+$report['description'] = 'Export CSV file for Palm device calendars';
+$report['categories'] = array(
+    'Reports downloadable as CSVs' => 60
+);
+$report['csv_output'] = true;
+$report['group_concat_expand'] = true;
+$report['queries'] = [];
+$report['queries']['master'] =<<<'EOD'
 SELECT
-            DATE_FORMAT(ADDTIME('$ConStartDatim',starttime),'%a') as 'Day',
-            DATE_FORMAT(ADDTIME('$ConStartDatim',starttime),'%l:%i %p') as 'Start Time',
-            left(duration,5) Length,
-	        Roomname,
-            trackname as Track,
-            Title,
-            if(group_concat(pubsname) is NULL,'',group_concat(pubsname SEPARATOR ', ')) as 'Participants'
+        DATE_FORMAT(ADDTIME('$ConStartDatim',starttime),'%a') as 'Day',
+        DATE_FORMAT(ADDTIME('$ConStartDatim',starttime),'%l:%i %p') as 'Start Time',
+        left(duration,5) Length,
+        Roomname,
+        trackname as Track,
+        Title,
+        if(group_concat(pubsname) is NULL,'',group_concat(pubsname SEPARATOR ', ')) as 'Participants'
     FROM
-            Rooms R
-       JOIN Schedule SCH USING (roomid)
-       JOIN Sessions S USING (sessionid)
-  LEFT JOIN Tracks T USING (trackid)
-  LEFT JOIN ParticipantOnSession POS ON SCH.sessionid=POS.sessionid
-  LEFT JOIN Participants P ON POS.badgeid=P.badgeid
+                Rooms R
+           JOIN Schedule SCH USING (roomid)
+           JOIN Sessions S USING (sessionid)
+      LEFT JOIN Tracks T USING (trackid)
+      LEFT JOIN ParticipantOnSession POS ON SCH.sessionid=POS.sessionid
+      LEFT JOIN Participants P ON POS.badgeid=P.badgeid
     WHERE
-            S.pubstatusid = 2
+        S.pubstatusid = 2
     GROUP BY
-            SCH.sessionid
+        SCH.sessionid
     ORDER BY
-            SCH.starttime, R.roomname
+        SCH.starttime, R.roomname;
 EOD;
-if (!$result = mysqli_query_exit_on_error($query)) {
-    exit(); // should have exited already
-}
-echo_if_zero_rows_and_exit($result);
-header('Content-disposition: attachment; filename=PDASchedule.csv');
-header('Content-type: text/csv');
-echo "day,start time,duration,room name,track,title,participants\n";
-render_query_result_as_csv($result);
-exit();
-?>
+$report['output_filename'] = 'PDASchedule.csv';
+$report['column_headings'] = 'day,start time,duration,room name,track,title,participants';
