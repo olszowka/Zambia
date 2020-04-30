4310c2a4402cecc8640ea67a5e9a743a0323c56c
Syd Weinstein (Git Key) <63214729+sydwhv@users.noreply.github.com>
2020-04-30 14:24:09 -0400

    Database changes to prepare for AdminPhases page addition

---
+++ Install/Upgrade_dbase/51_admin_phases.sql
@@ -0,0 +1,15 @@
+## This script Adds fields to phases in prep for new phases screen and to allow for display order sort
+##
+##	Created by Syd Weinstein on April 30, 2020
+## 	Copyright (c) 2020 by Peter Olszowka. All rights reserved. See copyright document for more details.
+##
+
+## Add new atom for the AdminPhases page to change Zambia Phases
+##
+insert into permissionatoms(permatomtag, page, notes)
+values('AdminPhases', 'AdminPhases', 'Change phase of Zambia use, allowing which sections are current');
+
+## Add both Administrator and AdminPhases to the Administrator role
+##
+insert into permissions(permatomid, phaseid, permroleid,badgeid)
+values(2,NULL, 1, NULL), (19,NULL,1, NULL);
