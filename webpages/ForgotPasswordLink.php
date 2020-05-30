2b54c55848dc2519fa5cc0f13a7990ab18fb5177
Peter Olszowka <p.olszowka@gmail.com>
2020-05-30 18:46:43 -0400

    Allow users to reset their own passwords (#66)
    * Incorporate Swift 5.4.8 into repo in preparation for password overhaul.
    
    * Incorporate Guzzle 6.5.3 into repo in preparation for password overhaul.
    
    * Allow users to reset their own passwords -- WIP
    
    * Allow users to reset their own passwords -- 1st complete draft
    
    * Refinements of self service password reset via email.  Includes integration with Swift Mailer and Guzzle into Repo.
    
    * Users can reset own passwords -- straw man 1
    
    * Users can reset own passwords -- use shared function to configure swift
    
    * Users can reset own passwords -- remove unneeded swift files; updated db creation scripts
    
    * Users can reset own passwords -- update schema to allow tracking of invalid badgeids
    
    Co-authored-by: Peter Olszowka <polszowka@cargurus.com>

---
+++ webpages/ForgotPasswordLink.php
@@ -0,0 +1,78 @@
+<?php
+// Created by Peter Olszowka on 2020-04-21;
+// Copyright (c) 2020 The Peter Olszowka. All rights reserved. See copyright document for more details.
+global $linki, $title;
+$title = "Reset Password";
+require ('PartCommonCode.php');
+$selector = getString('selector');
+$validator = getString('validator');
+if (RESET_PASSWORD_SELF !== true) {
+    http_response_code(403); // forbidden
+    participant_header($title, true, 'Login');
+    echo "<p class='alert alert-error vert-sep-above'>You have reached this page in error.</p>";
+    participant_footer();
+    exit;
+}
+participant_header($title, true, 'Login');
+if (empty($selector) || empty($validator)) {
+    echo "<p class='alert alert-error vert-sep-above'>Reset password link was missing required parameters.</p>";
+    participant_footer();
+    exit;
+}
+$selectorSQL = mysqli_real_escape_string($linki, $selector);
+$query = <<<EOD
+SELECT
+        PPRR.badgeidentered, PPRR.token, P.pubsname, CD.badgename, CD.firstname, CD.lastname
+    FROM
+             ParticipantPasswordResetRequests PPRR
+        JOIN Participants P ON PPRR.badgeidentered = P.badgeid
+        JOIN CongoDump CD USING (badgeid)
+    WHERE
+            selector = '$selectorSQL'
+        AND cancelled = 0
+        AND NOW() < expirationdatetime;
+EOD;
+if (!$result = mysqli_query_exit_on_error($query)) {
+    exit;
+}
+if (mysqli_num_rows($result) !== 1) {
+    // Report failure, but not specifically why
+    RenderXSLT('ForgotPasswordBadLink.xsl');
+    participant_footer();
+    exit;
+}
+list($badgeid, $token, $pubsname, $badgename, $firstname, $lastname) = mysqli_fetch_array($result);
+mysqli_free_result($result);
+$calc = hash('sha256', hex2bin($validator));
+if (!hash_equals($token, $calc)) {
+    // Report failure, but not specifically why
+    RenderXSLT('ForgotPasswordBadLink.xsl');
+    participant_footer();
+    exit;
+}
+$controlParams = array(
+    "selector" => $selector,
+    "validator" => $validator,
+    "badgeid" => $badgeid
+);
+$controlArray = generateControlString($controlParams);
+if (!empty($badgename)) {
+    $username = $badgename;
+} elseif (!empty($pubsname)) {
+    $username = $pubsname;
+} else {
+    $comboname = "$firstname $lastname";
+    if (!empty($comboname)) {
+        $username = $comboname;
+    } else {
+        $username = "";
+    }
+}
+$params = array(
+    "control" => $controlArray['control'],
+    "controliv" => $controlArray['controliv'],
+    "user_name" => $username,
+    "badgeid" => $badgeid
+);
+RenderXSLT('ForgotPasswordResetForm.xsl', $params);
+participant_footer();
