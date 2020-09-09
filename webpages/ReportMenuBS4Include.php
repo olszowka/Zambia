1ddc48812ec2b97599f3d06c3dfbb3faaa906b30
Syd Weinstein (Git Key) <63214729+sydwhv@users.noreply.github.com>
2020-09-09 17:24:00 -0400

    freeze point prior to rebase

---
+++ webpages/ReportMenuBS4Include.php
@@ -0,0 +1,16 @@
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Arisia+TV+Reports'>Arisia TV Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Conflict+Reports'>Conflict Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Events+Reports'>Events Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Fast+Track+Reports'>Fast Track Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=GOH+Reports'>GOH Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Gaming+Reports'>Gaming Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Grid+Reports'>Grid Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Hotel+Reports'>Hotel Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Participant+Info+Reports'>Participant Info Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Programming+Reports'>Programming Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Publication+Reports'>Publication Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Registration+Reports'>Registration Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Reports+downloadable+as+CSVs'>Reports downloadable as CSVs</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Security+Reports'>Security Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Tech+Reports'>Tech Reports</a>
+<a class='dropdown-item' href='staffReportsInCategory.php?reportcategory=Zambia+Administration+Reports'>Zambia Administration Reports</a>
