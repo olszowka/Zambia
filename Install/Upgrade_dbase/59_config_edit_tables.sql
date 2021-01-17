8754d7db396dd69ef6f15c99f270ab6ef6df05e7
Syd Weinstein (Git Key) <63214729+sydwhv@users.noreply.github.com>
2021-01-17 14:10:35 -0500

    add atom ids directly to insert in permissionatoms

--- Install/Upgrade_dbase/59_config_edit_tables.sql
+++ Install/Upgrade_dbase/59_config_edit_tables.sql
@@ -4,29 +4,29 @@
 ## 	Copyright (c) 2021 by Peter Olszowka. All rights reserved. See copyright document for more details.
 ##
 
-INSERT INTO PermissionAtoms(permatomtag, page, notes)
+INSERT INTO PermissionAtoms(permatomid, permatomtag, page, notes)
 VALUES
-	('ce_BioEditStatuses', 'Edit Configuration Tables', 'enables edit'),
-	('ce_Credentials', 'Edit Configuration Tables', 'enables edit'),
-	('ce_Roles', 'Edit Configuration Tables', 'enables edit'),
-	('ce_KidsCategories', 'Edit Configuration Tables', 'enables edit'),
-	('ce_LanguageStatuses', 'Edit Configuration Tables', 'enables edit'),
-	('ce_PubStatuses', 'Edit Configuration Tables', 'enables edit'),
-	('ce_SessionStatuses', 'Edit Configuration Tables', 'enables edit'),
-	('ce_Divisions', 'Edit Configuration Tables', 'enables edit'),
-	('ce_RegTypes', 'Edit Configuration Tables', 'enables edit'),
-	('ce_Tags', 'Edit Configuration Tables', 'enables edit'),
-	('ce_Times', 'Edit Configuration Tables', 'enables edit'),
-	('ce_Tracks', 'Edit Configuration Tables', 'enables edit'),
-	('ce_Types', 'Edit Configuration Tables', 'enables edit'),
-	('ce_Rooms', 'Edit Configuration Tables', 'enables edit'),
-	('ce_RoomSets', 'Edit Configuration Tables', 'enables edit'),
-	('ce_RoomHasSet', 'Edit Configuration Tables', 'enables edit'),
-	('ce_Features', 'Edit Configuration Tables', 'enables edit'),
-	('ce_Services', 'Edit Configuration Tables', 'enables edit'),
-	('ce_EmailFrom', 'Edit Configuration Tables', 'enables edit'),
-	('ce_EmailTo', 'Edit Configuration Tables', 'enables edit'),
-	('ce_EmailCC', 'Edit Configuration Tables', 'enables edit');
+	(2000, 'ce_BioEditStatuses', 'Edit Configuration Tables', 'enables edit'),
+	(2001, 'ce_Credentials', 'Edit Configuration Tables', 'enables edit'),
+	(2002, 'ce_Roles', 'Edit Configuration Tables', 'enables edit'),
+	(2003, 'ce_KidsCategories', 'Edit Configuration Tables', 'enables edit'),
+	(2004, 'ce_LanguageStatuses', 'Edit Configuration Tables', 'enables edit'),
+	(2005, 'ce_PubStatuses', 'Edit Configuration Tables', 'enables edit'),
+	(2006, 'ce_SessionStatuses', 'Edit Configuration Tables', 'enables edit'),
+	(2007, 'ce_Divisions', 'Edit Configuration Tables', 'enables edit'),
+	(2008, 'ce_RegTypes', 'Edit Configuration Tables', 'enables edit'),
+	(2009, 'ce_Tags', 'Edit Configuration Tables', 'enables edit'),
+	(2010, 'ce_Times', 'Edit Configuration Tables', 'enables edit'),
+	(2011, 'ce_Tracks', 'Edit Configuration Tables', 'enables edit'),
+	(2012, 'ce_Types', 'Edit Configuration Tables', 'enables edit'),
+	(2013, 'ce_Rooms', 'Edit Configuration Tables', 'enables edit'),
+	(2014, 'ce_RoomSets', 'Edit Configuration Tables', 'enables edit'),
+	(2015, 'ce_RoomHasSet', 'Edit Configuration Tables', 'enables edit'),
+	(2016, 'ce_Features', 'Edit Configuration Tables', 'enables edit'),
+	(2017, 'ce_Services', 'Edit Configuration Tables', 'enables edit'),
+	(2018, 'ce_EmailFrom', 'Edit Configuration Tables', 'enables edit'),
+	(2019, 'ce_EmailTo', 'Edit Configuration Tables', 'enables edit'),
+	(2020, 'ce_EmailCC', 'Edit Configuration Tables', 'enables edit');
 
 INSERT INTO Permissions(permatomid, phaseid, permroleid)
 SELECT a.permatomid, null, r.permroleid 
