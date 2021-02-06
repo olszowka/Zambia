f30f1f7595e181e89c0c38f0e1e4aa2e079fbfa6
Syd Weinstein (Git Key) <63214729+sydwhv@users.noreply.github.com>
2021-02-05 20:04:01 -0500

    add permission stuff

--- Install/Upgrade_dbase/60_photos.sql
+++ Install/Upgrade_dbase/60_photos.sql
@@ -19,6 +19,26 @@ CREATE TABLE PhotoDenialReasons (
   PRIMARY KEY (photodenialreasonid)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
+## Table name is too long for the atom id, lINSERT INTO PermissionAtoms(permatomid, permatomtag, page, notes)
+VALUES
+(2021, 'ce_PhotoDenialReasons', 'Edit Configuration Tables', 'enables edit');
+INSERT INTO Permissions(permatomid, phaseid, permroleid)
+SELECT a.permatomid, null, r.permroleid 
+FROM PermissionAtoms a
+JOIN PermissionRoles r ON (r.permrolename = 'Administrator')
+WHERE permatomtag = 'ce_PhotoDenialReasons';engthen it
+ALTER TABLE PermissionAtoms MODIFY COLUMN permatomtag varchar(32) NOT NULL;
+
+## NOW Add in the permissionatom and permission
+INSERT INTO PermissionAtoms(permatomid, permatomtag, page, notes)
+VALUES (2021, 'ce_PhotoDenialReasons', 'Edit Configuration Tables', 'enables edit');
+
+INSERT INTO Permissions(permatomid, phaseid, permroleid)
+SELECT a.permatomid, null, r.permroleid 
+FROM PermissionAtoms a
+JOIN PermissionRoles r ON (r.permrolename = 'Administrator')
+WHERE permatomtag = 'ce_PhotoDenialReasons';
+
 ALTER TABLE Participants ADD CONSTRAINT Participants_photodeny FOREIGN KEY (photodenialreasonid) REFERENCES PhotoDenialReasons (photodenialreasonid);
 
 INSERT INTO PatchLog (patchname) VALUES ('60_photos.sql');
