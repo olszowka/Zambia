97cfb32af6c505f8a0e1fae3353fb653fa01f9d2
Syd Weinstein (Git Key) <63214729+sydwhv@users.noreply.github.com>
2021-04-20 13:33:31 -0400

    Remove merge conflict on Availability

--- webpages/ParticipantHeader.php
+++ webpages/ParticipantHeader.php
@@ -63,7 +63,6 @@ function participant_header($title, $noUserRequired = false, $loginPageStatus = 
                             <li><a href="welcome.php">Overview</a></li>
                             <li><a href="my_contact.php">Profile</a></li>
                             <?php makeMenuItem("Photo", PARTICIPANT_PHOTOS === TRUE, "my_photo.php", false); ?>
-                            <?php makeMenuItem("Availability", may_I('my_availability'),"my_sched_constr.php",false); ?>
                             <?php if ($_SESSION['survey_exists']) { ?>
                             <li><a href="PartSurvey.php">Survey</a></li>
                             <?php }
