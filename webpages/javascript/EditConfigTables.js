a2a7cca6f7b81b89dd4e6bf1efed9fe72ac3b9eb
Syd Weinstein (Git Key) <63214729+sydwhv@users.noreply.github.com>
2021-04-20 15:42:00 -0400

    same patch on add event listener for config tables

--- webpages/javascript/EditConfigTables.js
+++ webpages/javascript/EditConfigTables.js
@@ -78,6 +78,8 @@ var EditConfigTable = function () {
             tabhide(x);
         $("#unsavedWarningModal").modal({ show: false });
         });
+        var addnewrowbut = document.getElementById("add-row");
+        addnewrowbut.addEventListener('click', function () { addnewrow(table); });
     }
 };
 
@@ -292,8 +294,6 @@ function opentable(tabledata) {
             }
         },
     });
-    var addnewrowbut = document.getElementById("add-row");
-    addnewrowbut.addEventListener('click', function () { addnewrow(table); });
     //console.log("Setting up options in table");
     document.getElementById("submitbtn").innerHTML = "Save";
     table.clearHistory();
