5cc46025011f8d03e39d7c08612cd2e616e16d2b
BC Holmes <github@bcholmes.org>
2021-06-05 23:56:41 -0400

    Another page converted to Bootstrap 4 (per #11).

--- webpages/renderMySuggestions.php
+++ webpages/renderMySuggestions.php
@@ -1,47 +1,51 @@
 <?php
 // Copyright (c) 2005-2021 Peter Olszowka. All rights reserved. See copyright document for more details.
 function renderMySuggestions($title, $error, $message, $paneltopics, $otherideas, $suggestedguests) {
-    participant_header($title);
+    participant_header($title, false, 'Normal', true);
+    echo "<div class=\"container mt-2\">";
     if ($error) {
         echo "<p class=\"alert alert-error\">" . $message . "</p>";
     } elseif ($message != "") {
         echo "<p class=\"alert alert-success\">" . $message . "</p>";
     }
     if (!may_I('my_suggestions_write')) {
-        echo "<p>We're sorry, but we are unable to accept your suggestions at this time.\n";
+        echo "<p class=\"alert alert-info\">We're sorry, but we are unable to accept your suggestions at this time.\n";
     }
+    echo "<div class=\"card\">";
+    echo "<div class=\"card-body\">";
     echo "<div class=\"my-suggestions-wrapper\">";
     echo "<form name=\"addform\" method=\"post\" action=\"SubmitMySuggestions.php\">\n";
-    echo "<div class=\"titledtextarea\">\n";
+    echo "<div class=\"titledtextarea form-group\">\n";
     echo "    <label for=\"paneltopics\">Program Topic Ideas:</label>\n";
-    echo "    <textarea name=\"paneltopics\" rows=\"6\" cols=\"72\"";
+    echo "    <textarea class=\"form-control\" name=\"paneltopics\" rows=\"6\" cols=\"72\"";
     if (!may_I('my_suggestions_write')) {
         echo " readonly class=\"readonly\"";
     }
     echo ">" . htmlspecialchars($paneltopics, ENT_COMPAT) . "</textarea>\n";
     echo "    </div>\n";
-    echo "<div class=\"titledtextarea\">\n";
+    echo "<div class=\"titledtextarea form-group\">\n";
     echo "    <label for=\"otherideas\">Other Programming Ideas:</label>\n";
-    echo "    <textarea name=\"otherideas\" rows=\"6\" cols=\"72\"";
+    echo "    <textarea class=\"form-control\" name=\"otherideas\" rows=\"6\" cols=\"72\"";
     if (!may_I('my_suggestions_write')) {
         echo " readonly class=\"readonly\"";
     }
     echo ">" . htmlspecialchars($otherideas, ENT_COMPAT) . "</textarea>\n";
     echo "    </div>\n";
-    echo "<div class=\"titledtextarea\">\n";
+    echo "<div class=\"titledtextarea form-group\">\n";
     echo "    <label for=\"suggestedguests\">Suggested Guests (please provide addresses and other contact information if possible):</label>\n";
-    echo "    <textarea name=\"suggestedguests\" rows=\"8\" cols=\"72\"";
+    echo "    <textarea class=\"form-control\" name=\"suggestedguests\" rows=\"8\" cols=\"72\"";
     if (!may_I('my_suggestions_write')) {
         echo " readonly class=\"readonly\"";
     }
     echo ">" . htmlspecialchars($suggestedguests, ENT_COMPAT) . "</textarea>\n";
     echo "    </div>\n";
-    echo "<div class=\"submit\">\n";
     if (may_I('my_suggestions_write')) {
-        echo "<div id=\"submit\"><button class=\"SubmitButton\" type=\"submit\" name=\"submit\">Save</button></div>\n";
+        echo "<div id=\"submit\"><button class=\"SubmitButton btn btn-primary\" type=\"submit\" name=\"submit\">Save</button></div>\n";
     }
-    echo "</div>\n";
     echo "</form>\n";
+    echo "</div>\n";
+    echo "</div>\n";
+    echo "</div>\n";
     echo "</div>\n";
     participant_footer();
 } ?>
