c43d0c8631ad85af95c19032e7dc76ecf7803804
Peter Olszowka <polszowka@cargurus.com>
2021-06-08 17:28:35 -0400

    TWeak PR--My interests and site-wide background.

--- webpages/renderMyInterests.php
+++ webpages/renderMyInterests.php
@@ -14,22 +14,22 @@ function renderMyInterests($title, $error, $message, $rolearray) {
     if (!may_I('my_gen_int_write')) {
         echo "<p>We're sorry, but we are unable to accept your suggestions at this time.\n";
     }
-    echo "<form name=\"addform\" method=POST action=\"SubmitMyInterests.php\">\n";
-    echo "<input type=\"hidden\" name=\"newrow\" value=\"" . ($newrow ? 1 : 0) . "\">\n";
-    echo "<input type=\"hidden\" name=\"rolerows\" value=\"" . $rolerows . "\">\n";
+    echo "<form name=\"addform\" method=\"POST\" action=\"SubmitMyInterests.php\" >\n";
+    echo "<input type=\"hidden\" name=\"newrow\" value=\"" . ($newrow ? 1 : 0) . "\" />\n";
+    echo "<input type=\"hidden\" name=\"rolerows\" value=\"" . $rolerows . "\" />\n";
     echo "<div class=\"row mt-3\">\n";
-    echo "  <div class=\"col-lg-6\">\n";
+    echo "  <div class=\"col-md-6\">\n";
     echo "    <label for=\"yespanels\">Workshops or presentations I'd like to run:</label>\n";
-    echo "    <textarea class=\"form-control\" id=\"yespanels\" name=\"yespanels\" rows=5 cols=72";
+    echo "    <textarea class=\"form-control\" id=\"yespanels\" name=\"yespanels\" rows=\"5\" cols=\"72\"";
     if (!may_I('my_gen_int_write')) {
         echo " readonly class=\"readonly\"";
     }
     echo ">" . htmlspecialchars($yespanels, ENT_COMPAT) . "</textarea>\n";
 
     echo "  </div>\n";
-    echo "  <div class=\"col-lg-6\">\n";
+    echo "  <div class=\"col-md-6\">\n";
     echo "    <label for=\"nopanels\">Panel types I am not interested in participating in:</label>\n";
-    echo "    <textarea class=\"form-control\" id=\"nopanels\" name=\"nopanels\" rows=5 cols=72";
+    echo "    <textarea class=\"form-control\" id=\"nopanels\" name=\"nopanels\" rows=\"5\" cols=\"72\"";
     if (!may_I('my_gen_int_write')) {
         echo " readonly class=\"readonly\"";
     }
@@ -37,17 +37,17 @@ function renderMyInterests($title, $error, $message, $rolearray) {
     echo "    </div>\n";
     echo "</div>\n";
     echo "<div class=\"row mt-3\">\n";
-    echo "  <div class=\"col-lg-6\">\n";
+    echo "  <div class=\"col-md-6\">\n";
     echo "    <label for=\"yespeople\">People with whom I'd like to be on a session: (Leave blank for none)</label>\n";
-    echo "    <textarea class=\"form-control\" id=\"yespeople\" name=\"yespeople\" rows=5 cols=72";
+    echo "    <textarea class=\"form-control\" id=\"yespeople\" name=\"yespeople\" rows=\"5\" cols=\"72\"";
     if (!may_I('my_gen_int_write')) {
         echo " readonly class=\"readonly\"";
     }
     echo ">" . htmlspecialchars($yespeople, ENT_COMPAT) . "</textarea>\n";
     echo "  </div>\n";
-    echo "  <div class=\"col-lg-6\">\n";
+    echo "  <div class=\"col-md-6\">\n";
     echo "    <label for=\"nopeople\">People with whom I'd rather not be on a session: (Leave blank for none)</label>\n";
-    echo "    <textarea class=\"form-control\" id=\"nopeople\" name=\"nopeople\" rows=5 cols=72";
+    echo "    <textarea class=\"form-control\" id=\"nopeople\" name=\"nopeople\" rows=\"5\" cols=\"72\"";
     if (!may_I('my_gen_int_write')) {
         echo " readonly class=\"readonly\"";
     }
@@ -56,42 +56,45 @@ function renderMyInterests($title, $error, $message, $rolearray) {
     echo "</div>\n";
     echo "<p class=\"mt-3\">Roles I'm willing to take on:</p>\n";
     echo "<div class=\"row mt-3\">\n";
+    echo "    <div class=\"col-12 roles-list-container\">\n";
     for ($i = 1; $i < $rolerows; $i++) {
-        echo "        <div class=\"col-md-4\">";
-        echo "                <input type=checkbox name=\"willdorole" . $i . "\" id=\"willdorole" . $i . "\"";
+        echo "        <div class=\"role-entry-container\">\n";
+        echo "            <label for=\"willdorole" . $i . "\">\n";
+        echo "                <input type=\"checkbox\" name=\"willdorole" . $i . "\" id=\"willdorole" . $i . "\"";
         if (isset($rolearray[$i]["badgeid"])) {
-            echo "checked";
+            echo " checked";
         }
         if (!may_I('my_gen_int_write')) {
             echo " disabled";
         }
-        echo ">";
-        echo "    <label class=\"checkbox\" for=\"willdorole" . $i . "\">" . $rolearray[$i]["rolename"] . "\n";
-        echo "                <input type=hidden name=\"diddorole" . $i . "\" value=\"";
-        echo ((isset($rolearray[$i]["badgeid"])) ? 1 : 0) . "\">\n";
-        echo "                <input type=hidden name=\"roleid" . $i . "\" value=\"" . $rolearray[$i]["roleid"] . "\">\n";
-        echo "                <input type=hidden name=\"rolename" . $i . "\" value=\"" . $rolearray[$i]["rolename"] . "\">\n";
-        echo "</div>\n";
+        echo " class=\"mr-2\" />";
+        echo $rolearray[$i]["rolename"] . "</label>\n";
+        echo "            <input type=\"hidden\" name=\"diddorole" . $i . "\" value=\"";
+        echo ((isset($rolearray[$i]["badgeid"])) ? 1 : 0) . "\" />\n";
+        echo "            <input type=\"hidden\" name=\"roleid" . $i . "\" value=\"" . $rolearray[$i]["roleid"] . "\" />\n";
+        echo "            <input type=\"hidden\" name=\"rolename" . $i . "\" value=\"" . $rolearray[$i]["rolename"] . "\" />\n";
+        echo "        </div>\n";
     }
 
-    echo "        <div class=\"col-md-4\">";
-    echo "                <input class=\"checkbox\" type=checkbox name=\"willdorole0\" id=\"willdorole0\" ";
+    echo "        <div class=\"role-entry-container\">\n";
+    echo "            <label for=\"willdorole0\">\n";
+    echo "                <input type=\"checkbox\" name=\"willdorole0\" id=\"willdorole0\" ";
     if (isset($rolearray[0]["badgeid"])) {
         echo "checked";
     }
     if (!may_I('my_gen_int_write')) {
         echo " disabled";
     }
-    echo ">\n";
-    echo "<label class=\"checkbox\" for=\"willdorole0\">" . $rolearray[0]["rolename"] . "  (Please describe below)</label>";
-    echo "                <input type=hidden name=\"roleid0\" value=\"" . $rolearray[0]["roleid"] . "\">\n";
-    echo "                <input type=hidden name=\"rolename0\" value=\"" . $rolearray[0]["rolename"] . "\">\n";
-    echo "                <input type=hidden name=\"diddorole0\" value=\"";
-    echo ((isset($rolearray[0]["badgeid"])) ? 1 : 0) . "\">\n";
-    echo "</div></div>";
-    echo "<div class=\"row\"><div class=\"col-12\">";
+    echo " class=\"mr-2\"/>";
+    echo $rolearray[0]["rolename"] . "  (Please describe below)</label>\n";
+    echo "            <input type=hidden name=\"roleid0\" value=\"" . $rolearray[0]["roleid"] . "\" />\n";
+    echo "            <input type=hidden name=\"rolename0\" value=\"" . $rolearray[0]["rolename"] . "\" />\n";
+    echo "            <input type=hidden name=\"diddorole0\" value=\"";
+    echo ((isset($rolearray[0]["badgeid"])) ? 1 : 0) . "\" />\n";
+    echo "</div></div></div>\n";
+    echo "<div class=\"row\"><div class=\"col-12\">\n";
     echo "<label class=\"mt-3\" for=\"otherroles\">Description for \"Other\" Roles:</label>\n";
-    echo "<textarea class=\"form-control\" id=\"otherroles\" name=\"otherroles\" rows=5 cols=72";
+    echo "<textarea class=\"form-control\" id=\"otherroles\" name=\"otherroles\" rows=\"5\" cols=\"72\"";
     if (!may_I('my_gen_int_write')) {
         echo " readonly class=\"readonly\"";
     }
