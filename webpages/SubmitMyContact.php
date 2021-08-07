0b4536fe1f0d04d75ae4c649845721c0fafaa25a
Syd Weinstein (Git Key) <63214729+sydwhv@users.noreply.github.com>
2021-08-06 20:11:54 -0400

    Fix quote typo

--- webpages/SubmitMyContact.php
+++ webpages/SubmitMyContact.php
@@ -96,7 +96,7 @@ foreach ($_POST as $name => $value) {
     $ccid = mb_substr($name, 13);
     switch ($value) {
         case "1":
-            $valuesClause2 .= ($valuesClause2 ? ", " : "") . "(''$badgeid', $ccid)";
+            $valuesClause2 .= ($valuesClause2 ? ", " : "") . "('$badgeid', $ccid)";
             break;
         case "0":
             $credentialClause3 .= ($credentialClause3 ? ", " : "") . $ccid;
